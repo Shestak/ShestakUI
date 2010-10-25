ace490f7e6324b574db2c8046732ec5f357236d8
Shestak <misha.shestakov@gmail.com>
2010-10-25 21:17:02 +0400

    Added missing files.

---
+++ ShestakUI/config/fonts.lua
@@ -0,0 +1,60 @@
+﻿----------------------------------------------------------------------------------------
+--	Fonts options !!!!!!!!!!!NOT WORKING!!!!!!!!!!!
+----------------------------------------------------------------------------------------
+SettingsCF["font"] = {
+	-- Stats font
+	["stats_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["stats_font_size"] = 8,
+	["stats_font_style"] = "OUTLINEMONOCHROME",
+	["stats_font_shadow"] = false,
+	-- Unit frames font
+	["unit_frames_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["unit_frames_font_size"] = 8,
+	["unit_frames_font_style"] = "OUTLINEMONOCHROME",
+	["unit_frames_font_shadow"] = false,
+	-- Auras font
+	["auras_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["auras_font_size"] = 8,
+	["auras_font_style"] = "OUTLINEMONOCHROME",
+	["auras_font_shadow"] = false,
+	-- Combat text font
+	["combat_text_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["combat_text_font_size"] = 16,
+	["combat_text_font_style"] = "OUTLINEMONOCHROME",
+	["combat_text_font_shadow"] = false,
+	-- Chat tabs font
+	["chat_tabs_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["chat_tabs_font_size"] = 8,
+	["chat_tabs_font_style"] = "OUTLINEMONOCHROME",
+	["chat_tabs_font_shadow"] = false,
+	-- Action bars font
+	["action_bars_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["action_bars_font_size"] = 8,
+	["action_bars_font_style"] = "OUTLINEMONOCHROME",
+	["action_bars_font_shadow"] = false,
+	-- Threat meter font
+	["threat_meter_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["threat_meter_font_size"] = 8,
+	["threat_meter_font_style"] = "OUTLINEMONOCHROME",
+	["threat_meter_font_shadow"] = false,
+	-- Raid cooldown font
+	["raid_cooldowns_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["raid_cooldowns_font_size"] = 8,
+	["raid_cooldowns_font_style"] = "OUTLINEMONOCHROME",
+	["raid_cooldowns_font_shadow"] = false,
+	-- Cooldowns timer font
+	["cooldown_timers_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["cooldown_timers_font_size"] = 16,
+	["cooldown_timers_font_style"] = "OUTLINEMONOCHROME",
+	["cooldown_timers_font_shadow"] = false,
+	-- Filger font
+	["filger_bars_name_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["filger_bars_name_font_size"] = 8,
+	["filger_bars_name_font_style"] = "OUTLINEMONOCHROME",
+	["filger_bars_name_font_shadow"] = false,
+	-- Stylization font
+	["stylization_font"] = "Interface\\AddOns\\ShestakUI\\media\\pixel.ttf",
+	["stylization_font_size"] = 8,
+	["stylization_font_style"] = "OUTLINEMONOCHROME",
+	["stylization_font_shadow"] = false,
+}
