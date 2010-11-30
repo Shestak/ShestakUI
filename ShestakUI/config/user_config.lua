bef7bbe8b2242dc4b7fe8cbe03738475c2998023
Shestak <misha.shestakov@gmail.com>
2010-11-30 15:25:20 +0300

    Change font size for zhTW client(http://shestak.org/forum/showpost.php?p=4453&postcount=14).

--- ShestakUI/config/user_config.lua
+++ ShestakUI/config/user_config.lua
@@ -77,9 +77,10 @@ if SettingsDB.client == "zhTW" then
 	SettingsCF["media"].normal_font = "Fonts\\bLEI00D.ttf"
 	SettingsCF["media"].pixel_font = "Fonts\\bLEI00D.ttf"
 	SettingsCF["media"].pixel_font_style = "OUTLINE"
+	SettingsCF["media"].pixel_font_size = 11
 
 	SettingsCF["font"].stats_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].stats_font_size = 15
+	SettingsCF["font"].stats_font_size = 12
 	SettingsCF["font"].stats_font_style = "OUTLINE"
 	SettingsCF["font"].stats_font_shadow = true
 
@@ -93,47 +94,47 @@ if SettingsDB.client == "zhTW" then
 	SettingsCF["font"].chat_font_shadow = true
 
 	SettingsCF["font"].chat_tabs_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].chat_tabs_font_size = 15
+	SettingsCF["font"].chat_tabs_font_size = 12
 	SettingsCF["font"].chat_tabs_font_style = "OUTLINE"
 	SettingsCF["font"].chat_tabs_font_shadow = true
 
 	SettingsCF["font"].action_bars_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].action_bars_font_size = 15
+	SettingsCF["font"].action_bars_font_size = 12
 	SettingsCF["font"].action_bars_font_style = "OUTLINE"
 	SettingsCF["font"].action_bars_font_shadow = true
 
 	SettingsCF["font"].threat_meter_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].threat_meter_font_size = 15
+	SettingsCF["font"].threat_meter_font_size = 12
 	SettingsCF["font"].threat_meter_font_style = "OUTLINE"
 	SettingsCF["font"].threat_meter_font_shadow = true
 
 	SettingsCF["font"].raid_cooldowns_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].raid_cooldowns_font_size = 15
+	SettingsCF["font"].raid_cooldowns_font_size = 12
 	SettingsCF["font"].raid_cooldowns_font_style = "OUTLINE"
 	SettingsCF["font"].raid_cooldowns_font_shadow = true
 
 	SettingsCF["font"].cooldown_timers_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].cooldown_timers_font_size = 20
+	SettingsCF["font"].cooldown_timers_font_size = 13
 	SettingsCF["font"].cooldown_timers_font_style = "OUTLINE"
 	SettingsCF["font"].cooldown_timers_font_shadow = true
 
 	SettingsCF["font"].loot_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].loot_font_size = 15
+	SettingsCF["font"].loot_font_size = 13
 	SettingsCF["font"].loot_font_style = "OUTLINE"
 	SettingsCF["font"].loot_font_shadow = true
 
 	SettingsCF["font"].nameplates_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].nameplates_font_size = 15
+	SettingsCF["font"].nameplates_font_size = 13
 	SettingsCF["font"].nameplates_font_style = "OUTLINE"
 	SettingsCF["font"].nameplates_font_shadow = true
 
 	SettingsCF["font"].unit_frames_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].unit_frames_font_size = 14
+	SettingsCF["font"].unit_frames_font_size = 12
 	SettingsCF["font"].unit_frames_font_style = "OUTLINE"
 	SettingsCF["font"].unit_frames_font_shadow = true
 
 	SettingsCF["font"].auras_font = "Fonts\\bLEI00D.ttf"
-	SettingsCF["font"].auras_font_size = 13
+	SettingsCF["font"].auras_font_size = 11
 	SettingsCF["font"].auras_font_style = "OUTLINE"
 	SettingsCF["font"].auras_font_shadow = true
 
@@ -150,4 +151,5 @@ elseif SettingsDB.client == "zhCN" then
 	SettingsCF["media"].normal_font = "Fonts\\ZYKai_T.ttf"
 	SettingsCF["media"].pixel_font = "Fonts\\ZYKai_T.ttf"
 	SettingsCF["media"].pixel_font_style = "OUTLINE"
+	SettingsCF["media"].pixel_font_size = 11
 end
