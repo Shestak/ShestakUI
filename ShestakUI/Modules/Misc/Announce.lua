52a515c49b4d129e2a9a54b7280772c330a30244
Shestak <misha.shestakov@gmail.com>
2011-03-03 21:27:40 +0300

    Added enemy drinking announce in chat.

--- ShestakUI/Modules/Misc/Announce.lua
+++ ShestakUI/Modules/Misc/Announce.lua
@@ -1,17 +1,32 @@
 local T, C, L = unpack(select(2, ...))
 ----------------------------------------------------------------------------------------
---	Announce your interrupts(by Elv22)
+--	Announce your interrupts(by Elv22) or enemy drinking(by Duffed)
 ----------------------------------------------------------------------------------------
 if C.misc.announce_interrupt ~= true then return end
 
-local announce = CreateFrame("Frame")
-announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
+local interrupt_announce = CreateFrame("Frame")
+interrupt_announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+interrupt_announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
 	if not (event == "SPELL_INTERRUPT" and sourceName == T.name) then return end
 	
 	if GetNumPartyMembers() > 0 then
 		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "PARTY", nil, nil)
 	elseif GetNumRaidMembers() > 0 then
 		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "RAID", nil, nil)
+	end
+end)
+
+local drinking_announce = CreateFrame("Frame")
+drinking_announce:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+drinking_announce:SetScript("OnEvent", function(self, event, ...)
+	if not (event == "UNIT_SPELLCAST_SUCCEEDED" and GetZonePVPInfo() == "arena") then return end
+	
+	local unit, spellName, spellrank, spelline, spellID = ...
+	if UnitIsEnemy("player", unit) and (spellID == 80167 or spellID == 94468 or spellID == 43183 or spellID == 57073 or spellName == "Trinken") then
+		if GetNumPartyMembers() > 0 then
+			SendChatMessage(UnitName(unit).." is drinking.", "PARTY")
+		elseif GetNumRaidMembers() > 0 then
+			SendChatMessage(UnitName(unit).." is drinking.", "RAID")
+		end
 	end
 end)
