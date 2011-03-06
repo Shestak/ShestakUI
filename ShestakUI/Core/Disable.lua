c7ef8e558657b8a49b1686585ab6093770dc3be7
Shestak <misha.shestakov@gmail.com>
2011-03-06 21:24:46 +0300

    Added stub if splitbar enabled.

--- ShestakUI/Core/Disable.lua
+++ ShestakUI/Core/Disable.lua
@@ -16,6 +16,11 @@ if C.actionbar.bottombars == 3 and C.actionbar.rightbars == 3 then
 	C.actionbar.rightbars = 2
 end
 
+if C.actionbar.split_bars == true then
+	C.actionbar.bottombars = 2
+	C.actionbar.rightbars = 2
+end
+
 if C.actionbar.bottombars < 1 then
 	C.actionbar.bottombars = 1
 end
