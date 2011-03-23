d33fa13e5db716971f24bcf6accaa54f4289525a
Shestak <misha.shestakov@gmail.com>
2011-03-24 02:07:23 +0300

    Disable BattlefieldMinimap in 4.1 client.

--- ShestakUI/Modules/Maps/BattlefieldMap.lua
+++ ShestakUI/Modules/Maps/BattlefieldMap.lua
@@ -1,5 +1,6 @@
 local T, C, L = unpack(select(2, ...))
 if not C.map.bg_map_stylization == true then return end
+if T.patch >= string.format("%s", "4.1.0") then return end
 
 ----------------------------------------------------------------------------------------
 --	BattlefieldMinimap style
