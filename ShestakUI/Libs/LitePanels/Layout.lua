314f8438b4b875cb6ca74c981d508b7b741f02f0
Shestak <misha.shestakov@gmail.com>
2011-03-27 04:19:06 +0400

    Improved styling AFK panel.

--- ShestakUI/Libs/LitePanels/Layout.lua
+++ ShestakUI/Libs/LitePanels/Layout.lua
@@ -1,39 +1,63 @@
-﻿----------------------------------------------------------------------------------------
+﻿local T, C, L = unpack(select(2, ...))
+
+----------------------------------------------------------------------------------------
 --	LitePanels configuration file
 --	BACKUP YOUR CHANGES TO THIS FILE BEFORE UPDATING!
 ----------------------------------------------------------------------------------------
-local T, C, L = unpack(select(2, ...))
 lpanels:CreateLayout("Load For All", {
+
 -- Player panel
-{   name = "PlayerBottomBlackBar", parent = "oUF_Player",
-    anchor_to = "BOTTOMLEFT", x_off = -6, y_off = -7,
-    width = 230, height = 3,
-    bg_color = "0 0 0", bg_alpha = 1,
+{	name = "PlayerBottomBlackBar", parent = "oUF_Player",
+	anchor_to = "BOTTOMLEFT", x_off = -6, y_off = -7,
+	width = 230, height = 3,
+	bg_color = "0 0 0", bg_alpha = 1,
 },
-{   name = "PlayerBottomColorBar", parent = "PlayerBottomBlackBar",
-    anchor_to = "CENTER",
-    width = 228, height = 1,
-    bg_color = "CLASS", bg_alpha = 1,
+{	name = "PlayerBottomColorBar", parent = "PlayerBottomBlackBar",
+	anchor_to = "CENTER",
+	width = 228, height = 1,
+	bg_color = "CLASS", bg_alpha = 1,
 },
-{   name = "PlayerLeftBlackBar", parent = "oUF_Player",
-    anchor_to = "BOTTOMLEFT", x_off = -7, y_off = -42,
-    width = 3, height = 100,
-    bg_color = "0 0 0", bg_alpha = 1,
+{	name = "PlayerLeftBlackBar", parent = "oUF_Player",
+	anchor_to = "BOTTOMLEFT", x_off = -7, y_off = -42,
+	width = 3, height = 100,
+	bg_color = "0 0 0", bg_alpha = 1,
 },
-{   name = "PlayerLeftColorBar", parent = "PlayerLeftBlackBar",
-    anchor_to = "CENTER",
-    width = 1, height = 98,
-    bg_color = "CLASS", bg_alpha = 1,
+{	name = "PlayerLeftColorBar", parent = "PlayerLeftBlackBar",
+	anchor_to = "CENTER",
+	width = 1, height = 98,
+	bg_color = "CLASS", bg_alpha = 1,
 },
+
 -- Target panel
-{   name = "TargetBottomBlackBar", parent = "oUF_Target",
-    anchor_to = "BOTTOMRIGHT", x_off = 6, y_off = -7,
-    width = 230, height = 3,
-    bg_color = "0 0 0", bg_alpha = 1,
+{	name = "TargetBottomBlackBar", parent = "oUF_Target",
+	anchor_to = "BOTTOMRIGHT", x_off = 6, y_off = -7,
+	width = 230, height = 3,
+	bg_color = "0 0 0", bg_alpha = 1,
 },
-{   name = "TargetBottomColorBar", parent = "TargetBottomBlackBar",
-    anchor_to = "CENTER",
-    width = 228, height = 1,
+{	name = "TargetBottomColorBar", parent = "TargetBottomBlackBar",
+	anchor_to = "CENTER",
+	width = 228, height = 1,
+	OnLoad = function(self)
+		self:RegisterEvent'PLAYER_TARGET_CHANGED'
+	end,
+	OnEvent = function(self)
+		local _, class = UnitClass'target'
+		local color = RAID_CLASS_COLORS[class]
+		if color then
+			self.bg:SetTexture(color.r, color.g, color.b)
+		else
+			self.bg:SetTexture(unpack(C.media.border_color))
+		end
+	end,
+},
+{	name = "TargetRightBlackBar", parent = "oUF_Target",
+	anchor_to = "BOTTOMRIGHT", x_off = 7, y_off = -42,
+	width = 3, height = 100,
+	bg_color = "0 0 0", bg_alpha = 1,
+},
+{	name = "TargetRightColorBar", parent = "TargetRightBlackBar",
+	anchor_to = "CENTER",
+	width = 1, height = 98,
 	OnLoad = function(self)
 		self:RegisterEvent'PLAYER_TARGET_CHANGED'
 	end,
@@ -47,36 +71,16 @@ lpanels:CreateLayout("Load For All", {
 		end
 	end,
 },
-{   name = "TargetRightBlackBar", parent = "oUF_Target",
-    anchor_to = "BOTTOMRIGHT", x_off = 7, y_off = -42,
-    width = 3, height = 100,
-    bg_color = "0 0 0", bg_alpha = 1,
-},
-{   name = "TargetRightColorBar", parent = "TargetRightBlackBar",
-    anchor_to = "CENTER",
-    width = 1, height = 98,
-	OnLoad = function(self)
-		self:RegisterEvent'PLAYER_TARGET_CHANGED'
-	end,
-	OnEvent = function(self)
-		local _,class = UnitClass'target'
-		local color = RAID_CLASS_COLORS[class]
-		if color then
-			self.bg:SetTexture(color.r, color.g, color.b)
-		else
-			self.bg:SetTexture(unpack(C.media.border_color))
-		end
-	end,
-},
+
 -- Target portrait border
-{   name = "TargetPortraitBlackBar", parent = "oUF_Target_PortraitOverlay",
-    anchor_to = "TOPLEFT", x_off = -1, y_off = 1,
-    width = C.unitframe.portrait_width + 6, height = C.unitframe.portrait_height + 6,
-    bg_color = "0 0 0", bg_alpha = 1,
+{	name = "TargetPortraitBlackBar", parent = "oUF_Target_PortraitOverlay",
+	anchor_to = "TOPLEFT", x_off = -1, y_off = 1,
+	width = C.unitframe.portrait_width + 6, height = C.unitframe.portrait_height + 6,
+	bg_color = "0 0 0", bg_alpha = 1,
 },
-{   name = "TargetPortraitColorBar", parent = "TargetPortraitBlackBar",
-    anchor_to = "CENTER",
-    width = C.unitframe.portrait_width + 4, height = C.unitframe.portrait_height + 4,
+{	name = "TargetPortraitColorBar", parent = "TargetPortraitBlackBar",
+	anchor_to = "CENTER",
+	width = C.unitframe.portrait_width + 4, height = C.unitframe.portrait_height + 4,
 	OnLoad = function(self)
 		self:RegisterEvent'PLAYER_TARGET_CHANGED'
 	end,
@@ -94,16 +98,15 @@ lpanels:CreateLayout("Load For All", {
 		end
 	end,
 },
-{   name = "TargetPortraitBlack", parent = "TargetPortraitColorBar",
-    anchor_to = "CENTER",
-    width = C.unitframe.portrait_width + 2, height = C.unitframe.portrait_height + 2,
-    bg_color = "0 0 0", bg_alpha = 1,
+{	name = "TargetPortraitBlack", parent = "TargetPortraitColorBar",
+	anchor_to = "CENTER",
+	width = C.unitframe.portrait_width + 2, height = C.unitframe.portrait_height + 2,
+	bg_color = "0 0 0", bg_alpha = 1,
 },
+
 -- AFK panel
 {	name = "AFK", anchor_to = "TOP", y_off = -210,
-	bg_alpha = 0.8, width = 180, height = 75,
-	inset = { left = -1, right = -1, top = -1, bottom = -1 },
-	border = "SOLID", border_color = "0.37 0.3 0.3",
+	width = 180, height = 75,
 	text = {
 			{	string = L_PANELS_AFK, anchor_to = "TOP", y_off = -10,
 				shadow = 0, outline = 3, font = C.font.stats_font, size = C.font.stats_font_size,
@@ -125,30 +128,32 @@ lpanels:CreateLayout("Load For All", {
 				shadow = 0, outline = 3, font = C.font.stats_font, size = C.font.stats_font_size, 
 			}
 		},
-        OnLoad = function(self)
-            self:RegisterEvent("PLAYER_FLAGS_CHANGED")
-            self:Hide()
-        end,
-        OnEvent = function(self)
-            if UnitIsAFK("player") and not afk_timer then
-                self.text2:SetText("0:00")
-                afk_timer = time()
-                self:Show()
-            elseif not UnitIsAFK("player") then
-                self:Hide()
-                afk_timer = nil
-            end
-        end,
-        OnClick = function(self, b)
-            self:Hide()
-            if b == "LeftButton" then SendChatMessage("", "AFK") end
-        end,
-        OnEnter = function(self) self.bg:SetTexture(0.1,0.1,0.1,0.8) end,
-        OnLeave = function(self) self.bg:SetTexture(0,0,0,0.8) end
-    },
+		OnLoad = function(self)
+			self:RegisterEvent("PLAYER_FLAGS_CHANGED")
+			self:SetTemplate("Transparent")
+			self:Hide()
+		end,
+		OnEvent = function(self)
+			if UnitIsAFK("player") and not afk_timer then
+				self.text2:SetText("0:00")
+				afk_timer = time()
+				self:Show()
+			elseif not UnitIsAFK("player") then
+				self:Hide()
+				afk_timer = nil
+			end
+		end,
+		OnClick = function(self, b)
+			self:Hide()
+			if b == "LeftButton" then SendChatMessage("", "AFK") end
+		end,
+		OnEnter = function(self) self:SetBackdropBorderColor(T.color.r, T.color.g, T.color.b) end,
+		OnLeave = function(self) self:SetBackdropBorderColor(unpack(C.media.border_color)) end
+	},
 })
 
 lpanels:CreateLayout("Enable top panel", {
+
 -- Top panel
 {	name = "StatContainer",
 	anchor_to = "TOP",
@@ -178,89 +183,89 @@ lpanels:CreateLayout("Enable top panel", {
 	bg_color = "CLASS", gradient_color = "CLASS",
 	bg_alpha = 0.2, gradient_alpha = 0,
 },
-{   name = "LineTBTL",
-    parent = "BoxL", anchor_to = "TOP", x_off = 0, y_off = 1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineTBTL",
+	parent = "BoxL", anchor_to = "TOP", x_off = 0, y_off = 1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineBBTL",
-    parent = "BoxL", anchor_to = "TOP", x_off = 0, y_off = -1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineBBTL",
+	parent = "BoxL", anchor_to = "TOP", x_off = 0, y_off = -1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineTL",
-    parent = "BoxL", anchor_to = "TOP",
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "CLASS", gradient_color = "CLASS",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineTL",
+	parent = "BoxL", anchor_to = "TOP",
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "CLASS", gradient_color = "CLASS",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineTBTR",
-    parent = "BoxR", anchor_to = "TOP", x_off = 0, y_off = 1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineTBTR",
+	parent = "BoxR", anchor_to = "TOP", x_off = 0, y_off = 1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0.8, gradient_alpha = 0,
 },
-{   name = "LineBBTR",
-    parent = "BoxR", anchor_to = "TOP", x_off = 0, y_off = -1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineBBTR",
+	parent = "BoxR", anchor_to = "TOP", x_off = 0, y_off = -1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0.8, gradient_alpha = 0,
 },
-{   name = "LineTR",
-    parent = "BoxR", anchor_to = "TOP",
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "CLASS", gradient_color = "CLASS",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineTR",
+	parent = "BoxR", anchor_to = "TOP",
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "CLASS", gradient_color = "CLASS",
+	bg_alpha = 0.8, gradient_alpha = 0,
 },
-{   name = "LineTBBL",
-    parent = "BoxL", anchor_to = "BOTTOM", x_off = 0, y_off = 1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineTBBL",
+	parent = "BoxL", anchor_to = "BOTTOM", x_off = 0, y_off = 1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineBBBL",
-    parent = "BoxL", anchor_to = "BOTTOM", x_off = 0, y_off = -1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineBBBL",
+	parent = "BoxL", anchor_to = "BOTTOM", x_off = 0, y_off = -1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineBL",
-    parent = "BoxL", anchor_to = "BOTTOM",
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "CLASS", gradient_color = "CLASS",
-    bg_alpha = 0, gradient_alpha = 0.8,
+{	name = "LineBL",
+	parent = "BoxL", anchor_to = "BOTTOM",
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "CLASS", gradient_color = "CLASS",
+	bg_alpha = 0, gradient_alpha = 0.8,
 },
-{   name = "LineTBBR",
-    parent = "BoxR", anchor_to = "BOTTOM", x_off = 0, y_off = 1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineTBBR",
+	parent = "BoxR", anchor_to = "BOTTOM", x_off = 0, y_off = 1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0.8, gradient_alpha = 0,
 },
-{   name = "LineBBBR",
-    parent = "BoxR", anchor_to = "BOTTOM", x_off = 0, y_off = -1,
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "0 0 0", gradient_color = "0 0 0",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineBBBR",
+	parent = "BoxR", anchor_to = "BOTTOM", x_off = 0, y_off = -1,
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "0 0 0", gradient_color = "0 0 0",
+	bg_alpha = 0.8, gradient_alpha = 0,
 },
-{   name = "LineBR",
-    parent = "BoxR", anchor_to = "BOTTOM",
-    width = "100%", height = 1,
-    gradient = "H",
-    bg_color = "CLASS", gradient_color = "CLASS",
-    bg_alpha = 0.8, gradient_alpha = 0,
+{	name = "LineBR",
+	parent = "BoxR", anchor_to = "BOTTOM",
+	width = "100%", height = 1,
+	gradient = "H",
+	bg_color = "CLASS", gradient_color = "CLASS",
+	bg_alpha = 0.8, gradient_alpha = 0,
 }
 })
 
