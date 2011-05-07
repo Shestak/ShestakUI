314fd0f351fd5cc9f168c4c77f2dd4ce650b523d
Shestak <misha.shestakov@gmail.com>
2011-05-07 06:43:13 +0400

    Added timer value on nameplate debuffs.

--- ShestakUI/Modules/ActionBars/Cooldowns.lua
+++ ShestakUI/Modules/ActionBars/Cooldowns.lua
@@ -9,15 +9,43 @@ local floor = math.floor
 local min = math.min
 
 local function GetFormattedTime(s)
-	local DAY, HOUR, MINUTE = 86400, 3600, 60
-	if s >= DAY then
-		return format("%dd", floor(s/DAY + 0.5)), s % DAY
-	elseif s >= HOUR then
-		return format("%dh", floor(s/HOUR + 0.5)), s % HOUR
-	elseif s >= MINUTE then
-		return format("%dm", floor(s/MINUTE + 0.5)), s % MINUTE
+	local day, hour, minute = 86400, 3600, 60
+	if s >= day then
+		return format("%dd", floor(s / day + 0.5)), s % day
+	elseif s >= hour then
+		return format("%dh", floor(s / hour + 0.5)), s % hour
+	elseif s >= minute then
+		return format("%dm", floor(s / minute + 0.5)), s % minute
 	end
 	return floor(s + 0.5), s - floor(s)
+end
+
+local function Timer_Stop(self)
+	self.enabled = nil
+	self:Hide()
+end
+
+local function Timer_ForceUpdate(self)
+	self.nextUpdate = 0
+	self:Show()
+end
+
+local function Timer_OnSizeChanged(self, width, height)
+	local fontScale = T.Round(width) / 40
+	if fontScale == self.fontScale then
+		return
+	end
+
+	self.fontScale = fontScale
+	if fontScale < 0.5 then
+		self:Hide()
+	else
+		self.text:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
+		self.text:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
+		if self.enabled then
+			Timer_ForceUpdate(self)
+		end
+	end
 end
 
 local function Timer_OnUpdate(self, elapsed)
@@ -34,13 +62,13 @@ local function Timer_OnUpdate(self, elapsed)
 					local time, nextUpdate = GetFormattedTime(remain)
 					self.text:SetText(time)
 					self.nextUpdate = nextUpdate
-					if(floor(remain + 0.5) > 5) then
+					if floor(remain + 0.5) > 5 then
 						self.text:SetTextColor(1, 1, 1)
 					else
 						self.text:SetTextColor(1, 0.2, 0.2)
 					end
 				else
-					self.text:Hide()
+					Timer_Stop(self)
 				end
 			end
 		end
@@ -48,39 +76,39 @@ local function Timer_OnUpdate(self, elapsed)
 end
 
 local function Timer_Create(self)
-	local scale = min(self:GetParent():GetWidth() / 40, 1)
-	if scale < 0.5 then
-		self.noOCC = true
-	else
-		local text = self:CreateFontString(nil, "OVERLAY")
-		text:Point("CENTER", 1, 0)
-		text:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-		text:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-        text:SetTextColor(1, 1, 1)
-		self.text = text
-		self:SetScript("OnUpdate", Timer_OnUpdate)
-		return text
-	end
+	local scaler = CreateFrame("Frame", nil, self)
+	scaler:SetAllPoints(self)
+
+	local timer = CreateFrame("Frame", nil, scaler)
+	timer:Hide()
+	timer:SetAllPoints(scaler)
+	timer:SetScript("OnUpdate", Timer_OnUpdate)
+
+	local text = timer:CreateFontString(nil, "OVERLAY")
+	text:Point("CENTER", 1, 0)
+	text:SetJustifyH("CENTER")
+	timer.text = text
+
+	Timer_OnSizeChanged(timer, scaler:GetSize())
+	scaler:SetScript("OnSizeChanged", function(self, ...) Timer_OnSizeChanged(timer, ...) end)
+
+	self.timer = timer
+	return timer
 end
 
-local function Timer_Start(self, start, duration)
-	self.start = start
-	self.duration = duration
-	self.nextUpdate = 0
-	local text = self.text or (not self.noOCC and Timer_Create(self))
-	if text then
-		text:Show()
-	end
-end
-
-local methods = getmetatable(_G["ActionButton1Cooldown"]).__index
-hooksecurefunc(methods, "SetCooldown", function(self, start, duration)
+hooksecurefunc(getmetatable(ActionButton1Cooldown).__index, "SetCooldown", function(self, start, duration)
+	if self.noOCC then return end
 	if start > 0 and duration > 2 then
-		Timer_Start(self, start, duration)
+		local timer = self.timer or Timer_Create(self)
+		timer.start = start
+		timer.duration = duration
+		timer.enabled = true
+		timer.nextUpdate = 0
+		if timer.fontScale >= 0.5 then timer:Show() end
 	else
-		local text = self.text
-		if text then
-			text:Hide()
+		local timer = self.timer
+		if timer then
+			Timer_Stop(timer)
 		end
 	end
 end)
