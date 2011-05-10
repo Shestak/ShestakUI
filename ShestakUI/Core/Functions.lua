4f05b721a610a8dcefc151d1179f95fcf198dd3c
Shestak <misha.shestakov@gmail.com>
2011-05-10 09:53:35 +0400

    Added Delay finction.

--- ShestakUI/Core/Functions.lua
+++ ShestakUI/Core/Functions.lua
@@ -275,6 +275,39 @@ T.UTF = function(string, i, dots)
 end
 
 ----------------------------------------------------------------------------------------
+--	Add time before calling a function
+----------------------------------------------------------------------------------------
+local waitTable = {}
+local waitFrame
+function T.Delay(delay, func, ...)
+	if(type(delay)~="number" or type(func)~="function") then
+		return false
+	end
+	if(waitFrame == nil) then
+		waitFrame = CreateFrame("Frame", "WaitFrame", UIParent)
+		waitFrame:SetScript("onUpdate", function(self, elapse)
+			local count = #waitTable
+			local i = 1
+			while(i<=count) do
+				local waitRecord = tremove(waitTable,i)
+				local d = tremove(waitRecord,1)
+				local f = tremove(waitRecord,1)
+				local p = tremove(waitRecord,1)
+				if(d>elapse) then
+				  tinsert(waitTable,i,{d-elapse,f,p})
+				  i = i + 1
+				else
+				  count = count - 1
+				  f(unpack(p))
+				end
+			end
+		end)
+	end
+	tinsert(waitTable,{delay,func,{...}})
+	return true
+end
+
+----------------------------------------------------------------------------------------
 --	Unit frames functions
 ----------------------------------------------------------------------------------------
 local ADDON_NAME, ns = ...
