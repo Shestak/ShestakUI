de841bcf8a5ff934fe0c7da3a8ba6b20e2cc4705
Shestak <misha.shestakov@gmail.com>
2011-05-17 01:52:06 +0400

    Same fixed in xCT.

--- ShestakUI/Modules/Blizzard/CombatText.lua
+++ ShestakUI/Modules/Blizzard/CombatText.lua
@@ -891,7 +891,7 @@ if(C.combattext.damage)then
 	if(C.combattext.icons)then
 		ct.blank="Interface\\AddOns\\ShestakUI\\Media\\Textures\\Blank.tga"
 	end
-	local misstypes={ABSORB=ABSORB,BLOCK=BLOCK,DODGE=DODGE,EVADE=EVADE,IMMUNE=IMMUNE,MISS=MISS,PARRY=PARRY,REFLECT=REFLECT,RESIST=RESIST}
+	local misstypes={ABSORB=ABSORB,BLOCK=BLOCK,DODGE=DODGE,EVADE=EVADE,IMMUNE=IMMUNE,MISS=MISS,PARRY=PARRY,REFLECT=REFLECT,RESIST=RESIST,DEFLECT=DEFLECT}
 	local dmg=function(self,event,...) 
 		local msg,icon
 		local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags = select(1,...)
