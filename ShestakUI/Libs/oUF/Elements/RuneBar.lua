d54201f0eb4ca828e55e1f6c6a87710ba82f732b
Shestak <misha.shestakov@gmail.com>
2011-05-29 00:16:32 +0400

    Updated oUF.

--- ShestakUI/Libs/oUF/Elements/RuneBar.lua
+++ ShestakUI/Libs/oUF/Elements/RuneBar.lua
@@ -14,6 +14,8 @@ oUF.colors.runes = {
 	{.9, .1, 1};
 }
 
+local runemap = { 1, 2, 5, 6, 3, 4 }
+
 local OnUpdate = function(self, elapsed)
 	local duration = self.duration + elapsed
 	if(duration >= self.max) then
@@ -24,9 +26,9 @@ local OnUpdate = function(self, elapsed)
 	end
 end
 
-local UpdateType = function(self, event, rune, alt)
-	local colors = self.colors.runes[GetRuneType(rune) or alt]
-	local rune = self.Runes[rune]
+local UpdateType = function(self, event, rid, alt)
+	local rune = self.Runes[runemap[rid]]
+	local colors = self.colors.runes[GetRuneType(rid) or alt]
 	local r, g, b = colors[1], colors[2], colors[3]
 
 	rune:SetStatusBarColor(r, g, b)
@@ -38,9 +40,9 @@ local UpdateType = function(self, event, rune, alt)
 end
 
 local UpdateRune = function(self, event, rid)
-	local rune = self.Runes[rid]
+	local rune = self.Runes[runemap[rid]]
 	if(rune) then
-		local start, duration, runeReady = GetRuneCooldown(rune:GetID())
+		local start, duration, runeReady = GetRuneCooldown(rid)
 		if(runeReady) then
 			rune:SetMinMaxValues(0, 1)
 			rune:SetValue(1)
@@ -72,10 +74,9 @@ local Enable = function(self, unit)
 
 		for i=1, 6 do
 			local rune = runes[i]
-			rune:SetID(i)
 			-- From my minor testing this is a okey solution. A full login always remove
 			-- the death runes, or at least the clients knowledge about them.
-			UpdateType(self, nil, i, math.floor((i+1)/2))
+			UpdateType(self, nil, i, math.floor((runemap[i]+1)/2))
 
 			if(not rune:GetStatusBarTexture()) then
 				rune:SetStatusBarTexture[[Interface\TargetingFrame\UI-StatusBar]]
