b5847d77cf38524db2f2e9f8297579febdec9500
Shestak <misha.shestakov@gmail.com>
2011-05-31 09:06:18 +0400

    Added bags/bank item glow if C.bag.enable == false.

---
+++ ShestakUI/Libs/oGlow/Pipes/Bank.lua
@@ -0,0 +1,29 @@
+local T, C, L = unpack(select(2, ...))
+if C.bag.enable == true then return end
+
+local update = function(self)
+	if(BankFrame:IsShown()) then
+		for i=1, NUM_BANKGENERIC_SLOTS or 28 do
+			local slotFrame = _G['BankFrameItem' .. i]
+			local slotLink = GetContainerItemLink(-1, i)
+
+			self:CallFilters('bank', slotFrame, slotLink)
+		end
+	end
+end
+
+local enable = function(self)
+	self:RegisterEvent('BANKFRAME_OPENED', update)
+	self:RegisterEvent('PLAYERBANKSLOTS_CHANGED', update)
+end
+
+local disable = function(self)
+	self:UnregisterEvent('BANKFRAME_OPENED', update)
+	self:UnregisterEvent('PLAYERBANKSLOTS_CHANGED', update)
+
+	for i=1, NUM_BANKGENERIC_SLOTS or 28 do
+		self:CallFilters('bank', _G['BankFrameItem' .. i])
+	end
+end
+
+oGlow:RegisterPipe('bank', enable, disable, update, 'Player bank frame', nil)
