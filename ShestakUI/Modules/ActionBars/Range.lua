37be6d468b20eb26e47eefe4f0aaca5c843dea45
Shestak <misha.shestakov@gmail.com>
2011-07-06 01:05:22 +0400

    Added Inquisition and Word of Glory for tullaRange.

--- ShestakUI/Modules/ActionBars/Range.lua
+++ ShestakUI/Modules/ActionBars/Range.lua
@@ -56,6 +56,8 @@ do
 	local HOLY_POWER_SPELLS = {
 		[85256] = GetSpellInfo(85256), -- Templar's Verdict
 		[53600] = GetSpellInfo(53600), -- Shield of the Righteous
+		[84963] = GetSpellInfo(84963), -- Inquisition
+		--[85673] = GetSpellInfo(85673), -- Word of Glory
 	}
 
 	isHolyPowerAbility = function(actionId)
