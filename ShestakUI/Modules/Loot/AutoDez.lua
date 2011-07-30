2a31f2b6f4ce9d36a3acaa68066145d0090d8f26
Shestak <misha.shestakov@gmail.com>
2011-07-31 03:46:48 +0400

    Added AutoNeed list items.

--- ShestakUI/Modules/Loot/AutoDez.lua
+++ ShestakUI/Modules/Loot/AutoDez.lua
@@ -8,11 +8,17 @@ if C.loot.auto_greed == true then
 	autogreed:RegisterEvent("START_LOOT_ROLL")
 	autogreed:SetScript("OnEvent", function(self, event, id)
 		local name = select(2, GetLootRollItemInfo(id))
-		if (name == select(1, GetItemInfo(43102))) then
+		if name == select(1, GetItemInfo(43102)) then
 			RollOnLoot(id, 2)
 		end
 		if T.level ~= MAX_PLAYER_LEVEL then return end
-		if(id and select(4, GetLootRollItemInfo(id))==2 and not (select(5, GetLootRollItemInfo(id)))) then
+		if id and select(4, GetLootRollItemInfo(id)) == 2 and not select(5, GetLootRollItemInfo(id)) then
+			for i in pairs(T.NeedLoot) do
+				if name == select(1, GetItemInfo(T.NeedLoot[i])) and RollOnLoot(id, 1) then
+					RollOnLoot(id, 1)
+					return
+				end
+			end
 			if RollOnLoot(id, 3) then
 				RollOnLoot(id, 3)
 			else
