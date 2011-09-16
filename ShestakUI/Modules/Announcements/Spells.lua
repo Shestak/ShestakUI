3e7c0d30d6576e7e693e969a6231e8996ae5d332
Shestak <misha.shestakov@gmail.com>
2011-09-16 17:38:39 +0400

    Same changes in SpellAnnounce.

--- ShestakUI/Modules/Announcements/Spells.lua
+++ ShestakUI/Modules/Announcements/Spells.lua
@@ -10,33 +10,35 @@ misdir_announce:SetScript("OnEvent", function(self, _, ...)
 	local _, event, _, sourceGUID, sourceName, _, _, _, destName, _, _, spellID = ...
 	local inInstance, instanceType = IsInInstance()
 	local spells = T.AnnounceSpells
-	if not ((event == "SPELL_CAST_SUCCESS" or event == "SPELL_RESURRECT") and (inInstance and (instanceType == "raid" or instanceType == "party"))) then return end
+	if not (inInstance and (instanceType == "raid" or instanceType == "party")) then return end
 
-	if C.announcements.spells_from_all == true then
-		if not (destName and sourceName) then return end
+	if event == "SPELL_CAST_SUCCESS" or event == "SPELL_RESURRECT" then
+		if C.announcements.spells_from_all == true then
+			if not (destName and sourceName) then return end
 
-		for i, spells in pairs(spells) do
-			if spellID == spells then
-				if GetRealNumRaidMembers() > 0 then
-					SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "RAID")
-				elseif GetRealNumPartyMembers() > 0 and not UnitInRaid("player") then
-					SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "PARTY")
-				else
-					SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "SAY")
+			for i, spells in pairs(spells) do
+				if spellID == spells then
+					if GetRealNumRaidMembers() > 0 then
+						SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "RAID")
+					elseif GetRealNumPartyMembers() > 0 and not UnitInRaid("player") then
+						SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "PARTY")
+					else
+						SendChatMessage(GetSpellLink(spellID)..": "..sourceName.." -> "..destName, "SAY")
+					end
 				end
 			end
-		end
-	else
-		if not (sourceGUID == UnitGUID("player") and destName) then return end
+		else
+			if not (sourceGUID == UnitGUID("player") and destName) then return end
 
-		for i, spells in pairs(spells) do
-			if spellID == spells then
-				if GetRealNumRaidMembers() > 0 then
-					SendChatMessage(GetSpellLink(spellID).." -> "..destName, "RAID")
-				elseif GetRealNumPartyMembers() > 0 and not UnitInRaid("player") then
-					SendChatMessage(GetSpellLink(spellID).." -> "..destName, "PARTY")
-				else
-					SendChatMessage(GetSpellLink(spellID).." -> "..destName, "SAY")
+			for i, spells in pairs(spells) do
+				if spellID == spells then
+					if GetRealNumRaidMembers() > 0 then
+						SendChatMessage(GetSpellLink(spellID).." -> "..destName, "RAID")
+					elseif GetRealNumPartyMembers() > 0 and not UnitInRaid("player") then
+						SendChatMessage(GetSpellLink(spellID).." -> "..destName, "PARTY")
+					else
+						SendChatMessage(GetSpellLink(spellID).." -> "..destName, "SAY")
+					end
 				end
 			end
 		end
