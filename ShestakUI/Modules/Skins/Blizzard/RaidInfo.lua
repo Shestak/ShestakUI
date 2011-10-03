626d8091322759ca923be4ec52b111f74a27fcf9
Shestak <misha.shestakov@gmail.com>
2011-10-03 04:54:13 +0400

    Skin RaidFinderFrame.

--- ShestakUI/Modules/Skins/Blizzard/RaidInfo.lua
+++ ShestakUI/Modules/Skins/Blizzard/RaidInfo.lua
@@ -6,10 +6,16 @@ if C.skins.blizzard_frames ~= true then return end
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
 	local StripAllTextures = {
+		"RaidInfoScrollFrame",
 		"RaidInfoFrame",
 		"RaidInfoInstanceLabel",
 		"RaidInfoIDLabel",
 	}
+
+	if T.PTRVersion() then
+		tinsert(StripAllTextures, "RaidParentFrame")
+		tinsert(StripAllTextures, "RaidFinderFrameRoleInset")
+	end
 
 	local KillTextures = {
 		"RaidInfoScrollFrameScrollBarBG",
@@ -17,6 +23,10 @@ local function LoadSkin()
 		"RaidInfoScrollFrameScrollBarBottom",
 		"RaidInfoScrollFrameScrollBarMiddle",
 	}
+
+	if T.PTRVersion() then
+		tinsert(KillTextures, "RaidParentFrameInset")
+	end
 
 	local buttons = {
 		"RaidFrameConvertToRaidButton",
@@ -38,11 +48,38 @@ local function LoadSkin()
 		_G[buttons[i]]:SkinButton()
 	end
 
-	RaidInfoScrollFrame:StripTextures()
 	RaidInfoFrame:CreateBackdrop("Transparent")
 	RaidInfoFrame.backdrop:Point("TOPLEFT", RaidInfoFrame, "TOPLEFT")
 	RaidInfoFrame.backdrop:Point("BOTTOMRIGHT", RaidInfoFrame, "BOTTOMRIGHT")
 	T.SkinCloseButton(RaidInfoCloseButton, RaidInfoFrame)
+
+	if T.PTRVersion() then
+		RaidParentFrame:CreateBackdrop("Transparent")
+		RaidParentFrame.backdrop:Point("TOPLEFT", RaidParentFrame, "TOPLEFT")
+		RaidParentFrame.backdrop:Point("BOTTOMRIGHT", RaidParentFrame, "BOTTOMRIGHT")
+		T.SkinCloseButton(RaidParentFrameCloseButton)
+		T.SkinDropDownBox(RaidFinderQueueFrameSelectionDropDown, 300)
+		RaidFinderQueueFrameSelectionDropDown:Point("RIGHT", -10, 0)
+
+		RaidFinderFrameFindRaidButton:SkinButton(true)
+		RaidFinderFrameCancelButton:SkinButton(true)
+
+		for i = 1, 2 do
+			T.SkinTab(_G["RaidParentFrameTab"..i])
+		end
+
+		local checkButtons = {
+			"RaidFinderQueueFrameRoleButtonTank",
+			"RaidFinderQueueFrameRoleButtonHealer",
+			"RaidFinderQueueFrameRoleButtonDPS",
+			"RaidFinderQueueFrameRoleButtonLeader",
+		}
+
+		for _, object in pairs(checkButtons) do
+			_G[object].checkButton:SetFrameLevel(_G[object].checkButton:GetFrameLevel() + 2)
+			T.SkinCheckBox(_G[object].checkButton)
+		end
+	end
 end
 
 tinsert(T.SkinFuncs["ShestakUI"], LoadSkin)
