35a95ed7ba9b584afcff6c7032878fd3f9e600d9
Shestak <misha.shestakov@gmail.com>
2011-11-12 22:31:33 +0400

    Fixed actionbar error in PTR.

--- ShestakUI/Modules/ActionBars/Range.lua
+++ ShestakUI/Modules/ActionBars/Range.lua
@@ -220,9 +220,6 @@ function tullaRange.SetButtonColor(button, colorType)
 
 		local icon =  _G[button:GetName() .. "Icon"]
 		icon:SetVertexColor(r, g, b)
-
-		local nt = button:GetNormalTexture()
-		nt:SetVertexColor(r, g, b)
 	end
 end
 
