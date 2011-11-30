9128422af13b00c6c305b63ca7ddcbc53cef7d7d
Shestak <misha.shestakov@gmail.com>
2011-11-30 15:23:16 +0400

    Same cleanup and changes.

--- ShestakUI/Core/Constants.lua
+++ ShestakUI/Core/Constants.lua
@@ -6,21 +6,13 @@
 T.dummy = function() return end
 T.name = select(1, UnitName("player"))
 T.class = select(2, UnitClass("player"))
-T.client = GetLocale() 
+T.level = UnitLevel("player")
+T.client = GetLocale()
+T.realm = GetRealmName()
+T.color = RAID_CLASS_COLORS[T.class]
+T.version = GetAddOnMetadata("ShestakUI", "Version")
 T.getscreenheight = tonumber(string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)"))
 T.getscreenwidth = tonumber(string.match(({GetScreenResolutions()})[GetCurrentResolution()], "(%d+)x+%d"))
-T.version = GetAddOnMetadata("ShestakUI", "Version")
-T.patch = select(4, GetBuildInfo())
-T.realm = GetRealmName()
-T.level = UnitLevel("player")
-T.color = RAID_CLASS_COLORS[T.class]
-T.PTRVersion = function()
-	if T.patch >= 40300 then
-		return true
-	else
-		return false
-	end
-end
 
 if T.name == "Черешок" or T.name == "Вершок" or T.name == "Вещмешок" or T.name == "Гребешок" or T.name == "Кулешок"
 or T.name == "Лапушок" or T.name == "Обушок" or T.name == "Ремешок" or T.name == "Шестак" or T.name == "Торгашок"
--- ShestakUI/Modules/Blizzard/Kill.lua
+++ ShestakUI/Modules/Blizzard/Kill.lua
@@ -12,10 +12,10 @@ Kill:SetScript("OnEvent", function(self, event, addon)
 			InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
 			InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
 			SetCVar("useCompactPartyFrames", 0)
-			CompactRaidFrameManager:UnregisterAllEvents()
-			CompactRaidFrameManager:Hide()
-			CompactRaidFrameContainer:UnregisterAllEvents()
-			CompactRaidFrameContainer:Hide()
+			CompactRaidFrameManager:Kill()
+			CompactRaidFrameContainer:Kill()
+			CompactUnitFrame_UpateVisible = T.dummy
+			CompactUnitFrame_UpdateAll = T.dummy
 		end
 	else
 		if addon == "Blizzard_AchievementUI" then
