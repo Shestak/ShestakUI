562eb96a37929076e50bd8b763a3ed1f6410ef13
Shestak <misha.shestakov@gmail.com>
2011-12-02 08:10:43 +0400

    Skin ExtraActionBarFrame.

--- ShestakUI/Modules/Blizzard/ExtraActionBar.lua
+++ ShestakUI/Modules/Blizzard/ExtraActionBar.lua
@@ -18,3 +18,26 @@ end)
 ExtraActionBarFrame:SetScript("OnMouseUp", function()
 	ExtraActionBarFrame:StopMovingOrSizing()
 end)
+
+------------------------------------------------------------------------------------------
+--	Skin ExtraActionBarFrame(by Roth)
+------------------------------------------------------------------------------------------
+local button = ExtraActionButton1
+local icon = button.icon
+local texture = button.style
+local disableTexture = function(style, texture)
+	if string.sub(texture, 1, 9) == "Interface" then
+		style:SetTexture("")
+	end
+end
+button.style:SetTexture("")
+hooksecurefunc(texture, "SetTexture", disableTexture)
+
+icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+icon:Point("TOPLEFT", button, 2, -2)
+icon:Point("BOTTOMRIGHT", button, -2, 2)
+icon:SetDrawLayer("ARTWORK")
+
+button:StyleButton(true)
+
+button:SetTemplate("Default")
