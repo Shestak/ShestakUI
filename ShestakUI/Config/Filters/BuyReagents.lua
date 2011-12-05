1fd7de46dfc60cd5022d3ff55ca862138af00916
Shestak <misha.shestakov@gmail.com>
2011-12-05 23:32:48 +0400

    Changes 40 to 20 items in AutoBuy list. It's very much.

--- ShestakUI/Config/Filters/BuyReagents.lua
+++ ShestakUI/Config/Filters/BuyReagents.lua
@@ -8,15 +8,15 @@ if C.misc.auto_buy_reagents ~= true and T.level ~= MAX_PLAYER_LEVEL then return 
 ----------------------------------------------------------------------------------------
 T.AutoBuy = {
 	MAGE = {
-		[17031] = 40,	-- Rune of Teleportation
-		[17032] = 40,	-- Rune of Portals
+		[17031] = 20,	-- Rune of Teleportation
+		[17032] = 20,	-- Rune of Portals
 		[17020] = 100,	-- Arcane Powder
 	},
 	ROGUE = {
-		[6947] = 40,	-- Instant Poison
-		[2892] = 40,	-- Deadly Poison
-		[3775] = 40,	-- Crippling Poison
-		[5237] = 40,	-- Mind-Numbing Poison
+		[6947] = 20,	-- Instant Poison
+		[2892] = 20,	-- Deadly Poison
+		[3775] = 20,	-- Crippling Poison
+		[5237] = 20,	-- Mind-Numbing Poison
 		[10918] = 20,	-- Wound Poison
 	},
 }
