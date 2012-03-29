24f789b66949918359ef054984657ebd21e104a3
Shestak <misha.shestakov@gmail.com>
2012-03-30 02:55:32 +0400

    Added more spells in CastbarTicks.

--- ShestakUI/Core/Functions.lua
+++ ShestakUI/Core/Functions.lua
@@ -1079,6 +1079,8 @@ local channelingTicks = {
 	-- Warlock
 	[GetSpellInfo(689)] = 3,	-- Drain Life
 	[GetSpellInfo(5740)] = 4,	-- Rain of Fire
+	[GetSpellInfo(1120)] = 5,	-- Drain Soul
+	[GetSpellInfo(755)] = 3,	-- Health Funnel
 	-- Druid
 	[GetSpellInfo(44203)] = 4,	-- Tranquility
 	[GetSpellInfo(16914)] = 10,	-- Hurricane
@@ -1086,6 +1088,8 @@ local channelingTicks = {
 	[GetSpellInfo(15407)] = 3,	-- Mind Flay
 	[GetSpellInfo(48045)] = 5,	-- Mind Sear
 	[GetSpellInfo(47540)] = 2,	-- Penance
+	[GetSpellInfo(64901)] = 4,	-- Hymn of Hope
+	[GetSpellInfo(64843)] = 4,	-- Divine Hymn
 	-- Mage
 	[GetSpellInfo(5143)] = 5,	-- Arcane Missiles
 	[GetSpellInfo(10)] = 5,		-- Blizzard
