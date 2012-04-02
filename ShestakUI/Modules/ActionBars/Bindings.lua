16d5d72b0b1eb1d9949628bcb5cf3cfe02aba8e8
Shestak <misha.shestakov@gmail.com>
2012-04-02 16:37:48 +0400

    Cleanup.

--- ShestakUI/Modules/ActionBars/Bindings.lua
+++ ShestakUI/Modules/ActionBars/Bindings.lua
@@ -1,5 +1,5 @@
 local T, C, L = unpack(select(2, ...))
-if IsAddOnLoaded("ncHoverBind") then return end
+if C.actionbar.enable ~= true or IsAddOnLoaded("ncHoverBind") then return end
 
 ----------------------------------------------------------------------------------------
 --	Binding buttons(ncHoverBind by nightcracker)
