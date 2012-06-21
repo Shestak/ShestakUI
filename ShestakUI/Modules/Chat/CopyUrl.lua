7ce9a0bf0145475a1aedf82422d3295c6721f0dc
Shestak <misha.shestakov@gmail.com>
2012-06-21 17:34:33 +0400

    Rewrite copy url from chat module.

--- ShestakUI/Modules/Chat/CopyUrl.lua
+++ ShestakUI/Modules/Chat/CopyUrl.lua
@@ -1,50 +1,42 @@
 local T, C, L = unpack(select(2, ...))
 if C.chat.enable ~= true then return end
 
-local SetItemRef_orig = SetItemRef
-function ReURL_SetItemRef(link, text, button, chatFrame)
-	if strsub(link, 1, 3) == "url" then
-		local ChatFrameEditBox = ChatEdit_ChooseBoxForSend()
-		local url = strsub(link, 5)
-		if not ChatFrameEditBox:IsShown() then
-			ChatEdit_ActivateChat(ChatFrameEditBox)
-		end
-		ChatFrameEditBox:Insert(url)
-		ChatFrameEditBox:HighlightText()
+----------------------------------------------------------------------------------------
+--	Copy url from chat(module from Gibberish by p3lim)
+----------------------------------------------------------------------------------------
+local patterns = {
+	"(https://%S+)",
+	"(http://%S+)",
+	"(www%.%S+)",
+	"(%d+%.%d+%.%d+%.%d+:?%d*)"
+}
 
-	else
-		SetItemRef_orig(link, text, button, chatFrame)
-	end
-end
-SetItemRef = ReURL_SetItemRef
-
-function ReURL_AddLinkSyntax(chatstring)
-	if type(chatstring) == "string" then
-		local extraspace
-		if not strfind(chatstring, "^ ") then
-			extraspace = true
-			chatstring = " "..chatstring
+for _, event in pairs({
+	"CHAT_MSG_GUILD",
+	"CHAT_MSG_PARTY",
+	"CHAT_MSG_RAID",
+	"CHAT_MSG_RAID_LEADER",
+	"CHAT_MSG_CHANNEL",
+	"CHAT_MSG_WHISPER",
+	"CHAT_MSG_BN_WHISPER",
+	"CHAT_MSG_SAY"
+}) do
+	ChatFrame_AddMessageEventFilter(event, function(self, event, str, ...)
+		for _, pattern in pairs(patterns) do
+			local result, match = string.gsub(str, pattern, "|cff00FF00|Hurl:%1|h[%1]|h|r")
+			if match > 0 then
+				return false, result, ...
+			end
 		end
-		chatstring = gsub(chatstring, " www%.([_A-Za-z0-9-]+)%.(%S+)%s?", ReURL_Link("www.%1.%2"))
-		chatstring = gsub(chatstring, " (%a+)://(%S+)%s?", ReURL_Link("%1://%2"))
-		chatstring = gsub(chatstring, " ([_A-Za-z0-9-%.]+)@([_A-Za-z0-9-]+)(%.+)([_A-Za-z0-9-%.]+)%s?", ReURL_Link("%1@%2%3%4"))
-		chatstring = gsub(chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?):(%d%d?%d?%d?%d?)%s?", ReURL_Link("%1.%2.%3.%4:%5"))
-		chatstring = gsub(chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%s?", ReURL_Link("%1.%2.%3.%4"))
-		if extraspace then
-			chatstring = strsub(chatstring, 2)
-		end
-	end
-	return chatstring
+	end)
 end
 
-function ReURL_Link(url)
-	url = " |cff00FF00|Hurl:"..url.."|h["..url.."]|h|r "
-	return url
-end
+local orig = SetItemRef
+function SetItemRef(link, str, ...)
+	if string.sub(link, 1, 3) ~= "url" then return orig(link, str, ...) end
 
--- Hook all the AddMessage funcs
-for i = 1, NUM_CHAT_WINDOWS do
-	local frame = _G["ChatFrame"..i]
-	local addmessage = frame.AddMessage
-	frame.AddMessage = function(self, text, ...) addmessage(self, ReURL_AddLinkSyntax(text), ...) end
+	local editbox = DEFAULT_CHAT_FRAME.editBox
+	ChatEdit_ActivateChat(editbox)
+	editbox:Insert(string.sub(link, 5))
+	editbox:HighlightText()
 end
