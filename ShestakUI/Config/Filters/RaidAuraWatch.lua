e01ba34f364e814490b4802182054c72442afd1e
Wetxius <wetxius@gmail.com>
2012-07-14 09:32:15 +0400

    Change buff position

--- ShestakUI/Config/Filters/RaidAuraWatch.lua
+++ ShestakUI/Config/Filters/RaidAuraWatch.lua
@@ -26,8 +26,8 @@ if C.raidframe.plugins_aura_watch == true then
 			},
 			PALADIN = {
 				{53563, "TOPRIGHT", {0.7, 0.3, 0.7}},			-- Beacon of Light
-				{20925, "TOPLEFT", {0.9, 0.9, 0.1}},			-- Sacred Shield
-				{114163, "TOPLEFT", {0.9, 0.6, 0.4}},			-- Eternal Flame
+				{20925, "BOTTOMLEFT", {0.9, 0.9, 0.1}},			-- Sacred Shield
+				{114163, "BOTTOMLEFT", {0.9, 0.6, 0.4}},		-- Eternal Flame
 				{1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true},		-- Hand of Protection
 				{1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true},	-- Hand of Freedom
 				{1038, "BOTTOMRIGHT", {0.93, 0.75, 0}, true},	-- Hand of Salvation
