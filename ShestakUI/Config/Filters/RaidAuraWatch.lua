9e4a206d3e91556aa4df5f850fc61df446f8327e
Wetxius <wetxius@gmail.com>
2012-07-21 11:17:14 +0400

    Add new Monk's spells to RaidAuraWatch.

--- ShestakUI/Config/Filters/RaidAuraWatch.lua
+++ ShestakUI/Config/Filters/RaidAuraWatch.lua
@@ -61,6 +61,8 @@ if C.raidframe.plugins_aura_watch == true then
 			},
 			MONK = {
 				{119611, "TOPRIGHT", {0.2, 0.7, 0.7}},			-- Renewing Mist
+				{124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}},		-- Enveloping Mist
+				{124081, "BOTTOMRIGHT", {0.7, 0.4, 0}},			-- Zen Sphere
 				{116849, "LEFT", {0.81, 0.85, 0.1}, true},		-- Life Cocoon
 			},
 			ALL = {
