d5351473b89d51654d42ccc4c00ca95b5ce768ce
Shestak <misha.shestakov@gmail.com>
2012-07-26 22:25:24 +0400

    Fixed monk actionbar pages.

--- ShestakUI/Modules/ActionBars/Bar1.lua
+++ ShestakUI/Modules/ActionBars/Bar1.lua
@@ -9,17 +9,17 @@ bar:ClearAllPoints()
 bar:SetAllPoints(ActionBarAnchor)
 bar:SetFrameStrata("BACKGROUND")
 
-local Page = {
-	["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 8; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
-	["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
-	["PRIEST"] = "[bonusbar:1] 7;",
-	["MONK"] = "[bonusbar:1] 7;",
-	["ROGUE"] = "[bonusbar:1] 7; [form:3] 7;",
-	["WARLOCK"] = "[form:2] 10;",
-	["DEFAULT"] = "[vehicleui:12] 12; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;",
-}
-
 local function GetBar()
+	local spec = GetSpecialization()
+	local Page = {
+		["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 8; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
+		["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
+		["PRIEST"] = "[bonusbar:1] 7;",
+		["MONK"] = "[form:1] "..(spec == 1 and 8 or spec == 2 and 9 or spec == 3 and 7 or 9).."; [form:2] 7;",
+		["ROGUE"] = "[bonusbar:1] 7; [form:3] 7;",
+		["WARLOCK"] = "[form:2] 10;",
+		["DEFAULT"] = "[vehicleui:12] 12; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;",
+	}
 	local condition = Page["DEFAULT"]
 	local class = T.class
 	local page = Page[class]
@@ -37,7 +37,7 @@ bar:RegisterEvent("CURRENCY_DISPLAY_UPDATE")
 bar:RegisterEvent("BAG_UPDATE")
 bar:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 bar:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_LOGIN" then
+	if event == "PLAYER_LOGIN" or event == "ACTIVE_TALENT_GROUP_CHANGED" then
 		local button
 		for i = 1, NUM_ACTIONBAR_BUTTONS do
 			button = _G["ActionButton"..i]
