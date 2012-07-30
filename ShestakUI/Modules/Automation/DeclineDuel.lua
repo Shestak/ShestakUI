861621ac81d0479afb4096a8eea9248788881dd7
Shestak <misha.shestakov@gmail.com>
2012-07-30 19:26:44 +0400

    Added PetBattlesDuel decline.

--- ShestakUI/Modules/Automation/DeclineDuel.lua
+++ ShestakUI/Modules/Automation/DeclineDuel.lua
@@ -6,9 +6,14 @@ if C.automation.decline_duel ~= true then return end
 ----------------------------------------------------------------------------------------
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("DUEL_REQUESTED")
+frame:RegisterEvent("PET_BATTLE_PVP_DUEL_REQUESTED")
 frame:SetScript("OnEvent", function(self, event, name)
+	if event == "DUEL_REQUESTED" then
+		CancelDuel()
+	elseif event == "PET_BATTLE_PVP_DUEL_REQUESTED" then
+		C_PetBattles.CancelPVPDuel()
+	end
 	HideUIPanel(StaticPopup1)
-	CancelDuel()
 	T.InfoTextShow(L_INFO_DUEL..name)
 	print(format("|cffffff00"..L_INFO_DUEL..name.."."))
 end)
