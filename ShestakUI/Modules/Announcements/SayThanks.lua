9ef02487e9c791ea08f0e3e4b06709d068b35d27
Shestak <misha.shestakov@gmail.com>
2012-08-24 07:04:09 +0400

    Added Symbiosis to SayThanks.

--- ShestakUI/Modules/Announcements/SayThanks.lua
+++ ShestakUI/Modules/Announcements/SayThanks.lua
@@ -4,12 +4,12 @@ if C.announcements.says_thanks ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Says thanks for some spells(SaySapped by Bitbyte, modified by m2jest1c)
 ----------------------------------------------------------------------------------------
-local SayThanks = CreateFrame("Frame")
-
 local ST_Buffs = {
-	[29166] = true,	-- Innervate
+	[29166] = true,		-- Innervate
+	[110309] = true,	-- Symbiosis
 }
 
+local SayThanks = CreateFrame("Frame")
 SayThanks:SetScript("OnEvent", function(_, event, _, applied, _, _, buffer, _, _, _, player, _, _, spell, ...)
 	for key, value in pairs(ST_Buffs) do
 		if spell == key and value == true and player == T.name and buffer ~= T.name and applied == "SPELL_AURA_APPLIED" then
