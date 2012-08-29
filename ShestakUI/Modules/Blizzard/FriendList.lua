d837dd218d2aa93b26dc3e0b7d8af2c2db7f3d83
Shestak <misha.shestakov@gmail.com>
2012-08-29 17:30:04 +0400

    Fixed FriendList coloring.

--- ShestakUI/Modules/Blizzard/FriendList.lua
+++ ShestakUI/Modules/Blizzard/FriendList.lua
@@ -274,11 +274,11 @@ local function friendsFrame()
 					end
 				end
 			elseif button.buttonType == FRIENDS_BUTTON_TYPE_BNET then
-				local _, givenName, surname, toonName, toonID, client, isOnline = BNGetFriendInfo(button.id)
+				local _, presenceName, battleTag, _, toonName, toonID, client, isOnline = BNGetFriendInfo(button.id)
 				if isOnline and client == BNET_CLIENT_WOW then
-					local _, toonName, client, _, _, _, _, class, _, zoneName, level, _ = BNGetToonInfo(toonID)
-					if givenName and surname and toonName and class then
-						nameText = format(BATTLENET_NAME_FORMAT, givenName, surname).." "..FRIENDS_WOW_NAME_COLOR_CODE.."("..classColor[class]..classColor[class]..toonName..FRIENDS_WOW_NAME_COLOR_CODE..")"
+					local _, toonName, client, _, _, _, _, class, _, zoneName, level = BNGetToonInfo(toonID)
+					if presenceName and battleTag and toonName and class then
+						nameText = format(BATTLENET_NAME_FORMAT, presenceName, "").." "..FRIENDS_WOW_NAME_COLOR_CODE.."("..classColor[class]..classColor[class]..toonName..FRIENDS_WOW_NAME_COLOR_CODE..")"
 						if zoneName == playerArea then
 							infoText = format("|cff00ff00%s|r", zoneName)
 						end
