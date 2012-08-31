67bb096fba53e8487e0ed554f33fb1368701391e
Shestak <misha.shestakov@gmail.com>
2012-08-31 04:02:16 +0400

    Fixed and cleanup CombustionHelper(not fully ready).

--- ShestakUI/Modules/Skins/CombustionHelper.lua
+++ ShestakUI/Modules/Skins/CombustionHelper.lua
@@ -11,11 +11,13 @@ CBHSkin:SetScript("OnEvent", function(self, event, addon)
 	if not IsAddOnLoaded("CombustionHelper") then return end
 
 	CombustionFrame:SetTemplate("Transparent")
-	LBtrackFrame:SetTemplate("Transparent")
+	CombustionFrame.SetBackdrop = T.dummy
+
+	CombuMBTrackerBorderFrame:SetTemplate("Transparent")
+	CombuMBTrackerBorderFrame.SetBackdrop = T.dummy
 
 	CombuCautscalevar = 1
 	combusettingstable.combuscale = 1
-	combusettingstable.comburefreshmode = false
 	combusettingstable.combuchat = false
 	combusettingstable.combureport = false
 	combusettingstable.combureportpyro = false
@@ -23,16 +25,25 @@ CBHSkin:SetScript("OnEvent", function(self, event, addon)
 	combusettingstable.combureportmunching = false
 	combusettingstable.combureportthreshold = false
 	combusettingstable.combutexturename = "Smooth"
-
+	combusettingstable.combumbtrackertexturename = "Smooth"
+	combusettingstable.combufontname = "Hooge"
+	combusettingstable.combumbtrackerfontname = "Hooge"
 	combusettingstable.bgcolornormal = {unpack(C.media.overlay_color)}
 	combusettingstable.edgecolornormal = {unpack(C.media.border_color)}
 	combusettingstable.bgcolorcombustion = {0, 1, 0, C.media.overlay_color[4]}
 	combusettingstable.bgcolorimpact = {1, 1, 0, C.media.overlay_color[4]}
 
-	--FFBButton:Hide()
-	--PyroButton:Hide()
-	--IgniteButton:Hide()
-	--LBButton:Hide()
+	combumbtrackersettingstable.combumbtrackerscale = 1
+	combumbtrackersettingstable.combumbtrackerchat = false
+	combumbtrackersettingstable.combumbtrackerrefreshmode = false
+	combumbtrackersettingstable.combumbtrackerfontname = "Hooge"
+	combumbtrackersettingstable.combumbtrackertexturename = "Smooth"
+	combumbtrackersettingstable.bgcolornormal = {unpack(C.media.overlay_color)}
+	combumbtrackersettingstable.edgecolornormal = {unpack(C.media.border_color)}
+
+	--RegPyroButton:Hide()
+	--RegIgniteButton:Hide()
+	--RegLBButton:Hide()
 
 	if CombuCautenablevar == true then
 		CombuCautTimerText:SetFont(C.font.stylization_font, (C.font.stylization_font_size / combusettingstable.combuscale) * 2, C.font.stylization_font_style)
@@ -44,95 +55,13 @@ CBHSkin:SetScript("OnEvent", function(self, event, addon)
 		CombuCautIcon:Point("BOTTOMRIGHT", -2, 2)
 	end
 
-	if combusettingstable.combubartimers == true then
-		combusettingstable.combubarwidth = 55
-
-		for _, label in ipairs({LBLabel, IgniteLabel, PyroLabel, FFBLabel, LBTextFrameLabel, IgnTextFrameLabel, PyroTextFrameLabel, FFBTextFrameLabel, CritTypeFrameLabel, CritTextFrameLabel, StatusTextFrameLabel, LBtrack1, LBtrack2, LBtrack3, LBtrack4, LBtrack5, LBtrack1Timer, LBtrack2Timer, LBtrack3Timer, LBtrack4Timer, LBtrack5Timer}) do
-			label:SetFont(C.font.stylization_font, C.font.stylization_font_size / combusettingstable.combuscale, C.font.stylization_font_style)
-			label:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
-		end
-
-		for _, label in ipairs({LBLabel, IgniteLabel, PyroLabel, FFBLabel}) do
-			label:SetWidth(combusettingstable.combubarwidth + 10)
-		end
-
-		for _, label in ipairs({LBtrack1Timer, LBtrack2Timer, LBtrack3Timer, LBtrack4Timer, LBtrack5Timer}) do
-			label:SetWidth(combusettingstable.combubarwidth - 15)
-		end
-
-		for _, bar in ipairs({LBbar, Ignbar, Pyrobar, FFBbar, Critbar, Combubar}) do
-			bar:SetHeight(C.font.stylization_font_size)
-		end
-
-		CombustionFrame:SetWidth((LBLabel:GetWidth() * 2) + 42)
-		StatusTextFrameLabel:SetWidth(CombustionFrame:GetWidth() - 10)
-		Combubar:SetWidth(CombustionFrame:GetWidth() - 10)
-
-		for _, label in ipairs({CritTypeFrameLabel, CritTextFrameLabel}) do
-			label:SetWidth(CombustionFrame:GetWidth() - 12)
-		end
-
-		for _, bar in ipairs({LBtrack1Bar, LBtrack2Bar, LBtrack3Bar, LBtrack4Bar, LBtrack5Bar}) do
-			bar:SetHeight(C.font.stylization_font_size)
-			bar:SetWidth(LBtrackFrame:GetWidth() - 15)
-		end
-	else
-		combusettingstable.combubarwidth = 132
-
-		CombustionFrame:SetWidth(132)
-		CombustionFrame:SetHeight(77)
-		LBtrackFrame:SetWidth(132)
-
-		for _, label in ipairs({LBLabel, IgniteLabel, PyroLabel, FFBLabel, LBTextFrameLabel, IgnTextFrameLabel, PyroTextFrameLabel, FFBTextFrameLabel, StatusTextFrameLabel, LBtrack1, LBtrack2, LBtrack3, LBtrack4, LBtrack5, LBtrack1Timer, LBtrack2Timer, LBtrack3Timer, LBtrack4Timer, LBtrack5Timer}) do
-			label:SetTextHeight(C.font.stylization_font_size + 5)
-			label:SetHeight(C.font.stylization_font_size + 5)
-			label:SetFont(C.font.stylization_font, C.font.stylization_font_size / combusettingstable.combuscale, C.font.stylization_font_style)
-			label:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
-		end
-
-		for _, label in ipairs({LBLabel, IgniteLabel, PyroLabel, FFBLabel}) do
-			label:SetWidth(85)
-		end
-
-		for _, label in ipairs({LBtrack1Timer, LBtrack2Timer, LBtrack3Timer, LBtrack4Timer, LBtrack5Timer}) do
-			label:SetWidth(62)
-		end
-
-		for _, label in ipairs({CritTypeFrameLabel, CritTextFrameLabel}) do
-			label:SetWidth(118)
-			label:SetTextHeight(C.font.stylization_font_size + 5)
-			label:SetHeight(C.font.stylization_font_size + 5)
-			label:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
-			label:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
-		end
-
-		for _, bar in ipairs({LBbar, Ignbar, Pyrobar, FFBbar, Critbar, Combubar}) do
-			bar:SetHeight(C.font.stylization_font_size + 5)
-			bar:SetWidth(118)
-		end
-
-		for _, bar in ipairs({LBtrack1Bar, LBtrack2Bar, LBtrack3Bar, LBtrack4Bar, LBtrack5Bar}) do
-			bar:SetHeight(C.font.stylization_font_size)
-			bar:SetWidth(LBtrackFrame:GetWidth() - 15)
-		end
-
-		StatusTextFrameLabel:SetWidth(CombustionFrame:GetWidth() - 10)
-		Combubar:SetWidth(CombustionFrame:GetWidth() - 10)
+	for _, label in ipairs({RegLBLabel, RegIgniteLabel, RegPyroLabel, RegLBTextFrameLabel, RegIgnTextFrameLabel, RegPyroTextFrameLabel, RegStatusTextFrameLabel}) do
+		label:SetFont(C.font.stylization_font, C.font.stylization_font_size / combusettingstable.combuscale, C.font.stylization_font_style)
+		label:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
 	end
 
-	if combusettingstable.combulbup == true then
-		LBtrackFrame:SetPoint("BOTTOMLEFT", CombustionFrame, "TOPLEFT", 0, 3)
-		LBtrackFrame:SetPoint("BOTTOMRIGHT", CombustionFrame, "TOPRIGHT", 0, 3)
-	elseif combusettingstable.combulbdown == true then
-		LBtrackFrame:SetPoint("TOPLEFT", CombustionFrame, "BOTTOMLEFT", 0, -3)
-		LBtrackFrame:SetPoint("TOPRIGHT", CombustionFrame, "BOTTOMRIGHT", 0, -3)
-	elseif combusettingstable.combulbright == true then
-		LBtrackFrame:SetPoint("TOPLEFT", CombustionFrame, "TOPRIGHT", 3, 0)
-	elseif combusettingstable.combulbleft == true then
-		LBtrackFrame:SetPoint("TOPRIGHT", CombustionFrame, "TOPLEFT", -3, 0)
-	else
-		LBtrackFrame:SetPoint("BOTTOMLEFT", CombustionFrame, "TOPLEFT", 0, 3)
-		LBtrackFrame:SetPoint("BOTTOMRIGHT", CombustionFrame, "TOPRIGHT", 0, 3)
+	for _, bar in ipairs({RegLBbar, RegIgnbar, RegPyrobar, RegCombubar}) do
+		bar:SetHeight(C.font.stylization_font_size)
 	end
 
 	-- Options
@@ -144,37 +73,27 @@ CBHSkin:SetScript("OnEvent", function(self, event, addon)
 
 	local buttons = {
 		"CombuvalueokButton",
-		"CombuIgniteokButton",
 		"CombutimerokButton",
 		"ComburesetButton",
 		"CombuCautAnnounceEditBoxokButton",
 		"CombuCautAnnounceAltEditBoxokButton",
-		"CombuCautresetButton"
+		"CombuCautresetButton",
+		"CombuMBTrackerresetButton"
 	}
 
 	for i = 1, getn(buttons) do
-		local button = _G[buttons[i]]
-		if button then
-			button:SkinButton()
-		end
+		_G[buttons[i]]:SkinButton()
 	end
 
 	local checkboxes = {
 		"CombulockButton",
-		"CombucritButton",
-		"ComburefreshButton",
 		"CombureportButton",
 		"Combureportthreshold",
-		"CombuIgnitePredictButton",
-		"CombuffbButton",
 		"CombuMunchingButton",
-		"CombuFlamestrikeButton",
 		"ComburefreshpyroButton",
 		"CombuimpactButton",
 		"CombutrackerButton",
 		"CombuchatButton",
-		"CombuLBtrackerButton",
-		"CombuLBtargetButton",
 		"CombuThresholdSoundButton",
 		"CombuBarButton",
 		"CombuTileButton",
@@ -186,31 +105,17 @@ CBHSkin:SetScript("OnEvent", function(self, event, addon)
 		"CombuCautAnnounceRaidButton",
 		"CombuCautAnnounceSayButton",
 		"CombuCautAnnounceYellButton",
-		"CombuCautAnnounceAltButton"
+		"CombuCautAnnounceAltButton",
+		"CombuMBTrackertargetButton",
+		"CombuMBTrackerFlamestrikeButton",
+		"CombuMBTrackerlockButton",
+		"CombuMBTrackerButton",
+		"CombuMBTrackerchatButton",
+		"CombuMBTrackerrefreshButton",
+		"CombuMBTrackerTileButton"
 	}
 
 	for i = 1, getn(checkboxes) do
-		local checkbox = _G[checkboxes[i]]
-		if checkbox then
-			T.SkinCheckBox(checkbox)
-		end
-	end
-
-	local dropdown = {
-		"LBtrackerDropDown",
-		"CombuAutohideDropDown",
-		"CombuLanguageDropDown",
-		"CombuTextureDropDown",
-		"CombuFontDropDown",
-		"CombuSoundDropDown",
-		"CombuBorderDropDown",
-		"CombuBackgroundDropDown"
-	}
-
-	for i = 1, getn(dropdown) do
-		local frame = _G[dropdown[i]]
-		if frame then
-			--T.SkinDropDownBox(frame)
-		end
+		T.SkinCheckBox(_G[checkboxes[i]])
 	end
 end)
