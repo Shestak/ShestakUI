5740859e43df82750a15b03e274e877c02b84d28
Shestak <misha.shestakov@gmail.com>
2012-08-31 07:36:39 +0400

    Fixed telltarget slash command.

--- ShestakUI/Modules/Chat/TellTarget.lua
+++ ShestakUI/Modules/Chat/TellTarget.lua
@@ -8,13 +8,13 @@ for i = 1, NUM_CHAT_WINDOWS do
 	local editbox = _G["ChatFrame"..i.."EditBox"]
 	editbox:HookScript("OnTextChanged", function(self)
 		local text = self:GetText()
-		if text:len() < 5 then
-			if text:sub(1, 4) == "/tt " or text:sub(1, 4) == "/ее " then
+		if text:len() < 7 then
+			if text:sub(1, 4) == "/tt " or text:sub(1, 6) == "/ее " then
 				local unitname, realm = UnitName("target")
 				if unitname then
 					if unitname then unitname = gsub(unitname, " ", "") end
 					if unitname and not UnitIsSameServer("player", "target") then
-						unitname = unitname .. "-" .. gsub(realm, " ", "")
+						unitname = unitname.."-"..gsub(realm, " ", "")
 					end
 					ChatFrame_SendTell((unitname), ChatFrame1)
 				end
@@ -28,5 +28,4 @@ SlashCmdList.TELLTARGET = function(msg)
 	SendChatMessage(msg, "WHISPER")
 end
 SLASH_TELLTARGET1 = "/tt"
-SLASH_TELLTARGET2 = "/telltarget"
-SLASH_TELLTARGET3 = "/ее"
+SLASH_TELLTARGET2 = "/ее"
