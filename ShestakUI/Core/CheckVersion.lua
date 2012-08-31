452a4a06425b5f874f3ba823479af8c1b798fa10
Shestak <misha.shestakov@gmail.com>
2012-08-31 22:02:51 +0400

    Fixed chat error.

--- ShestakUI/Core/CheckVersion.lua
+++ ShestakUI/Core/CheckVersion.lua
@@ -6,6 +6,7 @@
 local check = function(self, event, prefix, message, channel, sender)
 	if event == "CHAT_MSG_ADDON" then
 		if prefix ~= "ShestakUIVersion" or sender == T.name then return end
+		if tonumber(message) == nil then return end
 		if tonumber(message) > tonumber(T.version) then
 			print("|cffad2424"..L_MISC_UI_OUTDATED.."|r")
 			self:UnregisterEvent("CHAT_MSG_ADDON")
@@ -13,7 +14,7 @@ local check = function(self, event, prefix, message, channel, sender)
 	else
 		if UnitInBattleground("player") and UnitInBattleground("player") > 0 then
 			SendAddonMessage("ShestakUIVersion", tonumber(T.version), "BATTLEGROUND")
-		elseif UnitInRaid("player") then
+		elseif UnitInRaid("player") and GetNumGroupMembers() > 5 then
 			SendAddonMessage("ShestakUIVersion", tonumber(T.version), "RAID")
 		elseif UnitInParty("player") then
 			SendAddonMessage("ShestakUIVersion", tonumber(T.version), "PARTY")
