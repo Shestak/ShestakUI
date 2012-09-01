5d64b49f62ddcaadad7a397e93ec984020f785ad
Shestak <misha.shestakov@gmail.com>
2012-09-01 23:19:15 +0400

    Updated oGlow.

--- ShestakUI/Libs/oGlow/Pipes/Inspect.lua
+++ ShestakUI/Libs/oGlow/Pipes/Inspect.lua
@@ -3,7 +3,7 @@ if (select(4, GetAddOnInfo("Fizzle"))) then return end
 local _E
 local slots = {
 	"Head", "Neck", "Shoulder", "Shirt", "Chest", "Waist", "Legs", "Feet", "Wrist", "Hands",
-	"Finger0", "Finger1", "Trinket0", "Trinket1", "Back", "MainHand", "SecondaryHand", "Tabard"
+	"Finger0", "Finger1", "Trinket0", "Trinket1", "Back", "MainHand", "SecondaryHand", [19] = "Tabard"
 }
 
 local _MISSING = {}
