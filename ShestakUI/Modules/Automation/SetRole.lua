b3f307a33fa6beb6c78d6e6aee4a5abfa2d9b8ca
Shestak <misha.shestakov@gmail.com>
2012-09-02 12:17:01 +0400

    Updated auto set role(use Blizzard API).

--- ShestakUI/Modules/Automation/SetRole.lua
+++ ShestakUI/Modules/Automation/SetRole.lua
@@ -4,33 +4,25 @@ if C.automation.auto_role ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Automatically sets your role(Auto role setter by iSpawnAtHome)
 ----------------------------------------------------------------------------------------
-local ATT = GetActiveSpecGroup(false, false)
-local PTT = GetSpecialization(false, false, ATT)
-local frame = CreateFrame("Frame")
-
 local function SetRole()
-	ATT = GetActiveSpecGroup(false, false)
-	PTT = GetSpecialization(false, false, ATT)
-
+	local spec = GetSpecialization()
 	if T.level >= 10 then
-		if PTT == nil then
+		if spec == nil then
 			UnitSetRole("player", "No Role")
-		elseif PTT ~= nil then
-			if GetNumSubgroupMembers() > 0 or GetNumGroupMembers() > 0 then
-				if T.Role == "Tank" then
-					UnitSetRole("player", "TANK")
-				elseif T.Role == "Healer" then
-					UnitSetRole("player", "HEALER")
-				elseif T.Role == "Melee" or T.Role == "Caster" then
-					UnitSetRole("player", "DAMAGER")
-				end
+		elseif spec ~= nil then
+			if GetNumGroupMembers() > 0 then
+				local role = GetSpecializationRole(spec)
+				UnitSetRole("player", role)
 			end
 		end
 	end
 end
 
+local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_ENTERING_BATTLEGROUND")
 frame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 frame:RegisterEvent("GROUP_ROSTER_UPDATE")
 frame:RegisterEvent("PLAYER_TALENT_UPDATE")
 frame:SetScript("OnEvent", SetRole)
+
+RolePollPopup:SetScript("OnShow", function() RolePollPopupAcceptButton:Click() end)
