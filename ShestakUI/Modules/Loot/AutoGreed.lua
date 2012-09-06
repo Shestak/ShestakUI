c117b6aa36e3bd9471819e57eb78efdc93b3e2c3
Shestak <misha.shestakov@gmail.com>
2012-09-06 05:06:41 +0400

    Fixed group loot for me.

--- ShestakUI/Modules/Loot/AutoGreed.lua
+++ ShestakUI/Modules/Loot/AutoGreed.lua
@@ -8,7 +8,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("START_LOOT_ROLL")
 frame:SetScript("OnEvent", function(self, event, id)
 	local name = select(2, GetLootRollItemInfo(id))
-	if T.level ~= MAX_PLAYER_LEVEL and T.author ~= true then return end
+	if T.level ~= MAX_PLAYER_LEVEL then return end
 	if id and select(4, GetLootRollItemInfo(id)) == 2 and not select(5, GetLootRollItemInfo(id)) then
 		for i in pairs(T.NeedLoot) do
 			if name == select(1, GetItemInfo(T.NeedLoot[i])) and RollOnLoot(id, 1) then
