14d89cf8df9fd5c5e41c71853dbc8f098d24add7
Oz <oz.asudem@gmail.com>
2012-09-23 17:58:24 +0200

    Cleanup (btw, second actionbar removed).

--- ShestakUI_Extra/Config/Profiles.lua
+++ ShestakUI_Extra/Config/Profiles.lua
@@ -64,8 +64,8 @@ end
 ----------------------------------------------------------------------------------------
 --	Nagiko@Pozzo dell'Eternità Config
 ----------------------------------------------------------------------------------------
-if T.name == "Nagiko" and T.realm == "Pozzo dell'Eternità" then
-	C["extra_position"].extra_bar = {"CENTER", oUF_Target, "CENTER", 56, 251}
+if T.name == "Ozzolo" and T.realm == "Pozzo dell'Eternità" then
+	C["extra_position"].extra_bar = {"CENTER", oUF_Target, "CENTER", -36, 128}
 	C["extra_bar"].enable = true
 end
 
