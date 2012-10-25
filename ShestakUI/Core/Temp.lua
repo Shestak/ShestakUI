499408792eab183f7bb7c39e2c7246087ed4bbc0
m2jest1c <artem.fufaldin@gmail.com>
2012-10-25 17:20:46 +0600

    some changes for me (announcement of Void Shift)

--- ShestakUI/Core/Temp.lua
+++ ShestakUI/Core/Temp.lua
@@ -3,3 +3,25 @@
 ----------------------------------------------------------------------------------------
 --	Temporary stuff
 ----------------------------------------------------------------------------------------
+if T.coauthor ~= true then return end
+
+local function GetChat()
+	if IsInRaid() then
+		if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
+			return "RAID_WARNING"
+		else
+			return "RAID"
+		end
+	elseif IsInGroup() then
+		return "PARTY"
+	end
+	return "SAY"
+end
+
+local frame = CreateFrame("FRAME")
+frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+frame:SetScript("OnEvent", function(_, event, _, _, _, _, player, _, _, _, target, _, _, spell)
+if event == "COMBAT_LOG_EVENT_UNFILTERED" and player == T.name and spell == 108968 then
+	SendChatMessage(string.format("Внимание! Обмен здоровьем с %s! %s", target, GetSpellLink(spell)), GetChat())
+end
+end)
