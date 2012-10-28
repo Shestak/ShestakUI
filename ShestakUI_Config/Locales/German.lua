33e848fe243aac67d2c3c03570f7572b9a69d333
Shestak <misha.shestakov@gmail.com>
2012-10-29 00:46:41 +0400

    Updated locales for new option.

--- ShestakUI_Config/Locales/German.lua
+++ ShestakUI_Config/Locales/German.lua
@@ -364,6 +364,7 @@ L_GUI_UF_PLUGINS_HOLY_BAR = "Aktivere Heilige Kraft Leiste"
 L_GUI_UF_PLUGINS_SHARD_BAR = "Aktiviere Splitter Leiste"
 L_GUI_UF_PLUGINS_RUNE_BAR = "Runenleiste"
 L_GUI_UF_PLUGINS_TOTEM_BAR = "Totemleiste"
+L_GUI_UF_PLUGINS_RANGE_BAR = "Enable Range bar" -- Needs review
 
 -- Raid Frames options
 L_GUI_UF_AGGRO_BORDER = "Rahmen bei Bedrohung"
