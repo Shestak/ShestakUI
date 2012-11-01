37aa73a8a7edcdc0741bc5686ac840d932cbb153
m2jest1c <artem.fufaldin@gmail.com>
2012-11-02 03:17:23 +0600

    some changes for me (test)

--- DarkShestakUI/Core/Temp.lua
+++ DarkShestakUI/Core/Temp.lua
@@ -20,8 +20,17 @@ end
 
 local frame = CreateFrame("FRAME")
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-frame:SetScript("OnEvent", function(_, event, _, _, _, _, player, _, _, _, target, _, _, spell, ...)
-if event == "COMBAT_LOG_EVENT_UNFILTERED" and player == T.name and spell == 108968 then
-	SendChatMessage(string.format("Внимание! Обмен здоровьем с %s! %s", target, GetSpellLink(spell)), GetChat())
-end
+frame:SetScript("OnEvent", function(_, event, _, subEvent, _, _, player, _, _, _, target, _, _, spell, ...)
+	if event == "COMBAT_LOG_EVENT_UNFILTERED" then 
+		if player == T.name and spell == 108968 then
+			SendChatMessage(string.format("Внимание! Обмен здоровьем с %s! %s", target, GetSpellLink(spell)), GetChat())
+		end
+		if subEvent == "SPELL_CREATE" then
+			if spell == 126499 then 
+			SendChatMessage(string.format("%s готовит %s, кушайте, товарищи хилеры!", player, GetSpellLink(spell)), GetChat())
+			elseif spell == 105193 then
+			SendChatMessage(string.format("%s готовит %s, кушайте, товарищи рейдеры!", player, GetSpellLink(spell)), GetChat())
+			end
+		end
+	end
 end)
