aba342cf5e2ff0c93fe18faf01ff1a34e2db5cf2
Shestak <misha.shestakov@gmail.com>
2012-11-17 16:37:26 +0400

    Changes texture for switch button.

--- ShestakUI/Modules/Maps/MiniMapButtons.lua
+++ ShestakUI/Modules/Maps/MiniMapButtons.lua
@@ -5,8 +5,7 @@ if C.minimap.enable ~= true then return end
 --	Switch layout mouseover button on minimap
 ----------------------------------------------------------------------------------------
 local switch = CreateFrame("Button", "SwitchLayout", UIParent)
-switch:SetTemplate("Transparent")
-switch:SetBackdropBorderColor(T.color.r, T.color.g, T.color.b)
+switch:SetTemplate("ClassColor")
 if C.actionbar.toggle_mode == true then
 	switch:SetPoint("TOPLEFT", Minimap, "TOPRIGHT", 3, -18)
 else
@@ -16,8 +15,7 @@ switch:SetSize(19, 19)
 switch:SetAlpha(0)
 
 switch.t = switch:CreateTexture(nil, "OVERLAY")
-switch.t:SetTexture("Interface\\Icons\\achievement_guildperk_quick and dead")
-switch.t:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+switch.t:SetTexture("Interface\\LFGFrame\\LFGROLE")
 switch.t:SetPoint("TOPLEFT", switch, 2, -2)
 switch.t:SetPoint("BOTTOMRIGHT", switch, -2, 2)
 
@@ -43,6 +41,21 @@ end)
 
 switch:SetScript("OnLeave", function()
 	switch:FadeOut()
+end)
+
+switch:RegisterEvent("PLAYER_LOGIN")
+switch:SetScript("OnEvent", function(self)
+	if SavedOptions.RaidLayout == "DPS" then
+		switch.t:SetTexCoord(0.25, 0.5, 0, 1)
+	elseif SavedOptions.RaidLayout == "HEAL" then
+		switch.t:SetTexCoord(0.75, 1, 0, 1)
+	elseif SavedOptions.RaidLayout == "NONE" then
+		switch.t:SetTexture("Interface\\ChatFrame\\UI-ChatIcon-Blizz")
+		switch.t:SetTexCoord(0.2, 0.8, -0.1, 1.1)
+	elseif SavedOptions.RaidLayout == "UNKNOWN" then
+		switch.t:SetTexture("Interface\\InventoryItems\\WoWUnknownItem01")
+		switch.t:SetTexCoord(0.2, 0.8, 0.2, 0.8)
+	end
 end)
 
 ----------------------------------------------------------------------------------------
