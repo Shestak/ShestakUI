6b1b7b16e43c8f38331e0599e5779355aa5d331f
Oz <oz.asudem@gmail.com>
2012-11-21 11:50:12 +0100

    Changed a default setting.

--- ShestakUI/Config/Settings.lua
+++ ShestakUI/Config/Settings.lua
@@ -362,7 +362,7 @@ C["nameplate"] = {
 	["good_color"] = {0.2, 0.8, 0.2},			-- Good threat color
 	["near_color"] = {1, 1, 0},					-- Near threat color
 	["bad_color"] = {1, 0, 0},					-- Bad threat color
-	["track_auras"] = false,					-- Show debuffs
+	["track_auras"] = true,						-- Show debuffs
 	["auras_size"] = 22,						-- Debuffs size
 	["healer_icon"] = true,						-- Show icon above enemy healers nameplate in battlegrounds
 }
