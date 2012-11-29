59ba218f0039cfca13cae56306091cbb9c52eaaf
Shestak <misha.shestakov@gmail.com>
2012-11-29 20:57:33 +0400

    Fixed raid icons and debuffs on nameplates.

--- ShestakUI/Modules/Blizzard/Nameplates.lua
+++ ShestakUI/Modules/Blizzard/Nameplates.lua
@@ -4,9 +4,11 @@ if C.nameplate.enable ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Based on dNameplates(by Dawn, editor Elv22)
 ----------------------------------------------------------------------------------------
-local numChildren = -1
-local frames = {}
+local frames, numChildren, select = {}, -1, select
 local noscalemult = T.mult * C.general.uiscale
+local goodR, goodG, goodB = unpack(C.nameplate.good_color)
+local badR, badG, badB = unpack(C.nameplate.bad_color)
+local transitionR, transitionG, transitionB = unpack(C.nameplate.near_color)
 
 local NamePlates = CreateFrame("Frame", nil, UIParent)
 NamePlates:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
@@ -14,18 +16,18 @@ if C.nameplate.track_auras == true then
 	NamePlates:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 end
 
-local Abbrev = function(name)
+local function Abbrev(name)
 	local newname = (string.len(name) > 18) and string.gsub(name, "%s?(.[\128-\191]*)%S+%s", "%1. ") or name
 	return T.UTF(newname, 18, false)
 end
 
-local function QueueObject(parent, object)
-	parent.queue = parent.queue or {}
-	parent.queue[object] = true
+local function QueueObject(frame, object)
+	frame.queue = frame.queue or {}
+	frame.queue[object] = true
 end
 
-local function HideObjects(parent)
-	for object in pairs(parent.queue) do
+local function HideObjects(frame)
+	for object in pairs(frame.queue) do
 		if object:GetObjectType() == "Texture" then
 			object:SetTexture(nil)
 		elseif object:GetObjectType() == "FontString" then
@@ -37,55 +39,56 @@ local function HideObjects(parent)
 end
 
 -- Create a fake backdrop frame using textures
-local function CreateVirtualFrame(parent, point)
-	if point == nil then point = parent end
+local function CreateVirtualFrame(frame, point)
+	if point == nil then point = frame end
 	if point.backdrop then return end
 
-	parent.backdrop = parent:CreateTexture(nil, "BORDER")
-	parent.backdrop:SetDrawLayer("BORDER", -8)
-	parent.backdrop:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 3, noscalemult * 3)
-	parent.backdrop:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", noscalemult * 3, -noscalemult * 3)
-	parent.backdrop:SetTexture(unpack(C.media.backdrop_color))
+	frame.backdrop = frame:CreateTexture(nil, "BORDER")
+	frame.backdrop:SetDrawLayer("BORDER", -8)
+	frame.backdrop:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 3, noscalemult * 3)
+	frame.backdrop:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", noscalemult * 3, -noscalemult * 3)
+	frame.backdrop:SetTexture(unpack(C.media.backdrop_color))
 
-	parent.bordertop = parent:CreateTexture(nil, "BORDER")
-	parent.bordertop:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 2, noscalemult * 2)
-	parent.bordertop:SetPoint("TOPRIGHT", point, "TOPRIGHT", noscalemult * 2, noscalemult * 2)
-	parent.bordertop:SetHeight(noscalemult)
-	parent.bordertop:SetTexture(unpack(C.media.border_color))
-	parent.bordertop:SetDrawLayer("BORDER", -7)
+	frame.bordertop = frame:CreateTexture(nil, "BORDER")
+	frame.bordertop:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 2, noscalemult * 2)
+	frame.bordertop:SetPoint("TOPRIGHT", point, "TOPRIGHT", noscalemult * 2, noscalemult * 2)
+	frame.bordertop:SetHeight(noscalemult)
+	frame.bordertop:SetTexture(unpack(C.media.border_color))
+	frame.bordertop:SetDrawLayer("BORDER", -7)
 
-	parent.borderbottom = parent:CreateTexture(nil, "BORDER")
-	parent.borderbottom:SetPoint("BOTTOMLEFT", point, "BOTTOMLEFT", -noscalemult * 2, -noscalemult * 2)
-	parent.borderbottom:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", noscalemult * 2, -noscalemult * 2)
-	parent.borderbottom:SetHeight(noscalemult)
-	parent.borderbottom:SetTexture(unpack(C.media.border_color))
-	parent.borderbottom:SetDrawLayer("BORDER", -7)
+	frame.borderbottom = frame:CreateTexture(nil, "BORDER")
+	frame.borderbottom:SetPoint("BOTTOMLEFT", point, "BOTTOMLEFT", -noscalemult * 2, -noscalemult * 2)
+	frame.borderbottom:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", noscalemult * 2, -noscalemult * 2)
+	frame.borderbottom:SetHeight(noscalemult)
+	frame.borderbottom:SetTexture(unpack(C.media.border_color))
+	frame.borderbottom:SetDrawLayer("BORDER", -7)
 
-	parent.borderleft = parent:CreateTexture(nil, "BORDER")
-	parent.borderleft:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 2, noscalemult * 2)
-	parent.borderleft:SetPoint("BOTTOMLEFT", point, "BOTTOMLEFT", noscalemult * 2, -noscalemult * 2)
-	parent.borderleft:SetWidth(noscalemult)
-	parent.borderleft:SetTexture(unpack(C.media.border_color))
-	parent.borderleft:SetDrawLayer("BORDER", -7)
+	frame.borderleft = frame:CreateTexture(nil, "BORDER")
+	frame.borderleft:SetPoint("TOPLEFT", point, "TOPLEFT", -noscalemult * 2, noscalemult * 2)
+	frame.borderleft:SetPoint("BOTTOMLEFT", point, "BOTTOMLEFT", noscalemult * 2, -noscalemult * 2)
+	frame.borderleft:SetWidth(noscalemult)
+	frame.borderleft:SetTexture(unpack(C.media.border_color))
+	frame.borderleft:SetDrawLayer("BORDER", -7)
 
-	parent.borderright = parent:CreateTexture(nil, "BORDER")
-	parent.borderright:SetPoint("TOPRIGHT", point, "TOPRIGHT", noscalemult * 2, noscalemult * 2)
-	parent.borderright:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", -noscalemult * 2, -noscalemult * 2)
-	parent.borderright:SetWidth(noscalemult)
-	parent.borderright:SetTexture(unpack(C.media.border_color))
-	parent.borderright:SetDrawLayer("BORDER", -7)
+	frame.borderright = frame:CreateTexture(nil, "BORDER")
+	frame.borderright:SetPoint("TOPRIGHT", point, "TOPRIGHT", noscalemult * 2, noscalemult * 2)
+	frame.borderright:SetPoint("BOTTOMRIGHT", point, "BOTTOMRIGHT", -noscalemult * 2, -noscalemult * 2)
+	frame.borderright:SetWidth(noscalemult)
+	frame.borderright:SetTexture(unpack(C.media.border_color))
+	frame.borderright:SetDrawLayer("BORDER", -7)
 end
 
-local function SetVirtualBorder(parent, r, g, b)
-	parent.bordertop:SetTexture(r, g, b)
-	parent.borderbottom:SetTexture(r, g, b)
-	parent.borderleft:SetTexture(r, g, b)
-	parent.borderright:SetTexture(r, g, b)
+local function SetVirtualBorder(frame, r, g, b)
+	frame.bordertop:SetTexture(r, g, b)
+	frame.borderbottom:SetTexture(r, g, b)
+	frame.borderleft:SetTexture(r, g, b)
+	frame.borderright:SetTexture(r, g, b)
 end
 
 -- Create aura icons
-local function CreateAuraIcon(parent)
-	local button = CreateFrame("Frame", nil, parent)
+local function CreateAuraIcon(frame)
+	local button = CreateFrame("Frame", nil, frame)
+	button:SetParent(frame.hp)
 	button:SetWidth(C.nameplate.auras_size)
 	button:SetHeight(C.nameplate.auras_size)
 
@@ -238,8 +241,9 @@ end
 -- Color Nameplate
 local function Colorize(frame)
 	local r, g, b = frame.healthOriginal:GetStatusBarColor()
+	local texcoord = {0, 0, 0, 0}
 
-	for class, color in pairs(RAID_CLASS_COLORS) do
+	for class, _ in pairs(RAID_CLASS_COLORS) do
 		local r, g, b = floor(r * 100 + 0.5) / 100, floor(g * 100 + 0.5) / 100, floor(b * 100 + 0.5) / 100
 		if class == "MONK" then
 			b = b - 0.01
@@ -248,7 +252,7 @@ local function Colorize(frame)
 			frame.hasClass = true
 			frame.isFriendly = false
 			if C.nameplate.class_icons == true then
-				local texcoord = CLASS_BUTTONS[class]
+				texcoord = CLASS_BUTTONS[class]
 				frame.class.Glow:Show()
 				frame.class:SetTexCoord(texcoord[1], texcoord[2], texcoord[3], texcoord[4])
 			end
@@ -257,34 +261,28 @@ local function Colorize(frame)
 		end
 	end
 
-	frame.isTagged = nil
+	frame.isTagged = false
+	frame.hasClass = false
 
 	if r + b + b > 2 then	-- Tapped
 		r, g, b = 0.6, 0.6, 0.6
 		frame.isFriendly = false
 		frame.isTagged = true
-		texcoord = {0, 0, 0, 0}
 	elseif g + b == 0 then	-- Hostile
 		r, g, b = unpack(T.oUF_colors.reaction[1])
 		frame.isFriendly = false
-		texcoord = {0, 0, 0, 0}
 	elseif r + b == 0 then	-- Friendly npc
 		r, g, b = unpack(T.oUF_colors.power["MANA"])
 		frame.isFriendly = true
-		texcoord = {0, 0, 0, 0}
 	elseif r + g > 1.95 then	-- Neutral
 		r, g, b = unpack(T.oUF_colors.reaction[4])
 		frame.isFriendly = false
-		texcoord = {0, 0, 0, 0}
 	elseif r + g == 0 then	-- Friendly player
 		r, g, b = unpack(T.oUF_colors.reaction[5])
 		frame.isFriendly = true
-		texcoord = {0, 0, 0, 0}
 	else	-- Enemy player
 		frame.isFriendly = false
-		texcoord = {0, 0, 0, 0}
 	end
-	frame.hasClass = false
 
 	if C.nameplate.class_icons == true then
 		if frame.hasClass == true then
@@ -301,8 +299,7 @@ end
 
 -- HealthBar OnShow, use this to set variables for the nameplate
 local function UpdateObjects(frame)
-	local frame = frame:GetParent()
-	local r, g, b = frame.hp:GetStatusBarColor()
+	frame = frame:GetParent()
 
 	-- Set scale
 	while frame.hp:GetEffectiveScale() < 1 do
@@ -325,6 +322,7 @@ local function UpdateObjects(frame)
 	frame.hp.rcolor, frame.hp.gcolor, frame.hp.bcolor = frame.hp:GetStatusBarColor()
 	frame.hp.hpbg:SetTexture(frame.hp.rcolor, frame.hp.gcolor, frame.hp.bcolor, 0.25)
 	SetVirtualBorder(frame.hp, unpack(C.media.border_color))
+
 	if C.nameplate.enhance_threat == true then
 		frame.hp.name:SetTextColor(frame.hp.rcolor, frame.hp.gcolor, frame.hp.bcolor)
 	end
@@ -354,21 +352,6 @@ local function UpdateObjects(frame)
 	else
 		frame.hp.level:SetText(level..(elite and "+" or ""))
 		frame.hp.level:Show()
-	end
-
-	frame.overlay:ClearAllPoints()
-	frame.overlay:SetAllPoints(frame.hp)
-
-	-- Aura tracking
-	if C.nameplate.track_auras == true then
-		if frame.icons then return end
-		frame.icons = CreateFrame("Frame", nil, frame)
-		frame.icons:SetPoint("BOTTOMRIGHT", frame.hp, "TOPRIGHT", 0, C.font.nameplates_font_size + 7)
-		frame.icons:SetWidth(20 + C.nameplate.width)
-		frame.icons:SetHeight(C.nameplate.auras_size)
-		frame.icons:SetFrameLevel(frame.hp:GetFrameLevel() + 2)
-		frame:RegisterEvent("UNIT_AURA")
-		frame:HookScript("OnEvent", OnAura)
 	end
 
 	HideObjects(frame)
@@ -484,16 +467,35 @@ local function SkinObjects(frame, nameFrame)
 	cb:HookScript("OnValueChanged", OnValueChanged)
 	frame.cb = cb
 
-	-- Highlight
-	overlay:SetTexture(1, 1, 1, 0.15)
-	overlay:SetAllPoints(hp)
-	frame.overlay = overlay
+	-- Aura tracking
+	if C.nameplate.track_auras == true then
+		if not frame.icons then
+			frame.icons = CreateFrame("Frame", nil, frame.hp)
+			frame.icons:SetPoint("BOTTOMRIGHT", frame.hp, "TOPRIGHT", 0, C.font.nameplates_font_size + 7)
+			frame.icons:SetWidth(20 + C.nameplate.width)
+			frame.icons:SetHeight(C.nameplate.auras_size)
+			frame.icons:SetFrameLevel(frame.hp:GetFrameLevel() + 2)
+			frame:RegisterEvent("UNIT_AURA")
+			frame:HookScript("OnEvent", OnAura)
+		end
+	end
 
-	-- Reposition and Resize RaidIcon
-	raidicon:ClearAllPoints()
-	raidicon:SetPoint("BOTTOM", hp, "TOP", 0, C.nameplate.track_auras == true and 38 or 16)
-	raidicon:SetSize((C.nameplate.height * 2) + 8, (C.nameplate.height * 2) + 8)
-	frame.raidicon = raidicon
+	-- Highlight texture
+	if not frame.overlay then
+		overlay:SetTexture(1, 1, 1, 0.15)
+		overlay:SetParent(frame.hp)
+		overlay:SetAllPoints()
+		frame.overlay = overlay
+	end
+
+	-- Raid icon
+	if not frame.raidicon then
+		raidicon:SetParent(frame.hp)
+		raidicon:ClearAllPoints()
+		raidicon:SetPoint("BOTTOM", hp, "TOP", 0, C.nameplate.track_auras == true and 38 or 16)
+		raidicon:SetSize((C.nameplate.height * 2) + 8, (C.nameplate.height * 2) + 8)
+		frame.raidicon = raidicon
+	end
 
 	-- Hide Old Stuff
 	QueueObject(frame, oldhp)
@@ -513,9 +515,6 @@ local function SkinObjects(frame, nameFrame)
 	frames[frame] = true
 end
 
-local goodR, goodG, goodB = unpack(C.nameplate.good_color)
-local badR, badG, badB = unpack(C.nameplate.bad_color)
-local transitionR, transitionG, transitionB = unpack(C.nameplate.near_color)
 local function UpdateThreat(frame, elapsed)
 	frame.hp:Show()
 	if frame.hasClass or frame.isTagged then return end
@@ -582,7 +581,7 @@ end
 
 -- Force the name text of a nameplate to be behind other nameplates unless it is our target
 local function AdjustNameLevel(frame, ...)
-	if UnitName("target") == frame.hp.name:GetText() and frame:GetAlpha() == 1 then
+	if UnitName("target") == frame.hp.name:GetText() and frame:GetParent():GetAlpha() == 1 then
 		frame.hp.name:SetDrawLayer("OVERLAY")
 	else
 		frame.hp.name:SetDrawLayer("BORDER")
@@ -592,7 +591,7 @@ end
 -- Health Text, also border coloring for certain plates depending on health
 local function ShowHealth(frame, ...)
 	-- Show current health value
-	local minHealth, maxHealth = frame.healthOriginal:GetMinMaxValues()
+	local _, maxHealth = frame.healthOriginal:GetMinMaxValues()
 	local valueHealth = frame.healthOriginal:GetValue()
 	local d = (valueHealth / maxHealth) * 100
 
@@ -617,7 +616,7 @@ local function ShowHealth(frame, ...)
 		SetVirtualBorder(frame.hp, unpack(C.media.border_color))
 	end
 
-	if UnitName("target") and frame:GetAlpha() == 1 then
+	if UnitName("target") and frame:GetParent():GetAlpha() == 1 then
 		frame.hp:SetSize((C.nameplate.width + C.nameplate.ad_width) * noscalemult, (C.nameplate.height + C.nameplate.ad_height) * noscalemult)
 		frame.cb:SetSize((C.nameplate.width + C.nameplate.ad_width) * noscalemult, (C.nameplate.height + C.nameplate.ad_height) * noscalemult)
 		frame.cb.icon:SetSize(((C.nameplate.height + C.nameplate.ad_height) * 2) + 8, ((C.nameplate.height + C.nameplate.ad_height) * 2) + 8)
@@ -659,20 +658,20 @@ end
 -- Run a function for all visible nameplates, we use this for the blacklist, to check unitguid, and to hide drunken text
 local function ForEachPlate(functionToRun, ...)
 	for frame in pairs(frames) do
-		if frame and frame:IsShown() then
+		if frame and frame:GetParent():IsShown() then
 			functionToRun(frame, ...)
 		end
 	end
 end
 
 -- Check if the frames default overlay texture matches blizzards nameplates default overlay texture
-local select = select
 local function HookFrames(...)
 	for index = 1, select("#", ...) do
 		local frame = select(index, ...)
 
-		if not frames[frame] and (frame:GetName() and not frame.isSkinned and frame:GetName():find("NamePlate%d")) then
-			SkinObjects(frame:GetChildren())
+		if frame:GetName() and not frame.isSkinned and frame:GetName():find("NamePlate%d") then
+			local child1, child2 = frame:GetChildren()
+			SkinObjects(child1, child2)
 			frame.isSkinned = true
 		end
 	end
