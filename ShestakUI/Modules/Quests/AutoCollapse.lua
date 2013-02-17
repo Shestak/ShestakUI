94bc1c571f3356bfebc162ce45805af10384cf82
Shestak <misha.shestakov@gmail.com>
2013-02-17 23:47:39 +0400

    Added extra check for WatchFrameCollapseExpandButton.

--- ShestakUI/Modules/Quests/AutoCollapse.lua
+++ ShestakUI/Modules/Quests/AutoCollapse.lua
@@ -9,7 +9,13 @@ frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", function(self, event)
 	if IsInInstance() then
 		WatchFrame_CollapseExpandButton_OnClick(WatchFrame_CollapseExpandButton)
+		if C.skins.blizzard_frames == true then
+			WatchFrameCollapseExpandButton.text:SetText("+")
+		end
 	elseif WatchFrame.collapsed and not InCombatLockdown() then
 		WatchFrame_CollapseExpandButton_OnClick(WatchFrame_CollapseExpandButton)
+		if C.skins.blizzard_frames == true then
+			WatchFrameCollapseExpandButton.text:SetText("-")
+		end
 	end
 end)
