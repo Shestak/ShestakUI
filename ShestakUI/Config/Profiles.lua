8bc5a53f9c32ef86dc05b9665617f081fcf72a84
Shestak <misha.shestakov@gmail.com>
2013-03-06 13:07:04 +0400

    Some changes in Profiles.lua

--- ShestakUI/Config/Profiles.lua
+++ ShestakUI/Config/Profiles.lua
@@ -126,25 +126,30 @@ end
 --	Oz@Pozzo dell'Eternità Config
 ----------------------------------------------------------------------------------------
 if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
+	C["position"].bank = {"RIGHT", ChatFrame1, "BOTTOM", 349, 371}
 	C["position"].bn_popup = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5}
-	C["position"].quest = {"TOPRIGHT", UIParent, "TOPRIGHT", -239, -113}
-	C["position"].threat_meter = {"CENTER", UIParent, "CENTER", 0, -91}
+	C["position"].vehicle = {"BOTTOMLEFT", "ActionBarAnchor", "BOTTOMRIGHT", 109, 100}
+	C["position"].ghost = {"TOPLEFT", "oUF_Player", "TOPLEFT", -2, 65}
+	C["position"].quest = {"TOPRIGHT", UIParent, "TOPRIGHT", -238, -111}
+	C["position"].loot = {"TOPLEFT", UIParent, "TOPLEFT", 21, -532}
+	C["position"].group_loot = {"TOPLEFT", UIParent, "TOPLEFT", 241, -211}
+	C["position"].threat_meter = {"BOTTOM", "oUF_Target", "BOTTOM", -283, 77}
 	C["position"].raid_cooldown = {"TOPRIGHT", UIParent, "TOPRIGHT", -21, -115}
-	C["position"].enemy_cooldown = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -398, 293}
-	C["position"].top_panel = {"TOP", UIParent, "TOP", 0, -118}
+	C["position"].enemy_cooldown = {"BOTTOMLEFT", "oUF_Target", "BOTTOMRIGHT", 9, -62}
 	C["position"].raid_buffs = {"BOTTOMLEFT", Minimap, "TOPLEFT", -2, 5}
-	C["position"].raid_utility = {"BOTTOMLEFT", UIParent, "BOTTOMLEFT", 21, 210}
-	C["position"].extra_button = {"BOTTOM", UIParent, "BOTTOM", -369, 197}
-	C["position"].stance_bar = {"BOTTOM", UIParent, "BOTTOM", -298, 258}
-	C["position"].unitframes.player = {"BOTTOM", UIParent, "BOTTOM", -284, 355}
-	C["position"].unitframes.target = {"BOTTOM", UIParent, "BOTTOM", 283, 355}
+	C["position"].raid_utility = {"TOPLEFT", UIParent, "TOPLEFT", 241, -21}
+	C["position"].extra_button = {"RIGHT", "oUF_Player", "BOTTOM", -53, -126}
+	C["position"].stance_bar = {"RIGHT", "oUF_Player", "BOTTOM", 82, -85}
+	C["position"].unitframes.player = {"TOPLEFT", "ActionBarAnchor", "BOTTOMRIGHT", -558, 373}
+	C["position"].unitframes.target = {"TOPRIGHT", "ActionBarAnchor", "BOTTOMLEFT", 558, 373}
 	C["position"].unitframes.tank = {"BOTTOMLEFT", "ActionBarAnchor", "BOTTOMRIGHT", 5, 18}
-	C["position"].unitframes.player_castbar = {"CENTER", UIParent, "CENTER", 0, -177}
-	C["position"].unitframes.target_castbar = {"CENTER", UIParent, "CENTER", 11, -154}
+	C["position"].unitframes.player_castbar = {"CENTER", UIParent, "CENTER", 0, 136}
+	C["position"].unitframes.target_castbar = {"BOTTOM", "oUF_Target", "BOTTOM", -272, -4}
+	C["position"].unitframes.raid_heal = {"TOPLEFT", "ActionBarAnchor", "BOTTOMRIGHT", -331, 335}
 	C["media"].border_color = {0.1, 0.1, 0.1, 1}
+	C["media"].overlay_color = {0, 0, 0, 0.5}
 	C["general"].welcome_message = false
 	C["misc"].invite_keyword = "inv"
-	C["misc"].vehicle_mouseover = true
 	C["misc"].profession_database = true
 	C["misc"].item_level = true
 	C["misc"].gem_counter = true
@@ -160,11 +165,11 @@ if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
 	C["skins"].blizzard_frames = true
 	C["skins"].dbm = true
 	C["skins"].npcscan = true
-	C["skins"].weak_auras = true
 	C["combattext"].icons = false
 	C["combattext"].scrollable = true
 	C["combattext"].killingblow = true
-	C["enemycooldown"].show_always = true
+	C["reminder"].raid_buffs_size = 19
+	C["enemycooldown"].show_inpvp = true
 	C["threat"].width = 125
 	C["threat"].bar_rows = 2
 	C["tooltip"].item_icon = true
@@ -177,12 +182,15 @@ if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
 	C["tooltip"].who_targetting = true
 	C["tooltip"].symbiosis = true
 	C["tooltip"].item_transmogrify = true
+	C["chat"].background = true
+	C["chat"].background_alpha = 0.5
 	C["chat"].filter = false
 	C["chat"].chat_bar = true
 	C["chat"].chat_bar_mouseover = true
 	C["chat"].tabs_mouseover = true
 	C["chat"].damage_meter_spam = true
 	C["minimap"].size = 129
+	C["minimap"].toggle_menu = false
 	C["map"].map_boss_count = true
 	C["loot"].auto_greed = false
 	C["nameplate"].show_castbar_name = true
@@ -191,7 +199,6 @@ if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
 	C["nameplate"].healer_icon = true
 	C["actionbar"].hotkey = false
 	C["actionbar"].bottombars = 1
-	C["aura"].show_spiral = true
 	C["aura"].focus_debuffs = true
 	C["aura"].fot_debuffs = true
 	C["aura"].pet_debuffs = true
@@ -208,6 +215,7 @@ if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
 	C["unitframe"].plugins_necro_strike = true
 	C["unitframe"].plugins_reputation_bar = true
 	C["unitframe"].plugins_experience_bar = true
+	C["unitframe"].plugins_friendship_bar = true
 	C["unitframe"].plugins_smooth_bar = true
 	C["unitframe"].plugins_talents = true
 	C["unitframe"].plugins_diminishing = true
@@ -216,6 +224,7 @@ if T.name == "Oz" and T.realm == "Pozzo dell'Eternità" then
 	C["raidframe"].icons_role = true
 	C["raidframe"].plugins_aura_watch_timer = true
 	C["raidframe"].plugins_auto_resurrection = true
+	C["toppanel"].enable = false
 	C["stats"].battleground = true
 end
 
