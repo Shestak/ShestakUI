55d05fb558d3f5352c339a5d05ea35b4e73f8fc2
Wetxius <wetxius@gmail.com>
2013-03-14 22:12:55 +0400

    Updated drinking spell id for MoP.

--- ShestakUI/Modules/Announcements/Drinking.lua
+++ ShestakUI/Modules/Announcements/Drinking.lua
@@ -10,7 +10,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
 	if not (event == "UNIT_SPELLCAST_SUCCEEDED" and GetZonePVPInfo() == "arena") then return end
 
 	local unit, _, _, _, spellID = ...
-	if UnitIsEnemy("player", unit) and (spellID == 80167 or spellID == 94468 or spellID == 43183 or spellID == 57073) then
+	if UnitIsEnemy("player", unit) and (spellID == 118358 or spellID == 104270) then
 		SendChatMessage(UnitName(unit)..L_MISC_DRINKING, "PARTY")
 	end
 end)
