27d04f67555ddb23328722b45db2d345d2ee5877
m2jest1c <artem.fufaldin@gmail.com>
2013-04-24 15:22:39 +0600

    one fix from ShestakUI (2c0e42896b commit)

--- DarkShestakUI/Core/Launch.lua
+++ DarkShestakUI/Core/Launch.lua
@@ -7,7 +7,6 @@ local function InstallUI()
 	-- Don't need to set CVar multiple time
 	SetCVar("screenshotQuality", 8)
 	SetCVar("cameraDistanceMax", 50)
-	SetCVar("cameraDistanceMaxFactor", 3.4)
 	SetCVar("ShowClassColorInNameplate", 1)
 	SetCVar("mapQuestDifficulty", 1)
 	SetCVar("showTutorials", 0)
