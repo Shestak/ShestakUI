eca29ab4db61168f582a6c6f8b77e38545f31c25
Wetxius <wetxius@gmail.com>
2013-04-30 18:08:09 +0400

    Cleanup.

--- ShestakUI/Modules/Loot/AutoGreed.lua
+++ ShestakUI/Modules/Loot/AutoGreed.lua
@@ -1,5 +1,5 @@
 local T, C, L, _ = unpack(select(2, ...))
-if C.loot.auto_greed ~= true then return end
+if C.loot.auto_greed ~= true or T.level ~= MAX_PLAYER_LEVEL then return end
 
 ----------------------------------------------------------------------------------------
 --	Auto greed/disenchant on green items(by Tekkub) and NeedTheOrb(by Myrilandell of Lothar)
@@ -8,7 +8,6 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("START_LOOT_ROLL")
 frame:SetScript("OnEvent", function(self, event, id)
 	local name = select(2, GetLootRollItemInfo(id))
-	if T.level ~= MAX_PLAYER_LEVEL then return end
 	if id and select(4, GetLootRollItemInfo(id)) == 2 and not select(5, GetLootRollItemInfo(id)) then
 		for i in pairs(T.NeedLoot) do
 			if name == select(1, GetItemInfo(T.NeedLoot[i])) and RollOnLoot(id, 1) then
