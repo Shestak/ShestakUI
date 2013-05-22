949fc19dad3b81806044807b2b868dd89a2f11ad
Wetxius <wetxius@gmail.com>
2013-05-22 19:18:52 +0400

    Fixed WorldMap.

--- ShestakUI/Modules/Maps/WorldMap.lua
+++ ShestakUI/Modules/Maps/WorldMap.lua
@@ -48,20 +48,20 @@ local SmallerMapSkin = function()
 	WorldMapFrameTitle:SetPoint("TOP", WorldMapDetailFrame, 0, -3)
 	WorldMapFrameTitle:SetFont(C.media.normal_font, 17)
 
-	T.SkinCheckBox(WorldMapQuestShowObjectives)
-	WorldMapQuestShowObjectives:ClearAllPoints()
-	WorldMapQuestShowObjectives:SetPoint("BOTTOMRIGHT", WorldMapButton, "BOTTOMRIGHT", -3 - WorldMapQuestShowObjectivesText:GetWidth(), 0)
+	-- T.SkinCheckBox(WorldMapQuestShowObjectives)
+	-- WorldMapQuestShowObjectives:ClearAllPoints()
+	-- WorldMapQuestShowObjectives:SetPoint("BOTTOMRIGHT", WorldMapButton, "BOTTOMRIGHT", -3 - WorldMapQuestShowObjectivesText:GetWidth(), 0)
 
 	T.SkinCheckBox(WorldMapTrackQuest)
 	WorldMapTrackQuest:ClearAllPoints()
 	WorldMapTrackQuest:SetPoint("BOTTOMLEFT", WorldMapButton, "BOTTOMLEFT", 0, 0)
 
-	T.SkinCheckBox(WorldMapShowDigSites)
-	WorldMapShowDigSites:ClearAllPoints()
-	WorldMapShowDigSites:SetPoint("BOTTOM", WorldMapQuestShowObjectives, "TOP", 0, 0)
+	-- T.SkinCheckBox(WorldMapShowDigSites)
+	-- WorldMapShowDigSites:ClearAllPoints()
+	-- WorldMapShowDigSites:SetPoint("BOTTOM", WorldMapQuestShowObjectives, "TOP", 0, 0)
 
-	WorldMapShowDigSitesText:ClearAllPoints()
-	WorldMapShowDigSitesText:SetPoint("LEFT", WorldMapShowDigSites, "RIGHT", 0, 0)
+	-- WorldMapShowDigSitesText:ClearAllPoints()
+	-- WorldMapShowDigSitesText:SetPoint("LEFT", WorldMapShowDigSites, "RIGHT", 0, 0)
 
 	WorldMapFrameAreaLabel:SetFont(C.media.normal_font, 40)
 	WorldMapFrameAreaLabel:SetShadowOffset(2, -2)
