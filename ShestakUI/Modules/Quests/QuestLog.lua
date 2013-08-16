512e414288ad4046c8c61bd060762ce5b593c9fd
Wetxius <wetxius@gmail.com>
2013-08-16 12:38:41 +0400

    Cleanup.

--- ShestakUI/Modules/Quests/QuestLog.lua
+++ ShestakUI/Modules/Quests/QuestLog.lua
@@ -7,13 +7,13 @@ local function questlevel()
 	local buttons = QuestLogScrollFrame.buttons
 	local numButtons = #buttons
 	local scrollOffset = HybridScrollFrame_GetOffset(QuestLogScrollFrame)
-	local numEntries, numQuests = GetNumQuestLogEntries()
+	local numEntries = GetNumQuestLogEntries()
 
 	for i = 1, numButtons do
 		local questIndex = i + scrollOffset
 		local questLogTitle = buttons[i]
 		if questIndex <= numEntries then
-			local title, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily = GetQuestLogTitle(questIndex)
+			local title, level, _, _, isHeader = GetQuestLogTitle(questIndex)
 			if not isHeader then
 				questLogTitle:SetText("["..level.."] "..title)
 				QuestLogTitleButton_Resize(questLogTitle)
@@ -29,7 +29,6 @@ QuestLogScrollFrameScrollBar:HookScript("OnValueChanged", questlevel)
 ----------------------------------------------------------------------------------------
 hooksecurefunc("QuestLogTitleButton_OnClick", function(self, button)
 	local questIndex = self:GetID()
-	local questName = self:GetText()
 	if IsModifiedClick() then
 		if self.isHeader then return end
 		if IsControlKeyDown() then
