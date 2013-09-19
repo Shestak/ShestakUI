08a654814a109569080d144af219cd4109b77d7c
Wetxius <wetxius@gmail.com>
2013-09-19 19:12:39 +0400

    Improved Mounts.

--- ShestakUI/Modules/Misc/Mounts.lua
+++ ShestakUI/Modules/Misc/Mounts.lua
@@ -25,27 +25,31 @@ function Mountz(groundmount, flyingmount, underwatermount)
 			InVj = true
 		end
 	end
-	if InVj and nofly then
+	if InVj and IsSwimming() then
 		vjswim = true
 	end
 	if IsSwimming() and nofly and not vjswim then
 		swimablex = true
 	end
 	if IsControlKeyDown() then
-		flyablex = not flyablex
+		if not vjswim then
+			flyablex = not flyablex
+		else
+			vjswim = not vjswim
+		end
 	end
 	for i = 1, num, 1 do
 		local _, info, id = GetCompanionInfo("MOUNT", i)
-		if underwatermount and info == underwatermount and swimablex then
+		if flyingmount and info == flyingmount and flyablex and not swimablex then
 			CallCompanion("MOUNT", i)
 			return
-		elseif flyingmount and info == flyingmount and flyablex and not swimablex then
+		elseif groundmount and info == groundmount and not flyablex and not swimablex and not vjswim then
 			CallCompanion("MOUNT", i)
 			return
-		elseif groundmount and info == groundmount and not flyablex and not swimablex then
+		elseif underwatermount and info == underwatermount and swimablex then
 			CallCompanion("MOUNT", i)
 			return
-		elseif id == 75207 and vjswim and IsSwimming() and not swimablex then
+		elseif id == 75207 and vjswim then
 			CallCompanion("MOUNT", i)
 			return
 		end
