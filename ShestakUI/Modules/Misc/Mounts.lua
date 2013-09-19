58178413da5fa2d6a79ac76e9316b5fdb6cd7a9d
Wetxius <wetxius@gmail.com>
2013-09-19 15:30:14 +0400

    Updated description for Mounts.

--- ShestakUI/Modules/Misc/Mounts.lua
+++ ShestakUI/Modules/Misc/Mounts.lua
@@ -1,9 +1,9 @@
 ----------------------------------------------------------------------------------------
 --	Universal Mount macro(by Monolit)
---	/run Mountz("your_ground_mount","your_flying_mount")
+--	/run Mountz("your_ground_mount","your_flying_mount","your_underwater_mount")
 ----------------------------------------------------------------------------------------
 function Mountz(groundmount, flyingmount, underwatermount)
-	local flyablex, swimablex, vjswim, underwater, InVj, nofly
+	local flyablex, swimablex, vjswim, InVj, nofly
 	local num = GetNumCompanions("MOUNT")
 	if not num or IsMounted() then
 		Dismount()
