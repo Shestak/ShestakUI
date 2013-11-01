7af386b7544f47cf8e0d7a2efcb6b202bdef433a
Shestak <misha.shestakov@gmail.com>
2013-11-01 05:03:48 +0400

    Fixed SpellBookFrame error.

--- ShestakUI/Modules/Blizzard/Fixes.lua
+++ ShestakUI/Modules/Blizzard/Fixes.lua
@@ -116,17 +116,3 @@ TradeSkillClearButton:SetScript("OnEvent", OnEvent)
 ----------------------------------------------------------------------------------------
 setfenv(WorldMapFrame_OnShow, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
 setfenv(FriendsFrame_OnShow, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-setfenv(SpellBookFrame_OnShow, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-setfenv(SpellBookFrame_OnHide, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-
-local ParentalControls = CreateFrame("Frame")
-ParentalControls:RegisterEvent("ADDON_LOADED")
-ParentalControls:SetScript("OnEvent", function(self, event, addon)
-	if addon == "Blizzard_AchievementUI" then
-		setfenv(AchievementFrame_OnShow, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-		setfenv(AchievementFrame_OnHide, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-	elseif addon == "Blizzard_TalentUI" then
-		setfenv(PlayerTalentFrame_OnShow, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-		setfenv(PlayerTalentFrame_OnHide, setmetatable({UpdateMicroButtons = function() end}, {__index = _G}))
-	end
-end)
