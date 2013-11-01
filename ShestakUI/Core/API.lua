d7bcaf592f78db26b3362cbc206e4b9142ee2c83
Shestak <misha.shestakov@gmail.com>
2013-11-01 15:08:11 +0400

    Fixed LearnButton.Flash.

--- ShestakUI/Core/API.lua
+++ ShestakUI/Core/API.lua
@@ -222,6 +222,7 @@ local function SkinButton(f, strip)
 	if f.Middle then f.Middle:SetAlpha(0) end
 	if f.LeftSeparator then f.LeftSeparator:SetAlpha(0) end
 	if f.RightSeparator then f.RightSeparator:SetAlpha(0) end
+	if f.Flash then f.Flash:SetAlpha(0) end
 
 	f:SetTemplate("Overlay")
 	f:HookScript("OnEnter", T.SetModifiedBackdrop)
