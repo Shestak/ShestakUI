1264f952d4d238172c95a716070664d3c536d8e1
Wetxius <wetxius@gmail.com>
2013-11-19 15:32:01 +0400

    Fixed AutoSafariHat.

--- ShestakUI/Modules/Automation/AutoSafariHat.lua
+++ ShestakUI/Modules/Automation/AutoSafariHat.lua
@@ -5,21 +5,23 @@ if C.automation.safari_hat ~= true then return end
 --	Auto equip Safari Hat
 ----------------------------------------------------------------------------------------
 local frame = CreateFrame("Frame"), hat, show
-frame:RegisterEvent("PET_BATTLE_OPENING_DONE")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 frame:RegisterEvent("PET_BATTLE_CLOSE")
 frame:SetScript("OnEvent", function(self, event)
-	if event == "PET_BATTLE_OPENING_DONE" then
-		if ShowingHelm() then
-			show = true
-		else
-			show = false
+	if event == "PLAYER_TARGET_CHANGED" and not hat then
+		if UnitIsWildBattlePet("target") and not InCombatLockdown() then
+			if ShowingHelm() then
+				show = true
+			else
+				show = false
+			end
+			if GetItemCount(92738) > 0 then
+				hat = GetInventoryItemLink("player", 1)
+				EquipItemByName(92738)
+				ShowHelm(true)
+			end
 		end
-		if GetItemCount(92738) > 0 then
-			hat = GetInventoryItemLink("player", 1)
-			EquipItemByName(92738)
-			ShowHelm(true)
-		end
-	elseif event == "PET_BATTLE_CLOSE" then
+	elseif event == "PET_BATTLE_CLOSE" or (event == "PLAYER_TARGET_CHANGED" and not C_PetBattles.IsInBattle()) then
 		if hat then
 			EquipItemByName(hat)
 			ShowHelm(show)
