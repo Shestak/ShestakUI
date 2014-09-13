bf1cf2972791f39f46c6052d826066e0b52b1f9b
Michael <misha.shestakov@gmail.com>
2014-09-13 13:48:10 +0400

    Fixed ObjectiveTrackerFrame position.

--- ShestakUI/Modules/Quests/WatchFrame.lua
+++ ShestakUI/Modules/Quests/WatchFrame.lua
@@ -1,7 +1,7 @@
 local T, C, L, _ = unpack(select(2, ...))
 
 ----------------------------------------------------------------------------------------
---	Move WatchFrame
+--	Move ObjectiveTrackerFrame
 ----------------------------------------------------------------------------------------
 local frame = CreateFrame("Frame", "WatchFrameAnchor", UIParent)
 frame:SetPoint(unpack(C.position.quest))
@@ -12,88 +12,92 @@ else
 	frame:SetWidth(224)
 end
 
-WatchFrame:ClearAllPoints()
-WatchFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, 0)
-WatchFrame:SetHeight(T.getscreenheight / 1.6)
+ObjectiveTrackerFrame:ClearAllPoints()
+ObjectiveTrackerFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, 0)
+ObjectiveTrackerFrame:SetHeight(T.getscreenheight / 1.6)
 
-hooksecurefunc(WatchFrame, "SetPoint", function(_, _, parent)
+hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", function(_, _, parent)
 	if parent ~= frame then
-		WatchFrame:ClearAllPoints()
-		WatchFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, 0)
+		ObjectiveTrackerFrame:ClearAllPoints()
+		ObjectiveTrackerFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, 0)
 	end
 end)
 
 ----------------------------------------------------------------------------------------
---	Skin WatchFrame item buttons
+--	Skin ObjectiveTrackerFrame item buttons
 ----------------------------------------------------------------------------------------
-hooksecurefunc("WatchFrameItem_UpdateCooldown", function(self)
-	if not self.skinned and not InCombatLockdown() then
-		local icon = _G[self:GetName().."IconTexture"]
-		local border = _G[self:GetName().."NormalTexture"]
-		local count = _G[self:GetName().."Count"]
+--WoD hooksecurefunc("WatchFrameItem_UpdateCooldown", function(self)
+	-- if not self.skinned and not InCombatLockdown() then
+		-- local icon = _G[self:GetName().."IconTexture"]
+		-- local border = _G[self:GetName().."NormalTexture"]
+		-- local count = _G[self:GetName().."Count"]
 
-		self:SetSize(C.actionbar.button_size, C.actionbar.button_size)
-		self:SetTemplate("Default")
+		-- self:SetSize(C.actionbar.button_size, C.actionbar.button_size)
+		-- self:SetTemplate("Default")
 
-		icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-		icon:SetPoint("TOPLEFT", self, 2, -2)
-		icon:SetPoint("BOTTOMRIGHT", self, -2, 2)
+		-- icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+		-- icon:SetPoint("TOPLEFT", self, 2, -2)
+		-- icon:SetPoint("BOTTOMRIGHT", self, -2, 2)
 
-		count:ClearAllPoints()
-		count:SetPoint("BOTTOMRIGHT", 0, 2)
-		count:SetFont(C.font.action_bars_font, C.font.action_bars_font_size, C.font.action_bars_font_style)
-		count:SetShadowOffset(C.font.action_bars_font_shadow and 1 or 0, C.font.action_bars_font_shadow and -1 or 0)
+		-- count:ClearAllPoints()
+		-- count:SetPoint("BOTTOMRIGHT", 0, 2)
+		-- count:SetFont(C.font.action_bars_font, C.font.action_bars_font_size, C.font.action_bars_font_style)
+		-- count:SetShadowOffset(C.font.action_bars_font_shadow and 1 or 0, C.font.action_bars_font_shadow and -1 or 0)
 
-		border:SetTexture(nil)
+		-- border:SetTexture(nil)
 
-		self:StyleButton()
+		-- self:StyleButton()
 
-		self.skinned = true
-	end
-end)
+		-- self.skinned = true
+	-- end
+-- end)
 
 ----------------------------------------------------------------------------------------
---	Difficulty color for WatchFrame lines
+--	Difficulty color for ObjectiveTrackerFrame lines
 ----------------------------------------------------------------------------------------
-hooksecurefunc("WatchFrame_Update", function()
-	local numQuestWatches = GetNumQuestWatches()
+--WoD hooksecurefunc("WatchFrame_Update", function()
+	-- local numQuestWatches = GetNumQuestWatches()
 
-	for i = 1, numQuestWatches do
-		local questIndex = GetQuestIndexForWatch(i)
-		if questIndex then
-			local title, level = GetQuestLogTitle(questIndex)
-			local col = GetQuestDifficultyColor(level)
+	-- for i = 1, numQuestWatches do
+		-- local questIndex = GetQuestIndexForWatch(i)
+		-- if questIndex then
+			-- local title, level = GetQuestLogTitle(questIndex)
+			-- local col = GetQuestDifficultyColor(level)
 
-			for j = 1, #WATCHFRAME_QUESTLINES do
-				if WATCHFRAME_QUESTLINES[j].text:GetText() == title then
-					WATCHFRAME_QUESTLINES[j].text:SetTextColor(col.r, col.g, col.b)
-					WATCHFRAME_QUESTLINES[j].col = col
-				end
-			end
-			for k = 1, #WATCHFRAME_ACHIEVEMENTLINES do
-				WATCHFRAME_ACHIEVEMENTLINES[k].col = nil
-			end
-		end
-	end
-end)
+			-- for j = 1, #WATCHFRAME_QUESTLINES do
+				-- if WATCHFRAME_QUESTLINES[j].text:GetText() == title then
+					-- WATCHFRAME_QUESTLINES[j].text:SetTextColor(col.r, col.g, col.b)
+					-- WATCHFRAME_QUESTLINES[j].col = col
+				-- end
+			-- end
+			-- for k = 1, #WATCHFRAME_ACHIEVEMENTLINES do
+				-- WATCHFRAME_ACHIEVEMENTLINES[k].col = nil
+			-- end
+		-- end
+	-- end
+-- end)
 
-hooksecurefunc("WatchFrameLinkButtonTemplate_Highlight", function(self, onEnter)
-	i = self.startLine
-	if not (self.lines[i] and self.lines[i].col) then return end
-	if onEnter then
-		self.lines[i].text:SetTextColor(1, 0.8, 0)
-	else
-		self.lines[i].text:SetTextColor(self.lines[i].col.r, self.lines[i].col.g, self.lines[i].col.b)
-	end
-end)
+--WoD hooksecurefunc("WatchFrameLinkButtonTemplate_Highlight", function(self, onEnter)
+	-- i = self.startLine
+	-- if not (self.lines[i] and self.lines[i].col) then return end
+	-- if onEnter then
+		-- self.lines[i].text:SetTextColor(1, 0.8, 0)
+	-- else
+		-- self.lines[i].text:SetTextColor(self.lines[i].col.r, self.lines[i].col.g, self.lines[i].col.b)
+	-- end
+-- end)
+
+for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"}) do
+	local header = ObjectiveTrackerFrame.BlocksFrame[headerName].Background:Hide()
+end
 
 ----------------------------------------------------------------------------------------
 --	Skin WatchFrameCollapseExpandButton
 ----------------------------------------------------------------------------------------
 if C.skins.blizzard_frames == true then
-	T.SkinCloseButton(WatchFrameCollapseExpandButton, nil, "-", true)
-	WatchFrameCollapseExpandButton:HookScript("OnClick", function(self)
-		if WatchFrame.collapsed then
+	T.SkinCloseButton(ObjectiveTrackerFrame.HeaderMenu.MinimizeButton, nil, "-", true)
+	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:HookScript("OnClick", function(self)
+		if ObjectiveTrackerFrame.collapsed then
 			self.text:SetText("+")
 		else
 			self.text:SetText("-")
