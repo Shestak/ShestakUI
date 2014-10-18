96a7c23e2e8d9c71fae1109ca724e5383cc04844
Wetxius <wetxius@gmail.com>
2014-10-18 15:09:07 +0400

    Cleanup.

--- ShestakUI/Modules/Quests/QuestLog.lua
+++ ShestakUI/Modules/Quests/QuestLog.lua
@@ -6,7 +6,7 @@ local T, C, L, _ = unpack(select(2, ...))
 hooksecurefunc("QuestLogQuests_Update", function()
 	for i, button in pairs(QuestMapFrame.QuestsFrame.Contents.Titles) do
 		if button:IsShown() then
-			local level= strmatch(GetQuestLink(button.questLogIndex), "quest:%d+:(%d+)")
+			local level = strmatch(GetQuestLink(button.questLogIndex), "quest:%d+:(%d+)")
 			if level then
 				local height = button.Text:GetHeight()
 				button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
