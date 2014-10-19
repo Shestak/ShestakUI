d671c8f26dbfd4a15a00194a7d96a89c41bc3782
Shestak <misha.shestakov@gmail.com>
2014-10-19 18:12:33 +0400

    Updated auto select event boss from LFD tool.

--- ShestakUI/Modules/Misc/Misc.lua
+++ ShestakUI/Modules/Misc/Misc.lua
@@ -117,16 +117,12 @@ end
 ----------------------------------------------------------------------------------------
 --	Auto select current event boss from LFD tool(EventBossAutoSelect by Nathanyel)
 ----------------------------------------------------------------------------------------
-local firstLFD
 LFDParentFrame:HookScript("OnShow", function()
-	if not firstLFD then
-		firstLFD = 1
-		for i = 1, GetNumRandomDungeons() do
-			local id = GetLFGRandomDungeonInfo(i)
-			local isHoliday = select(15, GetLFGDungeonInfo(id))
-			if isHoliday and not GetLFGDungeonRewards(id) then
-				LFDQueueFrame_SetType(id)
-			end
+	for i = 1, GetNumRandomDungeons() do
+		local id = GetLFGRandomDungeonInfo(i)
+		local isHoliday = select(15, GetLFGDungeonInfo(id))
+		if isHoliday and not GetLFGDungeonRewards(id) then
+			LFDQueueFrame_SetType(id)
 		end
 	end
 end)
