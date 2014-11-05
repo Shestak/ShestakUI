4b42426c73ba45bb88c27db16e1b638679363fbd
Wetxius <wetxius@gmail.com>
2014-11-05 16:14:21 +0300

    Adjust slider height in ShestakUI_Config.

--- ShestakUI_Config/ShestakUI_Config.lua
+++ ShestakUI_Config/ShestakUI_Config.lua
@@ -745,7 +745,7 @@ function CreateUIConfig()
 	end
 	child:SetWidth(125)
 	child:SetHeight(offset)
-	slider:SetMinMaxValues(0, (offset == 0 and 1 or offset - 12 * 32))
+	slider:SetMinMaxValues(0, (offset == 0 and 1 or offset - 12 * 33))
 	slider:SetValue(1)
 	groups:SetScrollChild(child)
 
