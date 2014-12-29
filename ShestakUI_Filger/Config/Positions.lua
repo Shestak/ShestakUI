9bd4e630b6802e1058b8ae18ee5597831ae2bc61
leg883 <leg883@126.com>
2014-12-29 19:45:49 +0800

    重新定义锚点。

--- ShestakUI_Filger/Config/Positions.lua
+++ ShestakUI_Filger/Config/Positions.lua
@@ -2,13 +2,19 @@ local T, C, L, _ = unpack(ShestakUI)
 if C.unitframe.enable ~= true then return end
 
 C["filger_position"] = {
-	player_buff_icon = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 173},	-- "P_BUFF_ICON"
-	player_proc_icon = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 173},	-- "P_PROC_ICON"
-	special_proc_icon = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 213},	-- "SPECIAL_P_BUFF_ICON"
-	target_debuff_icon = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 213},	-- "T_DEBUFF_ICON"
-	target_buff_icon = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 253},	-- "T_BUFF"
-	pve_debuff = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 253},			-- "PVE/PVP_DEBUFF"
-	pve_cc = {"TOPLEFT", "oUF_Player", "BOTTOMLEFT", -2, -44},				-- "PVE/PVP_CC"
+	player_buff = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 173},	-- "P_BUFF"
+	player_proc = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 213},	-- "P_PROC"
+	
+	player_special = {"BOTTOMLEFT", "oUF_Player", "TOPRIGHT", 33, 80},	-- "P_SPECIAL"
+
+	target_buff = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 173},		-- "T_BUFF"
+	target_proc = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 213},		-- "T_PROC"
+	
+	pve_player = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 253},		-- "PVE/PVP_P"
+	pve_target = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 253},		-- "PVE/PVP_T"
+	
+	pve_cc = {"TOPLEFT", "oUF_Player", "BOTTOMLEFT", -2, -44},		-- "PVE/PVP_CC"
+	
 	cooldown = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 63, C.unitframe.plugins_swing and 29 or 17},		-- "COOLDOWN"
 	target_bar = {"BOTTOMLEFT", C.unitframe.portrait_enable and "oUF_Target_Portrait" or "oUF_Target", "BOTTOMRIGHT", C.unitframe.portrait_enable and 6 or 9, C.unitframe.portrait_enable and -3 or -41},	-- "T_DE/BUFF_BAR"
 }
