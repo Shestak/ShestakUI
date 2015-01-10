24cc59a83b50d0807afb991dc51e34f8fc93c59d
leg883 <leg883@126.com>
2015-01-10 19:30:51 +0800

    Update MagePortals.lua

--- ShestakUI/Modules/Misc/MagePortals.lua
+++ ShestakUI/Modules/Misc/MagePortals.lua
@@ -5,9 +5,9 @@ if T.class ~= "MAGE" or T.level < 17 then return end
 --	Mage portals menu(by Foof and Tohveli)
 ----------------------------------------------------------------------------------------
 if T.client == "zhCN" then
-	Portalname = { "暴風城", "鐵爐堡", "達納蘇斯", "艾克索達", "塞拉摩", "撒塔斯", "達拉然", "托巴拉德", "遠古達拉然", "恆春谷", "暴風之盾"}
+	Portalname = {"暴风城", "铁炉堡", "达纳苏斯", "埃索达", "塞拉摩", "撒塔斯", "达拉然", "托尔巴拉德", "远古达拉然", "锦绣谷", "暴風之盾", "奥格瑞玛", "幽暗城", "雷霆崖", "银月城", "斯通纳德", "战争之矛"}
 elseif T.client == "zhTW" then
-	Portalname = { "暴風城", "鐵爐堡", "達納蘇斯", "艾克索達", "塞拉摩", "撒塔斯", "達拉然", "托巴拉德", "遠古達拉然", "恆春谷", "暴風之盾"}
+	Portalname = {"暴風城", "鐵爐堡", "達納蘇斯", "艾克索達", "塞拉摩", "撒塔斯", "達拉然", "托巴拉德", "遠古達拉然", "恆春谷", "暴風之盾", "奧格瑪", "幽暗城", "雷霆崖", "銀月城", "斯通納德", "戰爭之矛"}
 else
 	Portalname = nil
 end
@@ -16,26 +16,26 @@ local spells = (UnitFactionGroup("player") == "Horde") and {
 	[1] = {3567,11417, Portalname[12]},		-- Orgrimmar
 	[2] = {3563,11418, Portalname[13]},		-- Undercity
 	[3] = {3566,11420, Portalname[14]},		-- Thunder Bluff
-	[4] = {32272,32267, Portalname[15]},		-- Silvermoon
-	[5] = {49358,49361, Portalname[16]},		-- Stonard
-	[6] = {35715,35717, Portalname[17]},		-- Shattrath
-	[7] = {53140,53142, Portalname[18]},		-- Dalaran
-	[8] = {88344,88346, Portalname[19]},		-- Tol Barad
-	[9] = {120145,120146, Portalname[20]},		-- Ancient Dalaran
-	[10] = {132627,132626, Portalname[21]},		-- Vale of Eternal Blossoms
-	[11] = {176242,176244, Portalname[22]},		-- Warspear
+	[4] = {32272,32267, Portalname[15]},	-- Silvermoon
+	[5] = {49358,49361, Portalname[16]},	-- Stonard
+	[6] = {35715,35717, Portalname[6]},	-- Shattrath
+	[7] = {53140,53142, Portalname[7]},	-- Dalaran
+	[8] = {88344,88346, Portalname[8]},	-- Tol Barad
+	[9] = {120145,120146, Portalname[9]},	-- Ancient Dalaran
+	[10] = {132627,132626, Portalname[10]},	-- Vale of Eternal Blossoms
+	[11] = {176242,176244, Portalname[17]},	-- Warspear
 } or { -- Alliance
 	[1] = {3561,10059, Portalname[1]},		-- Stormwind
 	[2] = {3562,11416, Portalname[2]},		-- Ironforge
 	[3] = {3565,11419, Portalname[3]},		-- Darnassus
-	[4] = {32271,32266, Portalname[4]},		-- Exodar
-	[5] = {49359,49360, Portalname[5]},		-- Theramore
-	[6] = {33690,33691, Portalname[6]},		-- Shattrath
-	[7] = {53140,53142, Portalname[7]},		-- Dalaran
-	[8] = {88342,88345, Portalname[8]},		-- Tol Barad
-	[9] = {120145,120146, Portalname[9]},		-- Ancient Dalaran
-	[10] = {132621,132620, Portalname[10]},		-- Vale of Eternal Blossoms
-	[11] = {176248,176246, Portalname[11]},		-- Stormshield
+	[4] = {32271,32266, Portalname[4]},	-- Exodar
+	[5] = {49359,49360, Portalname[5]},	-- Theramore
+	[6] = {33690,33691, Portalname[6]},	-- Shattrath
+	[7] = {53140,53142, Portalname[7]},	-- Dalaran
+	[8] = {88342,88345, Portalname[8]},	-- Tol Barad
+	[9] = {120145,120146, Portalname[9]},	-- Ancient Dalaran
+	[10] = {132621,132620, Portalname[10]},	-- Vale of Eternal Blossoms
+	[11] = {176248,176246, Portalname[11]},	-- Stormshield
 }
 
 local frame = CreateFrame("Frame", "TeleportMenu", UIParent)
@@ -59,7 +59,11 @@ for i, spell in pairs(spells) do
 
 	local l = b:CreateFontString("TeleportMenuName"..i, "OVERLAY")
 	l:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
-	l:SetText(spell[3])
+	if (T.client == "zhCN") or (T.client == "zhTW") then
+		l:SetText(spell[3])
+	else
+		l:SetText(string.sub(teleport, string.find(teleport, ":") + 1))
+	end
 	b:SetFontString(l)
 
 	b:RegisterForClicks("LeftButtonDown", "RightButtonDown")
