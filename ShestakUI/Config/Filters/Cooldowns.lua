5270a1077e0f537257f6d64e41602dd155af3279
leg883 <leg883@126.com>
2015-02-09 21:37:43 +0800

    Update Cooldowns.lua

--- ShestakUI/Config/Filters/Cooldowns.lua
+++ ShestakUI/Config/Filters/Cooldowns.lua
@@ -7,13 +7,49 @@ local T, C, L, _ = unpack(select(2, ...))
 ----------------------------------------------------------------------------------------
 if C.raidcooldown.enable == true then
 	T.raid_spells = {
-		[20484] = 600,	-- Rebirth
-		[61999] = 600,	-- Raise Ally
-		[20707] = 600,	-- Soulstone
-		[126393] = 600,	-- Eternal Guardian (Quilen)
-		[159956] = 600,	-- Dust of Life (Moth)
-		[159931] = 600,	-- Gift of Chi-Ji (Crane)
-		[108280] = 180,	-- Healing Tide Totem
+		--复活技能
+		[20484] = 600,	-- Rebirth 复生 德鲁伊
+		[61999] = 600,	-- Raise Ally 盟友复生 DK
+		[20707] = 600,	-- Soulstone 灵魂石 术士
+		[126393] = 600,	-- Eternal Guardian (Quilen) 永恒守护者 猎人
+		[159956] = 600,	-- Dust of Life (Moth) 生命之尘 猎人
+		[159931] = 600,	-- Gift of Chi-Ji (Crane) 赤精之赐 猎人
+		
+		--群体治疗 
+		[740] = 180,  -- Tranquility 宁静 
+		[115310] = 180,  -- Revival 还魂术 武僧
+		[64843] = 180,  -- Divine Hymn 神圣赞美诗 牧师
+		[108280] = 180,  -- Healing Tide Totem 治疗之潮图腾 萨满 
+		[157535] = 90,  -- Breath of the Serpent 蟠龙之息 武僧
+		[15286] = 180,  -- Vampiric Embrace 吸血鬼的擁抱 术士
+		[108281] = 120,  -- Ancestral Guidance 先祖導引 萨满 T5天赋
+		
+		--群体减伤 
+		[62618] = 180,  -- Power Word: Barrier 真言术：障 牧师
+		[98008] = 180,  -- Spirit Link Totem 灵魂链接图腾 萨满
+		[31821] = 180,   -- Devotion Aura 虔诚光环 圣骑士
+		[51052] = 120,  -- Anti-Magic Zone 反魔法领域 DK
+		[97462] = 180,  -- Rallying Cry 集结呐喊 战士
+		[88611] = 180,  --Smoke Bomb 烟雾弹 盗贼
+		
+		--单体减伤
+		[102342] = 60,  -- Ironbark 铁木树皮 德鲁伊
+		[116849] = 120,  -- Life Cocoon 作茧缚命 武僧
+		[6940] = 120,  -- Hand of Sacrifice 牺牲之手 圣骑士
+		[33206] = 180,  -- Pain Suppression 痛苦压制 牧师
+		[47788] = 180,  --Guardian Spirit 守护之魂 牧师
+		[114030] = 120,  --Vigilance 警戒 战士
+		[633] = 600,  -- Lay on Hands 圣疗术 圣骑士
+		[114039] = 600,  -- Hand of Purity 纯净之手 圣骑士
+		
+		--团队增益
+		[32182] = 300,  -- Heroism 英勇 
+		[2825] = 300,   -- Bloodlust 嗜血 
+		[80353] = 300,  -- Time Warp 时间扭曲 
+		[90355] = 300,  -- Ancient Hysteria 远古狂乱   
+		[159916] = 120,  -- Amplify Magic 魔法增效 
+		[106898] = 120,  -- Stampeding Roar 狂奔怒吼 
+		[172106] = 180,  -- Aspect of the Fox灵狐守护 
 	}
 end
 
