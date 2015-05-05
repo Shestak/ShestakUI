b27c0f5655847eba727ea9fbd6a46e4980ef105a
leg883 <leg883@126.com>
2015-05-06 01:30:18 +0800

    Update RaidBuffsReminder.lua

--- ShestakUI/Modules/Buffs/RaidBuffsReminder.lua
+++ ShestakUI/Modules/Buffs/RaidBuffsReminder.lua
@@ -2,220 +2,191 @@ local T, C, L, _ = unpack(select(2, ...))
 if C.reminder.raid_buffs_enable ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	Raid buffs on player(by Elv22)
+--	Raid buffs on player(by NDui@NGACN)
 ----------------------------------------------------------------------------------------
--- Locals
-local flaskbuffs = T.ReminderBuffs["Flask"]
-local battleelixirbuffs = T.ReminderBuffs["BattleElixir"]
-local guardianelixirbuffs = T.ReminderBuffs["GuardianElixir"]
-local foodbuffs = T.ReminderBuffs["Food"]
-local statbuffs = T.ReminderBuffs["Stat"]
-local staminabuffs = T.ReminderBuffs["Stamina"]
-local visible, flask, battleelixir, guardianelixir, food, stat, stamina, spell5, spell6
-
--- We need to check if you have two different elixirs if your not flasked, before we say your not flasked
-local function CheckElixir(unit)
-	if battleelixirbuffs and battleelixirbuffs[1] then
-		for i, battleelixirbuffs in pairs(battleelixirbuffs) do
-			local name, _, icon = GetSpellInfo(battleelixirbuffs)
-			if UnitAura("player", name) then
-				FlaskFrame.t:SetTexture(icon)
-				battleelixir = true
-				break
-			else
-				battleelixir = false
-			end
-		end
-	end
-
-	if guardianelixirbuffs and guardianelixirbuffs[1] then
-		for i, guardianelixirbuffs in pairs(guardianelixirbuffs) do
-			local name, _, icon = GetSpellInfo(guardianelixirbuffs)
-			if UnitAura("player", name) then
-				guardianelixir = true
-				if not battleelixir then
-					FlaskFrame.t:SetTexture(icon)
-				end
-				break
-			else
-				guardianelixir = false
-			end
-		end
-	end
-
-	if guardianelixir == true and battleelixir == true then
-		FlaskFrame:SetAlpha(C.reminder.raid_buffs_alpha)
-		flask = true
-		return
-	else
-		FlaskFrame:SetAlpha(1)
-		flask = false
-	end
-end
-
--- Main Script
-local function OnAuraChange(self, event, arg1, unit)
-	if event == "UNIT_AURA" and arg1 ~= "player" then return end
-
-	-- If We're a caster we may want to see different buffs
-	if T.Role == "Caster" or T.Role == "Healer" then
-		T.ReminderCasterBuffs()
-	else
-		T.ReminderPhysicalBuffs()
-	end
-
-	-- Start checking buffs to see if we can find a match from the list
-	if flaskbuffs and flaskbuffs[1] then
-		for i, flaskbuffs in pairs(flaskbuffs) do
-			local name, _, icon = GetSpellInfo(flaskbuffs)
-			if i == 1 then
-				FlaskFrame.t:SetTexture(icon)
-			end
-			if UnitAura("player", name) then
-				FlaskFrame:SetAlpha(C.reminder.raid_buffs_alpha)
-				flask = true
-				break
-			else
-				CheckElixir()
-			end
-		end
-	end
-
-	if foodbuffs and foodbuffs[1] then
-		for i, foodbuffs in pairs(foodbuffs) do
-			local name, _, icon = GetSpellInfo(foodbuffs)
-			if i == 1 then
-				FoodFrame.t:SetTexture(icon)
-			end
-			if UnitAura("player", name) then
-				FoodFrame:SetAlpha(C.reminder.raid_buffs_alpha)
-				food = true
-				break
-			else
-				FoodFrame:SetAlpha(1)
-				food = false
-			end
-		end
-	end
-
-	for i, statbuffs in pairs(statbuffs) do
-		local name, _, icon = GetSpellInfo(statbuffs)
-		if i == 1 then
-			StatFrame.t:SetTexture(icon)
-		end
-		if UnitAura("player", name) then
-			StatFrame:SetAlpha(C.reminder.raid_buffs_alpha)
-			stat = true
-			break
-		else
-			StatFrame:SetAlpha(1)
-			stat = false
-		end
-	end
-
-	for i, staminabuffs in pairs(staminabuffs) do
-		local name, _, icon = GetSpellInfo(staminabuffs)
-		if i == 1 then
-			StaminaFrame.t:SetTexture(icon)
-		end
-		if UnitAura("player", name) then
-			StaminaFrame:SetAlpha(C.reminder.raid_buffs_alpha)
-			stamina = true
-			break
-		else
-			StaminaFrame:SetAlpha(1)
-			stamina = false
-		end
-	end
-
-	for i, Spell5Buff in pairs(Spell5Buff) do
-		local name, _, icon = GetSpellInfo(Spell5Buff)
-		if i == 1 then
-			Spell5Frame.t:SetTexture(icon)
-		end
-		if UnitAura("player", name) then
-			Spell5Frame:SetAlpha(C.reminder.raid_buffs_alpha)
-			spell5 = true
-			break
-		else
-			Spell5Frame:SetAlpha(1)
-			spell5 = false
-		end
-	end
-
-	for i, Spell6Buff in pairs(Spell6Buff) do
-		local name, _, icon = GetSpellInfo(Spell6Buff)
-		if i == 1 then
-			Spell6Frame.t:SetTexture(icon)
-		end
-		if UnitAura("player", name) then
-			Spell6Frame:SetAlpha(C.reminder.raid_buffs_alpha)
-			spell6 = true
-			break
-		else
-			Spell6Frame:SetAlpha(1)
-			spell6 = false
-		end
-	end
-
-	local _, instanceType = IsInInstance()
-	if (not IsInGroup() or instanceType ~= "raid") and C.reminder.raid_buffs_always == false then
-		RaidBuffReminder:SetAlpha(0)
-		visible = false
-	elseif flask == true and food == true and stat == true and stamina == true and spell5 == true and spell6 == true then
-		if not visible then
-			RaidBuffReminder:SetAlpha(0)
-			visible = false
-		end
-		if visible then
-			UIFrameFadeOut(RaidBuffReminder, 0.5)
-			visible = false
-		end
-	else
-		if not visible then
-			UIFrameFadeIn(RaidBuffReminder, 0.5)
-			visible = true
-		end
-	end
-end
-
--- Create Anchor
+local BuffFrame = {}
+local Melee, text
 local RaidBuffsAnchor = CreateFrame("Frame", "RaidBuffsAnchor", UIParent)
-RaidBuffsAnchor:SetWidth((C.reminder.raid_buffs_size * 6) + 15)
-RaidBuffsAnchor:SetHeight(C.reminder.raid_buffs_size)
 RaidBuffsAnchor:SetPoint(unpack(C.position.raid_buffs))
+RaidBuffsAnchor:SetWidth(1)
+RaidBuffsAnchor:SetHeight(1)
+RaidBuffsAnchor:RegisterEvent("UNIT_AURA")
+RaidBuffsAnchor:RegisterEvent("PLAYER_ENTERING_WORLD")
+RaidBuffsAnchor:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+RaidBuffsAnchor:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+RaidBuffsAnchor:RegisterEvent("GROUP_ROSTER_UPDATE") 
+RaidBuffsAnchor:RegisterUnitEvent("UNIT_AURA", "player")
+RaidBuffsAnchor:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
+RaidBuffsAnchor:RegisterEvent("BAG_UPDATE")
 
--- Create Main bar
-local raidbuff_reminder = CreateFrame("Frame", "RaidBuffReminder", UIParent)
-raidbuff_reminder:CreatePanel("Invisible", (C.reminder.raid_buffs_size * 6) + 15, C.reminder.raid_buffs_size + 4, "TOPLEFT", RaidBuffsAnchor, "TOPLEFT", 0, 4)
-raidbuff_reminder:RegisterEvent("UNIT_AURA")
-raidbuff_reminder:RegisterEvent("PLAYER_ENTERING_WORLD")
-raidbuff_reminder:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-raidbuff_reminder:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-raidbuff_reminder:SetScript("OnEvent", OnAuraChange)
+for i = 1, 12 do
+	local button = CreateFrame("Frame", "RaidBuffReminder", UIParent)
+	button.Icon = button:CreateTexture(nil, "ARTWORK")
+	button.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	button.Icon:SetPoint("TOPLEFT", 2, -2)
+	button.Icon:SetPoint("BOTTOMRIGHT", -2, 2)
 
--- Function to create buttons
-local function CreateButton(name, relativeTo, firstbutton)
-	local button = CreateFrame("Frame", name, RaidBuffReminder)
-	if firstbutton == true then
-		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "BOTTOMLEFT", relativeTo, "BOTTOMLEFT", 0, 0)
-	else
-		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "LEFT", relativeTo, "RIGHT", 3, 0)
+	button.Text = button:CreateFontString(nil, "OVERLAY")
+	button.Text:SetPoint("CENTER")
+	button.Text:SetFont(C.font.stats_font, 14, "OUTLINE")
+
+	if i == 1 then
+		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "BOTTOMLEFT", RaidBuffsAnchor, "BOTTOMLEFT", 0, 0)
+	elseif i > 1 and i < 7 then
+		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "LEFT", BuffFrame[i-1], "RIGHT", 3, 0)
+	elseif i == 7 then
+		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "BOTTOMLEFT", BuffFrame[1], "TOPLEFT", 0, 3)
+	elseif i > 7 then
+		button:CreatePanel("Default", C.reminder.raid_buffs_size, C.reminder.raid_buffs_size, "LEFT", BuffFrame[i-1], "RIGHT", 3, 0)
 	end
-	button:SetFrameLevel(RaidBuffReminder:GetFrameLevel() + 2)
-
-	button.t = button:CreateTexture(name..".t", "OVERLAY")
-	button.t:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-	button.t:SetPoint("TOPLEFT", 2, -2)
-	button.t:SetPoint("BOTTOMRIGHT", -2, 2)
+	
+	button:SetAlpha(0)	
+	table.insert(BuffFrame,button)
 end
 
--- Create Buttons
-do
-	CreateButton("FlaskFrame", RaidBuffReminder, true)
-	CreateButton("FoodFrame", FlaskFrame, false)
-	CreateButton("StatFrame", FoodFrame, false)
-	CreateButton("StaminaFrame", StatFrame, false)
-	CreateButton("Spell5Frame", StaminaFrame, false)
-	CreateButton("Spell6Frame", Spell5Frame, false)
+local function OnAuraChange(self, event, arg1, unit, ...)
+	if event == "UNIT_AURA" and arg1 ~= "player" then return end
+	
+	local _, instanceType = IsInInstance()
+	for key, value in pairs(BuffFrame) do
+		if (not IsInGroup() or instanceType ~= "raid") and C.reminder.raid_buffs_always == false then
+			value:Hide()
+		else
+			value:Show()
+		end
+	end
+	
+	if T.Role == "Caster" or T.Role == "Healer" then
+		Melee = false
+	else
+		Melee = true
+	end
+
+	for i = 1, 9 do
+		if T.ReminderBuffs and T.ReminderBuffs[i][1] then
+			BuffFrame[i]:SetAlpha(1)
+			BuffFrame[i].Icon:SetTexture(select(3, GetSpellInfo(T.ReminderBuffs[i][1])))
+			BuffFrame[i].hasBuff = nil
+			for key, value in pairs(T.ReminderBuffs[i]) do
+				local name = GetSpellInfo(value)
+				if UnitAura("player", name) then
+					BuffFrame[i].Icon:SetTexture(select(3, GetSpellInfo(value)))
+					BuffFrame[i]:SetAlpha(0.2)
+					BuffFrame[i].hasBuff = true
+					BuffFrame[i].name = name
+					break
+				end
+			end
+		end
+	end
+	
+	--攻强/法强
+	if Melee then
+		if T.ReminderBuffs[10] and T.ReminderBuffs[10][1] then
+			BuffFrame[10]:SetAlpha(1)
+			BuffFrame[10].Icon:SetTexture(select(3, GetSpellInfo(T.ReminderBuffs[10][1])))
+			BuffFrame[10].hasBuff = nil
+			for key, value in pairs(T.ReminderBuffs[10]) do
+				local name = GetSpellInfo(value)
+				if UnitAura("player", name) then
+					BuffFrame[10].Icon:SetTexture(select(3, GetSpellInfo(value)))
+					BuffFrame[10]:SetAlpha(0.2)
+					BuffFrame[10].hasBuff = true
+					BuffFrame[10].name = name
+					break
+				end
+			end
+		end
+	else
+		if T.ReminderBuffs[11] and T.ReminderBuffs[11][1] then
+			BuffFrame[10]:SetAlpha(1)
+			BuffFrame[10].Icon:SetTexture(select(3, GetSpellInfo(T.ReminderBuffs[11][1])))
+			BuffFrame[10].hasBuff = nil
+			for key, value in pairs(T.ReminderBuffs[11]) do
+				local name = GetSpellInfo(value)
+				if UnitAura("player", name) then
+					BuffFrame[10].Icon:SetTexture(select(3, GetSpellInfo(value)))
+					BuffFrame[10]:SetAlpha(0.2)
+					BuffFrame[10].hasBuff = true
+					BuffFrame[10].name = name
+					break
+				end
+			end
+		end
+	end
+	--治疗石
+	if T.ReminderBuffs[12] and T.ReminderBuffs[12][1] then
+		local stone = select(10, GetItemInfo(T.ReminderBuffs[12][1]))
+		BuffFrame[11]:SetAlpha(1)
+		BuffFrame[11].Icon:SetTexture(stone)
+		BuffFrame[11].Text:SetText(nil)
+		BuffFrame[11].hasBuff = nil
+		for bag = 0, 4 do
+			for slot = 1, GetContainerNumSlots(bag) do
+				local tex = GetContainerItemInfo(bag, slot)
+				if tex and tex == stone then
+					BuffFrame[11]:SetAlpha(0.2)
+					BuffFrame[11].Text:SetText(GetItemCount(T.ReminderBuffs[12][1], nil, true))
+					BuffFrame[11].hasBuff = true
+					BuffFrame[11].info = {bag, slot}
+					break
+				end
+			end
+		end					
+	end
+	
+	if T.ReminderBuffs[13] and T.ReminderBuffs[13][1] then
+		BuffFrame[12]:SetAlpha(1)
+		BuffFrame[12].Icon:SetTexture(select(3, GetSpellInfo(T.ReminderBuffs[13][1])))
+		BuffFrame[12].hasBuff = nil
+		for key, value in pairs(T.ReminderBuffs[13]) do
+			local name = GetSpellInfo(value)
+			if UnitAura("player", name) then
+				BuffFrame[12].Icon:SetTexture(select(3, GetSpellInfo(value)))
+				BuffFrame[12]:SetAlpha(0.2)
+				BuffFrame[12].hasBuff = true
+				BuffFrame[12].name = name
+				break
+			end
+		end
+	end
 end
+
+for i = 1, 12 do
+	local function Button_OnEnter(self)
+		GameTooltip:Hide()
+		GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT", -5, -5)
+		GameTooltip:ClearLines()
+
+		if(self.hasBuff) then
+			if i == 11 then
+				GameTooltip:SetBagItem(unpack(BuffFrame[11].info))
+			else
+				GameTooltip:SetUnitAura("player", BuffFrame[i].name)
+			end
+		else	
+			
+			if i == 1 then text = "药剂"
+			elseif i == 2 then text = "食物"
+			elseif i == 3 then text = "属性"
+			elseif i == 4 then text = "耐力"
+			elseif i == 5 then text = "暴击"
+			elseif i == 6 then text = "精通"
+			elseif i == 7 then text = "溅射"
+			elseif i == 8 then text = "全能"
+			elseif i == 9 then text = "急速"
+			elseif i == 10 then
+				if Melee then text = "攻强" else text = "法强" end
+			elseif i == 11 then text = "治疗石"
+			elseif i == 12 then text = "符文"
+			end
+			GameTooltip:SetText(text, 1, 1, 1)
+		end
+		GameTooltip:Show()
+	end
+	
+	BuffFrame[i]:SetScript("OnEnter", Button_OnEnter)
+	BuffFrame[i]:SetScript("OnLeave", GameTooltip_Hide)
+end
+
+RaidBuffsAnchor:SetScript("OnEvent", OnAuraChange)
