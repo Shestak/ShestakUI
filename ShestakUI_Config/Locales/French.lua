0d4f83361267f30724cca80db0f47e78951d781d
Tryllemann <frode.h@aktivportal.com>
2015-05-30 13:18:25 +0200

    Update French.lua

--- ShestakUI_Config/Locales/French.lua
+++ ShestakUI_Config/Locales/French.lua
@@ -1,4 +1,4 @@
-﻿if GetLocale() ~= "frFR" then return end
+if GetLocale() ~= "frFR" then return end
 
 ----------------------------------------------------------------------------------------
 --	Localization for frFR client
@@ -356,6 +356,7 @@ L_GUI_UF_PLUGINS_DIMINISHING = "Diminishing Returns icons on arena frames" -- Ne
 -- Unit Frames Class bar options
 L_GUI_UF_PLUGINS_CLASS_BAR = "Unit Frames Class bars" -- Needs review
 L_GUI_UF_PLUGINS_COMBO_BAR = "Affichage des points de combo pour les Voleurs/Druides"
+L_GUI_UF_PLUGINS_COMBO_BAR_ALWAYS = "Always show Combo bar for Druid"
 L_GUI_UF_PLUGINS_SHADOW_BAR = "Enable Shadow Orbs bar" -- Needs review
 L_GUI_UF_PLUGINS_CHI_BAR = "Enable Chi bar" -- Needs review
 L_GUI_UF_PLUGINS_VENGEANCE_BAR = "Active la barre de menace"
