3039c85dd074be72a486d3cd4c1fd74eea642e10
leg883 <leg883@126.com>
2015-06-23 23:13:15 +0800

    Update Positions.lua

--- ShestakUI_Filger/Config/Positions.lua
+++ ShestakUI_Filger/Config/Positions.lua
@@ -4,14 +4,16 @@ if C.unitframe.enable ~= true then return end
 C["filger_position"] = {
 	player_buff = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 173},	-- "P_BUFF"
 	player_proc = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 213},	-- "P_PROC"
-	
 	player_special = {"BOTTOMLEFT", "oUF_Player", "TOPRIGHT", 33, 80},	-- "P_SPECIAL"
+	
+	player_raid = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 253},	-- "P_RAID"
+	target_raid = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 253},		-- "T_RAID"
 
 	target_buff = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 173},		-- "T_BUFF"
 	target_proc = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 213},		-- "T_PROC"
 	
-	pve_player = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 253},		-- "PVE/PVP_P"
-	pve_target = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 253},		-- "PVE/PVP_T"
+	pve_player = {"BOTTOMRIGHT", "oUF_Player", "TOPRIGHT", 2, 333},		-- "PVE/PVP_P"
+	pve_target = {"BOTTOMLEFT", "oUF_Target", "TOPLEFT", -2, 333},		-- "PVE/PVP_T"
 	
 	pve_cc = {"TOPLEFT", "oUF_Player", "BOTTOMLEFT", -2, -44},		-- "PVE/PVP_CC"
 	
