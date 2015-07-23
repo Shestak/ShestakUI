7fee41b82578bfb1dc80340125cbea8b9db2d3c0
Tryllemann <frode.h@aktivportal.com>
2015-07-23 18:58:34 +0200

    Temp fix for Blizzards bug
    killing the anoying Blizz error "Interface\FrameXML\RaidFrame.lua:181"

--- ShestakUI/Core/Temp.lua
+++ ShestakUI/Core/Temp.lua
@@ -1,5 +1,129 @@
-﻿local T, C, L, _ = unpack(select(2, ...))
+local T, C, L, _ = unpack(select(2, ...))
 
 ----------------------------------------------------------------------------------------
 --	Temporary stuff
 ----------------------------------------------------------------------------------------
+-- killing the anoying Blizz error "Interface\FrameXML\RaidFrame.lua:181" - by Semlar
+function RaidInfoFrame_Update(scrollToSelected)
+	RaidInfoFrame_UpdateSelectedIndex();
+	
+	local scrollFrame = RaidInfoScrollFrame;
+	local savedInstances = GetNumSavedInstances();
+	local savedWorldBosses = GetNumSavedWorldBosses();
+	local instanceName, instanceID, instanceReset, instanceDifficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName;
+	local frameName, frameNameText, frameID, frameReset, width;
+	local offset = HybridScrollFrame_GetOffset(scrollFrame);
+	local buttons = scrollFrame.buttons;
+	local numButtons = #buttons;
+	local buttonHeight = buttons[1]:GetHeight();
+	
+	if ( scrollToSelected == true and RaidInfoFrame.selectedIndex ) then --Using == true in case the HybridScrollFrame .update is changed to pass in the parent.
+		local button = buttons[RaidInfoFrame.selectedIndex - offset]
+		if ( not button or (button:GetTop() > scrollFrame:GetTop()) or (button:GetBottom() < scrollFrame:GetBottom()) ) then
+			local scrollFrame = RaidInfoScrollFrame;
+			local buttonHeight = scrollFrame.buttons[1]:GetHeight();
+			local scrollValue = min(((RaidInfoFrame.selectedIndex - 1) * buttonHeight), scrollFrame.range)
+			if ( scrollValue ~= scrollFrame.scrollBar:GetValue() ) then
+				scrollFrame.scrollBar:SetValue(scrollValue);
+			end
+		end
+	end
+
+	offset = HybridScrollFrame_GetOffset(scrollFrame);	--May have changed in the previous section to move selected parts into view.
+
+	local mouseIsOverScrollFrame = scrollFrame:IsVisible() and scrollFrame:IsMouseOver();
+
+	for i=1, numButtons do
+		local frame = buttons[i];
+		local index = i + offset;
+
+		if ( index <= savedInstances + savedWorldBosses) then
+			if (index <= savedInstances) then
+				instanceName, instanceID, instanceReset, instanceDifficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName = GetSavedInstanceInfo(index);
+				frame.worldBossID = nil;
+				frame.instanceID = instanceID;
+				--frame.longInstanceID = string.format("%x%x", instanceIDMostSig, instanceID);
+				frame.longInstanceID = instanceIDMostSig .. '.' .. instanceID
+			else
+				instanceName, instanceID, instanceReset = GetSavedWorldBossInfo(index - savedInstances);
+				locked = true;
+				extended = false;
+				difficultyName = RAID_INFO_WORLD_BOSS;
+				frame.worldBossID = instanceID;
+				frame.instanceID = nil;
+				frame.longInstanceID = nil;
+			end
+			
+			frame:SetID(index);
+
+			if ( RaidInfoFrame.selectedIndex == index ) then
+				frame:LockHighlight();
+			else
+				frame:UnlockHighlight();
+			end
+
+			frame.difficulty:SetText(difficultyName);
+
+			if ( extended or locked ) then
+				frame.reset:SetText(SecondsToTime(instanceReset, true, nil, 3));
+				frame.name:SetText(instanceName);
+			else
+				frame.reset:SetFormattedText("|cff808080%s|r", RAID_INSTANCE_EXPIRES_EXPIRED);
+				frame.name:SetFormattedText("|cff808080%s|r", instanceName);
+			end
+			
+			if ( extended ) then
+				frame.extended:Show();
+			else
+				frame.extended:Hide();
+			end
+			
+			frame:Show();
+			
+			if ( mouseIsOverScrollFrame and frame:IsMouseOver() ) then
+				RaidInfoInstance_OnEnter(frame);
+			end
+		else
+			frame:Hide();
+		end	
+	end
+	HybridScrollFrame_Update(scrollFrame, (savedInstances + savedWorldBosses) * buttonHeight, scrollFrame:GetHeight());
+end
+
+function RaidInfoFrame_UpdateSelectedIndex()
+	if (RaidInfoFrame.selectedRaidID) then
+		local savedInstances = GetNumSavedInstances();
+		for index=1, savedInstances do
+			local instanceName, instanceID, instanceReset, instanceDifficulty, locked, extended, instanceIDMostSig = GetSavedInstanceInfo(index);
+			if ( (instanceIDMostSig .. '.' .. instanceID) == RaidInfoFrame.selectedRaidID ) then
+				RaidInfoFrame.selectedIndex = index;
+				RaidInfoExtendButton:Enable();
+				if ( extended ) then
+					RaidInfoExtendButton.doExtend = false;
+					RaidInfoExtendButton:SetText(UNEXTEND_RAID_LOCK);
+				else
+					RaidInfoExtendButton.doExtend = true;
+					if ( locked ) then
+						RaidInfoExtendButton:SetText(EXTEND_RAID_LOCK);
+					else
+						RaidInfoExtendButton:SetText(REACTIVATE_RAID_LOCK);
+					end
+				end
+				return;
+			end
+		end
+	elseif (RaidInfoFrame.selectedWorldBossID) then
+		local savedInstances = GetNumSavedWorldBosses();
+		for index=1, savedInstances do
+			local _, worldBossID, _ = GetSavedWorldBossInfo(index);
+			if (worldBossID == RaidInfoFrame.selectedWorldBossID) then
+				RaidInfoExtendButton:SetText(EXTEND_RAID_LOCK);
+				RaidInfoExtendButton:Disable();
+				RaidInfoFrame.selectedIndex = index + GetNumSavedInstances();
+				return;
+			end
+		end
+	end
+	RaidInfoFrame.selectedIndex = nil;
+	RaidInfoExtendButton:Disable();
+end
