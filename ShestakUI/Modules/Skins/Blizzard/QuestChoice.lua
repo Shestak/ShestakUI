f75c63c0c1cf6e73a6872c7caa3350498b3fc7a0
leg883 <leg883@126.com>
2015-08-12 21:56:53 +0800

    Update QuestChoice.lua

--- ShestakUI/Modules/Skins/Blizzard/QuestChoice.lua
+++ ShestakUI/Modules/Skins/Blizzard/QuestChoice.lua
@@ -6,25 +6,24 @@ if C.skins.blizzard_frames ~= true then return end
 ----------------------------------------------------------------------------------------
 
 local function LoadSkin()
-
-    for i = 1, 2 do
-        local option = QuestChoiceFrame["Option"..i]
-        local rewards = option.Rewards
-        local icon = rewards.Item.Icon
-        local currencies = rewards.Currencies
+	for i = 1, 2 do
+		local option = QuestChoiceFrame["Option"..i]
+		local rewards = option.Rewards
+		local icon = rewards.Item.Icon
+		local currencies = rewards.Currencies
 
 		T.HandleIcon(icon)
 
-        for j = 1, 3 do
-            local cu = currencies["Currency"..j]
+		for j = 1, 3 do
+			local cu = currencies["Currency"..j]
 			T.HandleIcon(cu.Icon)
-        end
-    end
+		end
+	end
 
 	QuestChoiceFrame:CreateBackdrop("Transparent")
-    QuestChoiceFrame.Option1.OptionButton:SkinButton()
-    QuestChoiceFrame.Option2.OptionButton:SkinButton()
-    T.SkinCloseButton(QuestChoiceFrame.CloseButton)
+	QuestChoiceFrame.Option1.OptionButton:SkinButton()
+	QuestChoiceFrame.Option2.OptionButton:SkinButton()
+	T.SkinCloseButton(QuestChoiceFrame.CloseButton)
 	QuestChoiceFrame.CloseButton:SetFrameLevel(10)
 end
 
