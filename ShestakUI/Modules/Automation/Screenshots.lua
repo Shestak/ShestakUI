829b5161845ae13bbcf8ca56e0a6a4df09973159
leg883 <leg883@126.com>
2015-08-12 22:33:40 +0800

    Update Screenshots.lua

--- ShestakUI/Modules/Automation/Screenshots.lua
+++ ShestakUI/Modules/Automation/Screenshots.lua
@@ -4,10 +4,10 @@ if C.automation.screenshot ~= true then return end
 ----------------------------------------------------------------------------------------
 --  Take screenshots of defined events (Sinaris)
 ----------------------------------------------------------------------------------------
-local function OnEvent( self, event, ... )
-    C_Timer.After( 1, function() Screenshot() end )
+local function OnEvent(self, event, ...)
+    C_Timer.After(1, function() Screenshot() end)
 end
 
-local EventFrame = CreateFrame( 'Frame' )
-EventFrame:RegisterEvent( 'ACHIEVEMENT_EARNED' )
-EventFrame:SetScript( 'OnEvent', OnEvent )
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("ACHIEVEMENT_EARNED")
+EventFrame:SetScript("OnEvent", OnEvent)
