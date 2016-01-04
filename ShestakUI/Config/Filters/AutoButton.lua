a5d06c5077d6811e2b2d7c972e649b8099c24ab8
Wetxius <wetxius@gmail.com>
2016-01-04 19:06:38 +0300

    Deleted old items and resort in AutoButton.

--- ShestakUI/Config/Filters/AutoButton.lua
+++ ShestakUI/Config/Filters/AutoButton.lua
@@ -8,41 +8,36 @@ if C.misc.quest_auto_button ~= true then return end
 ----------------------------------------------------------------------------------------
 T.ABItems = {
 	-- Daily Quests
-	52507,	-- Stardust No.2
-	69981,	-- Ironforge Rations
-	69240,	-- Enchanted Salve
-	69235,	-- Fang of the Wolf
-	71978,	-- Darkmoon Bandage
-	62829,	-- Magnetized Scrap Collector
-	79344,	-- Pomharvest Firework
-	79885,	-- Barrel of Fireworks
-	80599,	-- Goblin Fishing Bomb
-	87841,	-- Korven's Experimental Grenades
-	82807,	-- Shado-Pan Dragon Gun
-	84242,	-- Shado-Pan Bandages
-	85261,	-- Shado-Pan Torch
-	89123,	-- Wu Kao Explosive
-	85950,	-- Mistfall Water Bucket
-	86532,	-- Bag of Shado-Pan Gas Bombs
-	77475,	-- Stack of Mantras
-	80403,	-- Angler's Fishing Spear
-	80127,	-- Shadelight Truffle Spores
-	87394,	-- Sonic Disruption Fork
-	84781,	-- Shado-Pan Torch
-	82381,	-- Yak's Milk Flask
-	84762,	-- Highly Explosive Yaungol Oil
 	32971,	-- Water Bucket
-	85581,	-- Shado-Pan Torch
-	85884,	-- Sonic Emitter
-	82346,	-- Pot of Fire
+	38689,	-- Chicken Net
+	52507,	-- Stardust No.2
+	62829,	-- Magnetized Scrap Collector
+	63351,	-- Tahret Dynasty Mallet
+	69235,	-- Fang of the Wolf
+	69240,	-- Enchanted Salve
+	69981,	-- Ironforge Rations
+	71978,	-- Darkmoon Bandage
+	77475,	-- Stack of Mantras
 	78947,	-- Silken Rope
+	79885,	-- Barrel of Fireworks
+	80127,	-- Shadelight Truffle Spores
+	80403,	-- Angler's Fishing Spear
+	80599,	-- Goblin Fishing Bomb
+	82346,	-- Pot of Fire
+	82381,	-- Yak's Milk Flask
+	82807,	-- Shado-Pan Dragon Gun
 	83134,	-- Bronze Claws
-	93180,	-- Re-Configured Remote
+	84762,	-- Highly Explosive Yaungol Oil
+	85884,	-- Sonic Emitter
+	86532,	-- Bag of Shado-Pan Gas Bombs
+	87394,	-- Sonic Disruption Fork
+	87841,	-- Korven's Experimental Grenades
 	92019,	-- The Bilgewater Molotov
-	93751,	-- Blessed Torch
+	93180,	-- Re-Configured Remote
 	93668,	-- Saur Fetish
-	93806,	-- Resonance Siphon
+	93751,	-- Blessed Torch
 	93761,	-- Arcane Emancipator
+	93806,	-- Resonance Siphon
 	-- Seaforium
 	46847,	-- Seaforium Bombs
 	47030,	-- Huge Seaforium Bombs
