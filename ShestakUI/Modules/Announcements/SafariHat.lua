4b6548aee848f00db90c09cbe912e55a4914ed31
Wetxius <wetxius@gmail.com>
2016-02-14 16:45:13 +0300

    Fixed Safari Hat announce.

--- ShestakUI/Modules/Announcements/SafariHat.lua
+++ ShestakUI/Modules/Announcements/SafariHat.lua
@@ -8,7 +8,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("PET_BATTLE_OPENING_DONE")
 frame:SetScript("OnEvent", function()
 	local name = GetSpellInfo(158486)
-	if not UnitAura("player", name) and GetItemCount(92738) > 0 then
+	if PlayerHasToy(92738) and not UnitAura("player", name) then
 		PlaySound("RaidWarning", "master")
 		RaidNotice_AddMessage(RaidWarningFrame, RESISTANCE_NONE.." "..GetSpellLink(158486).."!", ChatTypeInfo["RAID_WARNING"])
 		print("|cffff3300"..RESISTANCE_NONE.." "..GetSpellLink(158486).."|cffff3300!|r")
