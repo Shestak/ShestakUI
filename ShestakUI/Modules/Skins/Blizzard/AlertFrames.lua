e01f9238de39b88802b3bb3ce1f179e058d8e789
Wetxius <wetxius@gmail.com>
2016-02-16 18:18:59 +0300

    Hide background for GarrisonFollowerAlertFrame.

--- ShestakUI/Modules/Skins/Blizzard/AlertFrames.lua
+++ ShestakUI/Modules/Skins/Blizzard/AlertFrames.lua
@@ -524,7 +524,7 @@ local function LoadSkin()
 	hooksecurefunc("AlertFrame_SetGarrisonBuildingAlertFrameAnchors", SkinGarrisonBuildingPopUp)
 
 	local function SkinGarrisonMissionPopUp()
-		local frame = GarrisonMissionAlertFrame
+		local frame = _G["GarrisonMissionAlertFrame"]
 
 		if frame then
 			frame:SetAlpha(1)
@@ -569,22 +569,10 @@ local function LoadSkin()
 			end
 
 			-- Background
-			frame:GetRegions():Hide()
+			frame:StripTextures()
 			frame.FollowerBG:SetAlpha(0)
 			frame.glow:Kill()
 			frame.shine:Kill()
-
-			-- Icon
-			--frame.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-
-			-- Icon border
-			-- if not frame.Icon.b then
-				-- frame.Icon.b = CreateFrame("Frame", nil, frame)
-				-- frame.Icon.b:SetFrameLevel(2)
-				-- frame.Icon.b:SetTemplate("Default")
-				-- frame.Icon.b:SetPoint("TOPLEFT", frame.Icon, "TOPLEFT", -2, 2)
-				-- frame.Icon.b:SetPoint("BOTTOMRIGHT", frame.Icon, "BOTTOMRIGHT", 2, -2)
-			-- end
 		end
 	end
 	hooksecurefunc("AlertFrame_SetGarrisonFollowerAlertFrameAnchors", SkinGarrisonFollowerPopUp)
