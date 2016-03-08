9f7b144381a34cb7f52508000bdf662cc7776f8a
Wetxius <wetxius@gmail.com>
2016-03-08 19:51:20 +0300

    Added some aoespam spells for Monk.

--- ShestakUI/Config/Filters/CombatText.lua
+++ ShestakUI/Config/Filters/CombatText.lua
@@ -175,6 +175,7 @@ elseif T.class == "MONK" then
 		T.merge[173438] = 159620	-- Chi Explosion
 		T.merge[182078] = 159620	-- Chi Explosion
 		T.merge[173439] = 159620	-- Chi Explosion
+		T.aoespam[178173] = 3		-- Gift of the Ox
 		-- Damaging spells
 		T.aoespam[117952] = 3		-- Crackling Jade Lightning
 		T.aoespam[117418] = 3		-- Fists of Fury
@@ -191,6 +192,7 @@ elseif T.class == "MONK" then
 		T.aoespam[158221] = 3		-- Hurricane Strike
 		T.aoespam[152174] = 3		-- Chi Explosion
 		T.merge[157680] = 152174	-- Chi Explosion
+		T.aoespam[157676] = 1		-- Chi Explosion
 	end
 elseif T.class == "PALADIN" then
 	if C.combattext.merge_aoe_spam then
