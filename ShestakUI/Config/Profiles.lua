b5976e0f97797dd9bd439d561f4fe71f0b08e5c9
Wetxius <wetxius@gmail.com>
2016-03-09 18:51:26 +0300

    Added my config.

--- ShestakUI/Config/Profiles.lua
+++ ShestakUI/Config/Profiles.lua
@@ -184,3 +184,80 @@ or T.name == "Чакернаут" or T.name == "Чаккис" or T.name == "Эз
 	C["toppanel"].enable = false
 	C["position"].quest = {"TOPRIGHT", UIParent, "TOPRIGHT", -52, -73}
 end
+
+----------------------------------------------------------------------------------------
+--	Wetxius Config
+----------------------------------------------------------------------------------------
+if IsWetxius then
+	C["general"].welcome_message = false
+	C["misc"].shift_marking = false
+	C["misc"].vehicle_mouseover = true
+	C["misc"].quest_auto_button = true
+	C["misc"].disenchanting = true
+	C["misc"].click_cast = true
+	C["misc"].enchantment_scroll = true
+	C["announcements"].bad_gear = true
+	C["automation"].accept_invite = true
+	C["automation"].accept_quest = true
+	C["automation"].auto_collapse = true
+	C["automation"].auto_role = true
+	C["automation"].tab_binder = true
+	C["skins"].blizzard_frames = true
+	C["skins"].minimap_buttons = true
+	C["skins"].bigwigs = true
+	C["combattext"].crit_prefix = ""
+	C["combattext"].blizz_head_numbers = true
+	C["combattext"].dk_runes = false
+	C["pulsecooldown"].enable = true
+	C["pulsecooldown"].threshold = 8
+	C["threat"].enable = false
+	C["tooltip"].hide_combat = true
+	C["tooltip"].rank = false
+	C["tooltip"].spell_id = true
+	C["tooltip"].unit_role = true
+	C["chat"].background = true
+	C["chat"].spam = true
+	C["chat"].damage_meter_spam = true
+	C["chat"].chat_bar = true
+	C["chat"].chat_bar_mouseover = true
+	C["chat"].combatlog = false
+	C["minimap"].toggle_menu = false
+	C["nameplate"].health_value = true
+	C["nameplate"].healer_icon = true
+	C["actionbar"].toggle_mode = false
+	C["actionbar"].bottombars = 3
+	C["aura"].show_spiral = true
+	C["aura"].cast_by = true
+	C["filger"].show_tooltip = true
+	C["unitframe"].castbar_icon = true
+	C["unitframe"].plugins_enemy_spec = true
+	C["raidframe"].solo_mode = true
+	C["raidframe"].icons_leader = false
+	C["toppanel"].enable = false
+	C["stats"].fps = false
+	C["stats"].battleground = true
+	C["stats"].currency_cooking = false
+	C["stats"].currency_professions = false
+	C["stats"].currency_raid = false
+	C["stats"].currency_pvp = false
+	C["stats"].currency_misc = false
+	C["font"].stats_font = C.media.normal_font
+	C["font"].stats_font_style = "OUTLINE"
+	C["font"].stats_font_size = 12
+	C["position"].bag = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -21, 20}
+	C["position"].bank = {"BOTTOMLEFT", UIParent, "BOTTOMLEFT", 21, 20}
+	C["position"].auto_button = {"BOTTOMLEFT", "oUF_Player", "TOPRIGHT", 33, 83}
+	C["position"].arena = {"BOTTOMRIGHT", UIParent, "RIGHT", -55, -70}
+	if T.level == MAX_PLAYER_LEVEL then
+		C["automation"].screenshot = true
+		C["combattext"].heal_treshold = 1000
+		C["stats"].experience = false
+	end
+
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("PLAYER_LOGIN")
+	frame:SetScript("OnEvent", function()
+		SetCVar("lootUnderMouse", 1)
+		SetCVar("cameraDistanceMoveSpeed", 50)
+	end)
+end
