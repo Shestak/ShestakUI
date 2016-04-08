7659bcea417d439c8a833be1b2689939acfe986b
Wetxius <wetxius@gmail.com>
2016-04-08 14:19:33 +0300

    Cleanup.

--- ShestakUI/Libs/LiteStats/LiteStats.lua
+++ ShestakUI/Libs/LiteStats/LiteStats.lua
@@ -920,15 +920,6 @@ if guild.enabled then
 			end
 		end)
 	end
-	local function ShortValueXP(v)
-		if v >= 1e6 then
-			return ("%.1fm"):format(v / 1e6):gsub("%.?0+([km])$", "%1")
-		elseif v >= 1e3 or v <= -1e3 then
-			return ("%.1fk"):format(v / 1e3):gsub("%.?0+([km])$", "%1")
-		else
-			return v
-		end
-	end
 	hooksecurefunc("SortGuildRoster", function(type) CURRENT_GUILD_SORTING = type end)
 	Inject("Guild", {
 		text = {
