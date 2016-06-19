f9c6bf80dd1b48bfd7bbb7a1b1f86a746bf07ef6
Wetxius <wetxius@gmail.com>
2016-06-19 16:43:13 +0300

    Changed GetItemTransmogrifyInfo => C_Transmog.GetItemInfo.

--- ShestakUI/Modules/Tooltip/CheckItemTransmogrify.lua
+++ ShestakUI/Modules/Tooltip/CheckItemTransmogrify.lua
@@ -38,7 +38,7 @@ local WIMtooltip = function(tooltip)
 	local _, _, quality, _, _, itemType, subType, _, slot = GetItemInfo(itemID)
 	-- No weapon or armor, or misc 'weapon', or invalid slot
 	if not itemType or not (itemType == ARMOR or itemType == ENCHSLOT_WEAPON) or (subType == MISCELLANEOUS and (itemType == ENCHSLOT_WEAPON or slot == "INVTYPE_CLOAK")) or not locs[slot] then return end
-	local canBeChanged, noChangeReason, canBeSource, noSourceReason = GetItemTransmogrifyInfo(itemID)
+	local canBeChanged, noChangeReason, canBeSource, noSourceReason = C_Transmog.GetItemInfo(itemID)
 
 	if rndench and rndench ~= "0" and noSourceReason == "NO_STATS" then
 		canBeChanged = true
