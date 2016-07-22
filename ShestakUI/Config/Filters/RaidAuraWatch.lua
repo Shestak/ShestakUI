40d68938a2a2fe946c3c2401eb9dea20869899d1
Wetxius <wetxius@gmail.com>
2016-07-22 21:31:51 +0300

    Updated RaidAuraWatch for Monk.

--- ShestakUI/Config/Filters/RaidAuraWatch.lua
+++ ShestakUI/Config/Filters/RaidAuraWatch.lua
@@ -17,8 +17,8 @@ T.RaidBuffs = {
 	},
 	MONK = {
 		{119611, "TOPRIGHT", {0.2, 0.7, 0.7}},				-- Renewing Mist
-		--BETA {132120, "BOTTOMLEFT", {0.4, 0.8, 0.2}},			-- Enveloping Mist
-		{124081, "BOTTOMRIGHT", {0.7, 0.4, 0}},				-- Zen Sphere
+		{124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}},			-- Enveloping Mist
+		{115175, "BOTTOMRIGHT", {0.7, 0.4, 0}},				-- Soothing Mist
 		{116849, "LEFT", {0.81, 0.85, 0.1}, true},			-- Life Cocoon
 	},
 	PALADIN = {
