b713af9ab714044f134e1c735f49576559763679
Wetxius <wetxius@gmail.com>
2016-07-22 21:38:08 +0300

    Added new Rogue's procs.

--- ShestakUI/Config/Filters/FilgerSpells.lua
+++ ShestakUI/Config/Filters/FilgerSpells.lua
@@ -2427,6 +2427,19 @@ C["filger_spells"] = {
 			-- Blindside
 			--BETA {spellID = 121153, unitID = "player", caster = "player", filter = "BUFF"},
 
+			-- Jolly Roger
+			{spellID = 199603, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Grand Melee
+			{spellID = 193358, unitID = "player", caster = "player", filter = "BUFF"},
+			-- True Bearing
+			{spellID = 193359, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Buried Treasure
+			{spellID = 199600, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Broadsides
+			{spellID = 193356, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Shark Infested Waters
+			{spellID = 193357, unitID = "player", caster = "player", filter = "BUFF"},
+
 			-- Item Sets
 			-- Deathly Shadows (T18)
 			{spellID = 188700, unitID = "player", caster = "player", filter = "BUFF"},
