c3cd3c240a6e966a2ea5570283c1d487fe1d259a
Wetxius <wetxius@gmail.com>
2016-07-29 17:53:12 +0300

    Added addon check to use "/kb".

--- ShestakUI/Modules/ActionBars/Bindings.lua
+++ ShestakUI/Modules/ActionBars/Bindings.lua
@@ -340,7 +340,11 @@ end
 SLASH_MOUSEOVERBIND1 = "/bindkey"
 SLASH_MOUSEOVERBIND2 = "/hoverbind"
 SLASH_MOUSEOVERBIND3 = "/bk"
-SLASH_MOUSEOVERBIND4 = "/kb"
+
+if not IsAddOnLoaded("Bartender4") and not IsAddOnLoaded("Dominos") then
+	SLASH_MOUSEOVERBIND4 = "/kb"
+end
+
 if not IsAddOnLoaded("HealBot") then
 	SLASH_MOUSEOVERBIND5 = "/hb"
 end
