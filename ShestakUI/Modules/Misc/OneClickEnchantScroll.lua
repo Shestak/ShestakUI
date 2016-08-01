12f79bb34d6e0857aa4f57286c51d3a9116e3bd6
Wetxius <wetxius@gmail.com>
2016-08-01 10:55:27 +0300

    Updated OneClickEnchantScroll.

--- ShestakUI/Modules/Misc/OneClickEnchantScroll.lua
+++ ShestakUI/Modules/Misc/OneClickEnchantScroll.lua
@@ -11,40 +11,35 @@ frame:SetScript("OnEvent", function(self, event, addon)
 		local button = CreateFrame("Button", "TradeSkillCreateScrollButton", TradeSkillFrame, "MagicButtonTemplate")
 		if C.skins.blizzard_frames == true then
 			button:SkinButton(true)
-			button:SetPoint("TOPRIGHT", TradeSkillCreateButton, "TOPLEFT", -1, 0)
+			button:SetPoint("TOPRIGHT", TradeSkillFrame.DetailsFrame.CreateButton, "TOPLEFT", -1, 0)
 		else
-			button:SetPoint("TOPRIGHT", TradeSkillCreateButton, "TOPLEFT")
+			button:SetPoint("TOPRIGHT", TradeSkillFrame.DetailsFrame.CreateButton, "TOPLEFT")
 		end
 		button:SetScript("OnClick", function()
-			DoTradeSkill(TradeSkillFrame.selectedSkill)
+			C_TradeSkillUI.CraftRecipe(TradeSkillFrame.DetailsFrame.selectedRecipeID)
 			UseItemByName(38682)
 		end)
 
-		hooksecurefunc(TradeSkillDetailsMixin, "RefreshButtons", function(id)
-			local skillName, _, _, _, altVerb = GetTradeSkillInfo(id)
-			if IsTradeSkillGuild() or IsTradeSkillLinked() then
+		hooksecurefunc(TradeSkillFrame.RecipeList, "OnRecipeButtonClicked", function(_, recipeInfo)
+			if C_TradeSkillUI.IsTradeSkillGuild() or C_TradeSkillUI.IsNPCCrafting() or C_TradeSkillUI.IsTradeSkillLinked() then
 				button:Hide()
-			elseif altVerb and CURRENT_TRADESKILL == GetSpellInfo(7411) then
-				button:Show()
-				local creatable = 1
-				if not skillName then
-					creatable = nil
-				end
-				local scrollnum = GetItemCount(38682)
-				button:SetText(L_MISC_SCROLL.." ("..scrollnum..")")
-				if scrollnum == 0 then
-					creatable = nil
-				end
-				for i = 1, GetTradeSkillNumReagents(id) do
-					local _, _, reagentCount, playerReagentCount = GetTradeSkillReagentInfo(id, i)
-					if playerReagentCount < reagentCount then
-						creatable = nil
+			elseif recipeInfo.tradeSkillInfo.alternateVerb then
+				local _, tradeSkillName = C_TradeSkillUI.GetTradeSkillLine()
+				if tradeSkillName == GetSpellInfo(7411) then
+					button:Show()
+					local numCreateable = recipeInfo.tradeSkillInfo.numAvailable
+					local numScrollsAvailable = GetItemCount(38682)
+					button:SetText(L_MISC_SCROLL.." ("..numScrollsAvailable..")")
+					 if numScrollsAvailable == 0 then
+						numCreateable = 0
 					end
-				end
-				if creatable then
-					button:Enable()
+					if numCreateable > 0 then
+						button:Enable()
+					else
+						button:Disable()
+					end
 				else
-					button:Disable()
+					button:Hide()
 				end
 			else
 				button:Hide()
