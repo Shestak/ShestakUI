06b7c021f4cb02cdcb612b5dd85ed0b30032ccfc
Wetxius <wetxius@gmail.com>
2016-08-05 11:55:30 +0300

    Increased cameraDistanceMaxFactor for beta/ptr.

--- ShestakUI/Core/Launch.lua
+++ ShestakUI/Core/Launch.lua
@@ -6,7 +6,7 @@
 local function InstallUI()
 	-- Don't need to set CVar multiple time
 	SetCVar("screenshotQuality", 8)
-	SetCVar("cameraDistanceMaxFactor", 2)
+	SetCVar("cameraDistanceMaxFactor", 2.6)
 	SetCVar("showTutorials", 0)
 	SetCVar("gameTip", "0")
 	SetCVar("UberTooltips", 1)
