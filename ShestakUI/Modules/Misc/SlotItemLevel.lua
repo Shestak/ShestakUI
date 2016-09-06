59d54fa774d2e1f85bf47bb4a9d447ffd66c5362
max41479 <cod41479@list.ru>
2016-09-06 14:03:56 +0300

    Detect equipped items ilvl without tooltip scan. LIst of legion item upgrades is not complete. Artifact ilvl still detected via tooltip scan.

--- ShestakUI/Modules/Misc/SlotItemLevel.lua
+++ ShestakUI/Modules/Misc/SlotItemLevel.lua
@@ -20,6 +20,11 @@ local upgrades = {
 	["507"] = 24, ["530"] = 5, ["531"] = 10, ["535"] = 15, ["536"] = 30, ["537"] = 45
 }
 
+local legionUpgrades = {
+	["664"] = 689, ["767"] = 685, ["768"] = 693, ["1735"] = 705, ["1736"] = 709, ["1738"] = 709,
+	["1739"] = 703, ["1741"] = 713, ["1792"] = 699, ["1793"] = 703, ["1794"] = 695, ["1795"] = 700,
+}
+
 local function BOALevel(level, id)
 	if level > 97 then
 		if id == 133585 or id == 133595 or id == 133596 or id == 133597 or id == 133598 then
@@ -62,7 +67,7 @@ local tooltip = CreateFrame("GameTooltip", "ShestakUI_ItemScanningTooltip", UIPa
 tooltip:SetOwner(UIParent, "ANCHOR_NONE")
 
 --Scan tooltip for item level information and cache the value (cache except artifact)
-local function GetItemLevel(itemLink, quality)
+local function GetItemLevel(itemLink)
 	if not itemLink or not GetItemInfo(itemLink) then
 		return
 	end
@@ -80,7 +85,6 @@ local function GetItemLevel(itemLink, quality)
 		end
 	end
 
-	return itemLevelCache[itemLink]
 end
 
 local function CreateButtonsText(frame)
@@ -132,7 +136,38 @@ local function UpdateButtonsText(frame)
 							level = level + upgrades[uid]
 						end
 
-						level = GetItemLevel(itemLink, quality)
+						local numBonusIDs = tonumber(strmatch(itemLink, ".+:%d+:512:%d*:(%d+):"))
+						if numBonusIDs then
+							if numBonusIDs == 1 then
+								local bid1, levelLootedAt = strmatch(itemLink, ".+:%d+:512:%d*:%d+:(%d+):(%d+):")
+								if legionUpgrades[bid1] == nil then
+									print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid1 .. ". Item: " .. itemLink)
+									--print(itemLink)
+									--local printable = gsub(itemLink, "\124", "\124\124");
+									--ChatFrame1:AddMessage("Itemlink: \"" .. printable .. "\"");
+								else
+									level = legionUpgrades[bid1] + (levelLootedAt - 100) * 10
+								end
+							elseif numBonusIDs == 2 then
+								local bid1, bid2, levelLootedAt = strmatch(itemLink, ".+:%d+:512:%d*:%d+:(%d+):(%d+):(%d+):")
+								if legionUpgrades[bid1] == nil then
+									print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid1 .. ". Item: " .. itemLink)
+								elseif legionUpgrades[bid2] == nil then
+									print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid2 .. ". Item: " .. itemLink)
+								else
+									if legionUpgrades[bid1] > legionUpgrades[bid2] then
+										level = legionUpgrades[bid1] + (levelLootedAt - 100) * 10
+									else
+										level = legionUpgrades[bid2] + (levelLootedAt - 100) * 10
+									end
+								end
+							end
+						end
+
+						local artifact = tonumber(strmatch(itemLink, ".+:" .. ulvl .. ":%d+:(256):"))
+						if artifact then
+							level = GetItemLevel(itemLink)
+						end
 
 						text:SetText("|cFFFFFF00"..level)
 					end
