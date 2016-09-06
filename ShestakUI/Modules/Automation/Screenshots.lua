7b1f7599e1313188f4c74e81b625252e5539399f
leg883 <leg883@126.com>
2016-09-07 01:36:49 +0800

    Update Screenshots.lua

--- ShestakUI/Modules/Automation/Screenshots.lua
+++ ShestakUI/Modules/Automation/Screenshots.lua
@@ -4,10 +4,30 @@ if C.automation.screenshot ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Take screenshots of defined events (Sinaris)
 ----------------------------------------------------------------------------------------
-local function OnEvent(self, event, ...)
-	C_Timer.After(1, function() Screenshot() end)
+local function TakeScreen(delay, func, ...)
+	local waitTable = {}
+	local waitFrame = CreateFrame("Frame", nil, UIParent)
+	waitFrame:SetScript("OnUpdate", function(self, elapse)
+		local count = #waitTable
+		local i = 1
+		while (i <= count) do
+			local waitRecord = tremove(waitTable, i)
+			local d = tremove(waitRecord, 1)
+			local f = tremove(waitRecord, 1)
+			local p = tremove(waitRecord, 1)
+			if (d > elapse) then
+				tinsert(waitTable, i, {d-elapse, f, p})
+				i = i + 1
+			else
+				count = count - 1
+				f(unpack(p))
+			end
+		end
+	end)
+	tinsert(waitTable, {delay, func, {...}})
 end
-
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ACHIEVEMENT_EARNED")
-frame:SetScript("OnEvent", OnEvent)
+local AchScreen = CreateFrame("Frame")
+AchScreen:RegisterEvent("ACHIEVEMENT_EARNED")
+AchScreen:SetScript("OnEvent", function()
+	TakeScreen(1, Screenshot)
+end)
