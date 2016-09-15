76273e5becb450f8d1462f94691764a6289162f1
Wetxius <wetxius@gmail.com>
2016-09-15 15:25:04 +0300

    Small fix for calculation of BoA.

--- ShestakUI/Modules/Misc/SlotItemLevel.lua
+++ ShestakUI/Modules/Misc/SlotItemLevel.lua
@@ -22,7 +22,7 @@ local upgrades = {
 
 local legionUpgrades = {
 	["664"] = 689, ["767"] = 685, ["768"] = 693, ["1735"] = 705, ["1736"] = 699, ["1738"] = 709,
-	["1739"] = 703, ["1741"] = 713, ["1792"] = 699, ["1793"] = 703, ["1794"] = 695, ["1795"] = 700,
+	["1739"] = 703, ["1741"] = 713, ["1792"] = 699, ["1793"] = 703, ["1794"] = 695, ["1795"] = 700
 }
 
 local function BOALevel(level, id)
@@ -41,7 +41,7 @@ local function BOALevel(level, id)
 	elseif level > 67 then
 		level = 187 - (80 - level) * 4
 	elseif level > 57 then
-		level = 105 - (67 - level) * 2.9
+		level = 105 - (67 - level) * 2.88
 	elseif level > 5 then
 		level = level + 5
 	else
@@ -61,7 +61,7 @@ local itemLevelPattern = gsub(ITEM_LEVEL, "%%d", "(%%d+)")
 local tooltipLines = {
 	"ShestakUI_ItemScanningTooltipTextLeft2",
 	"ShestakUI_ItemScanningTooltipTextLeft3",
-	"ShestakUI_ItemScanningTooltipTextLeft4",
+	"ShestakUI_ItemScanningTooltipTextLeft4"
 }
 local tooltip = CreateFrame("GameTooltip", "ShestakUI_ItemScanningTooltip", UIParent, "GameTooltipTemplate")
 tooltip:SetOwner(UIParent, "ANCHOR_NONE")
