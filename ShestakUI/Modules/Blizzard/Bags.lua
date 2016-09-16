11ede4ea5b430b5e20e2b6dfde7bc4170f77bd5f
Max <cod41479@list.ru>
2016-09-16 16:24:01 +0300

    Now we get itemlevel of Legion items without tooltip scan.
    (except artiafact).
    And some refactoring.

--- ShestakUI/Modules/Blizzard/Bags.lua
+++ ShestakUI/Modules/Blizzard/Bags.lua
@@ -150,11 +150,6 @@ local upgrades = {
 	["507"] = 24, ["530"] = 5, ["531"] = 10, ["535"] = 15, ["536"] = 30, ["537"] = 45
 }
 
-local legionUpgrades = {
-	["664"] = 689, ["767"] = 685, ["768"] = 693, ["1735"] = 705, ["1736"] = 699, ["1738"] = 709,
-	["1739"] = 703, ["1741"] = 713, ["1792"] = 699, ["1793"] = 703, ["1794"] = 695, ["1795"] = 700
-}
-
 local function BOALevel(level, id)
 	if level > 97 then
 		if id == 133585 or id == 133595 or id == 133596 or id == 133597 or id == 133598 then
@@ -197,7 +192,7 @@ local tooltip = CreateFrame("GameTooltip", "ShestakUI_ItemScanningTooltip", UIPa
 tooltip:SetOwner(UIParent, "ANCHOR_NONE")
 
 -- Scan tooltip for item level information
-local function GetItemLevel(itemLink)
+local function GetItemLevelFromTooltip(itemLink)
 	if not itemLink or not GetItemInfo(itemLink) then
 		return
 	end
@@ -257,43 +252,14 @@ function Stuffing:SlotUpdate(b)
 
 				local numBonusIDs = tonumber(strmatch(clink, ".+:%d+:512:%d*:(%d+).+"))
 				if numBonusIDs then
-					if numBonusIDs == 1 then
-						local bid1, levelLootedAt = strmatch(clink, ".+:%d+:512:%d*:%d+:(%d+):(%d+).+")
-						if levelLootedAt == "110" then
-							levelLootedAt = levelLootedAt - 1
-						end
-						if legionUpgrades[bid1] == nil then
-							b.itemlevel = GetItemLevel(clink) or b.itemlevel
-							--print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid1 .. ". Item: " .. clink)
-							--print(clink)
-							--local printable = gsub(clink, "\124", "\124\124");
-							--ChatFrame1:AddMessage("Itemlink: \"" .. printable .. "\"");
-						else
-							b.itemlevel = legionUpgrades[bid1] + (levelLootedAt - 100) * 10
-						end
-					elseif numBonusIDs == 2 then
-						local bid1, bid2, levelLootedAt = strmatch(clink, ".+:%d+:512:%d*:%d+:(%d+):(%d+):(%d+).+")
-						if levelLootedAt == "110" then
-							levelLootedAt = levelLootedAt - 1
-						end
-						if legionUpgrades[bid1] == nil then
-							b.itemlevel = GetItemLevel(clink) or b.itemlevel
-							--print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid1 .. ". Item: " .. clink)
-						elseif legionUpgrades[bid2] == nil then
-							b.itemlevel = GetItemLevel(clink) or b.itemlevel
-							--print("|cffff0000WARNING: Unkhown item bonus ID: " .. bid2 .. ". Item: " .. clink)
-						else
-							if legionUpgrades[bid1] > legionUpgrades[bid2] then
-								b.itemlevel = legionUpgrades[bid1] + (levelLootedAt - 100) * 10
-							else
-								b.itemlevel = legionUpgrades[bid2] + (levelLootedAt - 100) * 10
-							end
-						end
+					if GetDetailedItemLevelInfo then
+						local effectiveLevel, previewLevel, origLevel = GetDetailedItemLevelInfo(clink)
+						b.itemlevel = effectiveLevel or b.itemlevel
 					end
 				end
 
 				if quality == 6 then
-					b.itemlevel = GetItemLevel(clink) or b.itemlevel
+					b.itemlevel = GetItemLevelFromTooltip(clink) or b.itemlevel
 				end
 
 				b.frame.text:SetText(b.itemlevel)
