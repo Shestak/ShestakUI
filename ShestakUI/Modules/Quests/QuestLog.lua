7c375c805cd1e95dcdcc548834ffa21974ce2d30
Wetxius <wetxius@gmail.com>
2016-11-02 09:38:32 +0300

    Added check for link to prevent error.

--- ShestakUI/Modules/Quests/QuestLog.lua
+++ ShestakUI/Modules/Quests/QuestLog.lua
@@ -6,12 +6,15 @@ local T, C, L, _ = unpack(select(2, ...))
 hooksecurefunc("QuestLogQuests_Update", function()
 	for i, button in pairs(QuestMapFrame.QuestsFrame.Contents.Titles) do
 		if button:IsShown() then
-			local level = strmatch(GetQuestLink(button.questID), "quest:%d+:(%d+)")
-			if level then
-				local height = button.Text:GetHeight()
-				button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
-				button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
-				button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+			local link = GetQuestLink(button.questID)
+			if link then
+				local level = strmatch(link, "quest:%d+:(%d+)")
+				if level then
+					local height = button.Text:GetHeight()
+					button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
+					button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
+					button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+				end
 			end
 		end
 	end
