39a2556db1be69c156fa8142087d3579c3afccb7
Wetxius <wetxius@gmail.com>
2016-11-16 16:49:48 +0300

    Added skin for reward icons.

--- ShestakUI/Modules/Skins/Blizzard/Garrison.lua
+++ ShestakUI/Modules/Skins/Blizzard/Garrison.lua
@@ -206,6 +206,22 @@ local function LoadSkin()
 		self.backdrop.overlay:SetVertexColor(1, 0.82, 0, 0.3)
 	end)
 
+	for i = 1, #GarrisonLandingPage.Report.List.listScroll.buttons do
+		local button = GarrisonLandingPage.Report.List.listScroll.buttons[i]
+		for _, reward in pairs(button.Rewards) do
+			reward.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			if not reward.backdrop then
+				reward:CreateBackdrop("Default")
+				reward.backdrop:SetPoint("TOPLEFT", reward.Icon, "TOPLEFT", -2, 2)
+				reward.backdrop:SetPoint("BOTTOMRIGHT", reward.Icon, "BOTTOMRIGHT", 2, -2)
+				reward.backdrop:SetFrameLevel(reward:GetFrameLevel())
+
+				reward.Quantity:SetParent(reward.backdrop)
+				reward.IconBorder:SetAlpha(0)
+			end
+		end
+	end
+
 	for _, xpBar in pairs({GarrisonLandingPage.FollowerTab.XPBar, GarrisonLandingPage.ShipFollowerTab.XPBar, GarrisonMissionFrame.FollowerTab.XPBar}) do
 		xpBar:StripTextures()
 		xpBar:CreateBackdrop("Default")
