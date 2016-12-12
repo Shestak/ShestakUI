a410b5a5193813dbce69a9a389d368024471f46c
Wetxius <wetxius@gmail.com>
2016-12-12 11:39:25 +0300

    Added skin for BonusRollFrame.SpecIcon.

--- ShestakUI/Modules/Skins/Blizzard/BonusRoll.lua
+++ ShestakUI/Modules/Skins/Blizzard/BonusRoll.lua
@@ -12,6 +12,10 @@ local function LoadSkin()
 	BonusRollFrame.backdrop:SetPoint("BOTTOMRIGHT", BonusRollFrame, "BOTTOMRIGHT", 5, -6)
 
 	BonusRollFrame.PromptFrame.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	BonusRollFrame.PromptFrame.Icon:SetSize(50, 50)
+	BonusRollFrame.PromptFrame.Icon:ClearAllPoints()
+	BonusRollFrame.PromptFrame.Icon:SetPoint("LEFT", BonusRollFrame, 0, 6)
+
 	BonusRollFrame.PromptFrame.IconBackdrop = CreateFrame("Frame", nil, BonusRollFrame.PromptFrame)
 	BonusRollFrame.PromptFrame.IconBackdrop:SetTemplate("Default")
 	BonusRollFrame.PromptFrame.IconBackdrop:SetFrameLevel(BonusRollFrame.PromptFrame.IconBackdrop:GetFrameLevel() - 1)
@@ -20,6 +24,20 @@ local function LoadSkin()
 
 	BonusRollFrame.PromptFrame.Timer:CreateBackdrop("Default")
 	BonusRollFrame.PromptFrame.Timer.Bar:SetColorTexture(1, 1, 1)
+	BonusRollFrame.PromptFrame.Timer:SetPoint("BOTTOMLEFT", BonusRollFrame.PromptFrame, "BOTTOMLEFT", 0, 1)
+
+	BonusRollFrame.SpecRing:SetTexture("")
+	BonusRollFrame.SpecIcon:SetPoint("TOPLEFT", BonusRollFrame, "TOPLEFT", 0, -3)
+	BonusRollFrame.SpecIcon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	-- BonusRollFrame.SpecIcon:SetTexture("Interface\\Icons\\spell_nature_brilliance")
+
+	BonusRollFrame.SpecIcon.b = CreateFrame("Frame", nil, BonusRollFrame)
+	BonusRollFrame.SpecIcon.b:SetFrameLevel(6)
+	BonusRollFrame.SpecIcon.b:SetTemplate("Default")
+	BonusRollFrame.SpecIcon.b:SetPoint("TOPLEFT", BonusRollFrame.SpecIcon, "TOPLEFT", -2, 2)
+	BonusRollFrame.SpecIcon.b:SetPoint("BOTTOMRIGHT", BonusRollFrame.SpecIcon, "BOTTOMRIGHT", 2, -2)
+	BonusRollFrame.SpecIcon:SetParent(BonusRollFrame.SpecIcon.b)
+	BonusRollFrame.SpecIcon.b:SetShown(BonusRollFrame.SpecIcon:IsShown() and BonusRollFrame.SpecIcon:GetTexture() ~= nil)
 
 	BonusRollMoneyWonFrame:StripTextures()
 	BonusRollMoneyWonFrame:CreateBackdrop("Transparent")
