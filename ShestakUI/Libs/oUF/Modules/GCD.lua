db4b9bc2b57b3176596c8a2d8ec6c684d6f9ad2c
Wetxius <wetxius@gmail.com>
2017-01-13 10:18:27 +0300

    Updated spells for GCD.

--- ShestakUI/Libs/oUF/Modules/GCD.lua
+++ ShestakUI/Libs/oUF/Modules/GCD.lua
@@ -11,18 +11,18 @@ local starttime, duration, usingspell, spellid
 local GetTime = GetTime
 
 local spells = {
-	["DEATHKNIGHT"] = 48266,
+	["DEATHKNIGHT"] = 50977,
 	["DEMONHUNTER"] = 204157,
-	["DRUID"] = 5176,
-	["HUNTER"] = 3044,
-	["MAGE"] = 44614,
+	["DRUID"] = 8921,
+	["HUNTER"] = 982,
+	["MAGE"] = 118,
 	["MONK"] = 100780,
 	["PALADIN"] = 35395,
 	["PRIEST"] = 585,
 	["ROGUE"] = 1752,
 	["SHAMAN"] = 403,
 	["WARLOCK"] = 686,
-	["WARRIOR"] = 34428,
+	["WARRIOR"] = 57755,
 }
 
 local Enable = function(self)
