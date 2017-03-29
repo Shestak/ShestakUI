5a21aca8e6733965df5a1124a4838b765b96a39b
Wetxius <wetxius@gmail.com>
2017-03-29 17:36:41 +0300

    Fixed non-existing CVar.

--- ShestakUI/Modules/Misc/Misc.lua
+++ ShestakUI/Modules/Misc/Misc.lua
@@ -95,23 +95,23 @@ end
 ----------------------------------------------------------------------------------------
 --	Custom Lag Tolerance(by Elv22)
 ----------------------------------------------------------------------------------------
-if C.general.custom_lagtolerance == true then
-	local customlag = CreateFrame("Frame")
-	local int = 5
-	local _, _, _, lag = GetNetStats()
-	local LatencyUpdate = function(self, elapsed)
-		int = int - elapsed
-		if int < 0 then
-			if GetCVar("reducedLagTolerance") ~= tostring(1) then SetCVar("reducedLagTolerance", tostring(1)) end
-			if lag ~= 0 and lag <= 400 then
-				SetCVar("maxSpellStartRecoveryOffset", tostring(lag))
-			end
-			int = 5
-		end
-	end
-	customlag:SetScript("OnUpdate", LatencyUpdate)
-	LatencyUpdate(customlag, 10)
-end
+--BETA if C.general.custom_lagtolerance == true then
+	-- local customlag = CreateFrame("Frame")
+	-- local int = 5
+	-- local _, _, _, lag = GetNetStats()
+	-- local LatencyUpdate = function(self, elapsed)
+		-- int = int - elapsed
+		-- if int < 0 then
+			-- if GetCVar("reducedLagTolerance") ~= tostring(1) then SetCVar("reducedLagTolerance", tostring(1)) end
+			-- if lag ~= 0 and lag <= 400 then
+				-- SetCVar("maxSpellStartRecoveryOffset", tostring(lag))
+			-- end
+			-- int = 5
+		-- end
+	-- end
+	-- customlag:SetScript("OnUpdate", LatencyUpdate)
+	-- LatencyUpdate(customlag, 10)
+-- end
 
 ----------------------------------------------------------------------------------------
 --	Auto select current event boss from LFD tool(EventBossAutoSelect by Nathanyel)
