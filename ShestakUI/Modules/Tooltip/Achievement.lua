07ed96cfc6f4463565d7d0f9c32797af18ee07e4
Wetxius <wetxius@gmail.com>
2017-04-15 18:42:49 +0300

    Blizzard now show achievement progress.
    Cleanup and show only who earned achievement.

--- ShestakUI/Modules/Tooltip/Achievement.lua
+++ ShestakUI/Modules/Tooltip/Achievement.lua
@@ -4,25 +4,10 @@ if C.tooltip.enable ~= true or C.tooltip.achievements ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Your achievement status in tooltip(Enhanced Achievements by Syzgyn)
 ----------------------------------------------------------------------------------------
-local colors = {
-	["GREEN"] = {
-		["r"] = 0.25,
-		["g"] = 0.75,
-		["b"] = 0.25,
-	},
-	["GRAY"] = {
-		["r"] = 0.5,
-		["g"] = 0.5,
-		["b"] = 0.5,
-	},
-}
-
 local function SetHyperlink(tooltip, refString)
-	local output = {[0] = {}, [1] = {}}
 	if select(3, string.find(refString, "(%a-):")) ~= "achievement" then return end
 
 	local _, _, achievementID = string.find(refString, ":(%d+):")
-	local numCriteria = GetAchievementNumCriteria(achievementID)
 	local _, _, GUID = string.find(refString, ":%d+:(.-):")
 
 	if GUID == UnitGUID("player") then
@@ -31,17 +16,9 @@ local function SetHyperlink(tooltip, refString)
 	end
 
 	tooltip:AddLine(" ")
-	local _, _, _, completed, month, day, year, _, _, _, _, _, wasEarnedByMe, earnedBy = GetAchievementInfo(achievementID)
+	local _, _, _, completed, _, _, _, _, _, _, _, _, wasEarnedByMe, earnedBy = GetAchievementInfo(achievementID)
 
 	if completed then
-		if year < 10 then year = "0"..year end
-
-		if T.client == "ruRU" then
-			tooltip:AddLine(L_TOOLTIP_ACH_COMPLETE..day.."/"..month.."/"..year, 0, 1, 0)
-		else
-			tooltip:AddLine(L_TOOLTIP_ACH_COMPLETE..month.."/"..day.."/"..year, 0, 1, 0)
-		end
-
 		if earnedBy then
 			if earnedBy ~= "" then
 				tooltip:AddLine(format(ACHIEVEMENT_EARNED_BY, earnedBy))
@@ -51,39 +28,6 @@ local function SetHyperlink(tooltip, refString)
 			elseif T.name ~= earnedBy then
 				tooltip:AddLine(format(ACHIEVEMENT_COMPLETED_BY, T.name))
 			end
-		end
-	elseif numCriteria == 0 then
-		tooltip:AddLine(L_TOOLTIP_ACH_INCOMPLETE)
-	else
-		tooltip:AddLine(L_TOOLTIP_ACH_STATUS)
-		for i = 1, numCriteria, 2 do
-			for a = 0, 1 do
-				output[a].text = nil
-				output[a].color = nil
-				if i + a <= numCriteria then
-					local name, _, completed, quantity, reqQuantity = GetAchievementCriteriaInfo(achievementID, i + a)
-					if completed then
-						output[a].text = name
-						output[a].color = "GREEN"
-					else
-						if quantity < reqQuantity and reqQuantity > 1 then
-							output[a].text = name.." ("..quantity.."/"..reqQuantity..")"
-							output[a].color = "GRAY"
-						else
-							output[a].text = name
-							output[a].color = "GRAY"
-						end
-					end
-				else
-					output[a].text = nil
-				end
-			end
-			if output[1].text == nil then
-				tooltip:AddLine(output[0].text, colors[output[0].color].r, colors[output[0].color].g, colors[output[0].color].b)
-			else
-				tooltip:AddDoubleLine(output[0].text, output[1].text, colors[output[0].color].r, colors[output[0].color].g, colors[output[0].color].b, colors[output[1].color].r, colors[output[1].color].g, colors[output[1].color].b)
-			end
-			output = {[0] = {}, [1] = {}}
 		end
 	end
 	tooltip:Show()
