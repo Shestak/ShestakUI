c57ea0c134ef48d5e224851140275e717868bd30
Wetxius <wetxius@gmail.com>
2017-09-02 18:28:53 +0300

    Updated version to 6.1.6.

--- ShestakUI/Modules/Skins/Blizzard/Collections.lua
+++ ShestakUI/Modules/Skins/Blizzard/Collections.lua
@@ -259,7 +259,7 @@ local function LoadSkin()
 	PetJournalHealPetButton.texture:ClearAllPoints()
 	PetJournalHealPetButton.texture:SetPoint("TOPLEFT", 2, -2)
 	PetJournalHealPetButton.texture:SetPoint("BOTTOMRIGHT", -2, 2)
-	if T.wowbuild >= 24904 then
+
 	PetJournalSummonRandomFavoritePetButton:CreateBackdrop("Default")
 	PetJournalSummonRandomFavoritePetButton.backdrop:SetAllPoints()
 	PetJournalSummonRandomFavoritePetButton:StyleButton()
@@ -268,7 +268,7 @@ local function LoadSkin()
 	PetJournalSummonRandomFavoritePetButton.texture:ClearAllPoints()
 	PetJournalSummonRandomFavoritePetButton.texture:SetPoint("TOPLEFT", 2, -2)
 	PetJournalSummonRandomFavoritePetButton.texture:SetPoint("BOTTOMRIGHT", -2, 2)
-	end
+
 
 	for i = 1, 3 do
 		local button = _G["PetJournalLoadoutPet"..i]
@@ -545,7 +545,7 @@ local function LoadSkin()
 	T.SkinScrollBar(WardrobeCollectionFrameScrollFrameScrollBar)
 	WardrobeCollectionFrameScrollFrameScrollBar:SetPoint("TOPLEFT", WardrobeCollectionFrame.SetsCollectionFrame.ScrollFrame, "TOPRIGHT", 4, 15)
 	WardrobeCollectionFrameScrollFrameScrollBar:SetPoint("BOTTOMLEFT", WardrobeCollectionFrame.SetsCollectionFrame.ScrollFrame, "BOTTOMRIGHT", 4, 14)
-	
+
 	local function SetItemQuality(self, itemFrame)
 		if (itemFrame.backdrop) then
 			local _, _, quality = GetItemInfo(itemFrame.itemID);
@@ -553,7 +553,7 @@ local function LoadSkin()
 			if (not itemFrame.collected) then
 				alpha = 0.4
 			end
-			
+
 			if (not quality or quality < 2) then --Not collected or item is white or grey
 				itemFrame.backdrop:SetBackdropBorderColor(0, 0, 0)
 			else
@@ -562,7 +562,7 @@ local function LoadSkin()
 		end
 	end
 	hooksecurefunc(WardrobeCollectionFrame.SetsCollectionFrame, "SetItemFrameQuality", SetItemQuality)
-	
+
 
 	for i = 1, 2 do
 		T.SkinTab(_G["WardrobeCollectionFrameTab"..i], true)
@@ -660,7 +660,7 @@ local function LoadSkin()
 			WardrobeCollectionFrame.SetsTransmogFrame["ModelR"..i.."C"..j]:CreateBackdrop("Overlay")
 		end
 	end
-	
+
 	local function SkinSetItemButtons(self)
 		for itemFrame in self.DetailsFrame.itemFramesPool:EnumerateActive() do
 			itemFrame.IconBorder:SetAlpha(0)
