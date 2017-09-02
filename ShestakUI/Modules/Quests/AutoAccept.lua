cc90b1bbf0d87168a2ad275a91fe394d5f2ac818
Wetxius <wetxius@gmail.com>
2017-09-02 14:48:14 +0300

    Updated AutoAccept.

--- ShestakUI/Modules/Quests/AutoAccept.lua
+++ ShestakUI/Modules/Quests/AutoAccept.lua
@@ -125,6 +125,7 @@ local ignoreQuestNPC = {
 	[111243] = true,	-- Archmage Lan'dalock
 	[108868] = true,	-- Hunter's order hall
 	[101462] = true,	-- Engineering
+	[103792] = true,	-- Griftah (one of his quests is a scam)
 }
 
 local function GetQuestLogQuests(onlyComplete)
@@ -199,11 +200,7 @@ local ignoreGossipNPC = {
 	[95201] = true,
 
 	-- Misc NPCs
-	[79740] = true, -- Warmaster Zog (Horde)
-	[79953] = true, -- Lieutenant Thorn (Alliance)
-	[84268] = true, -- Lieutenant Thorn (Alliance)
-	[84511] = true, -- Lieutenant Thorn (Alliance)
-	[84684] = true, -- Lieutenant Thorn (Alliance)
+	[117871] = true, -- War Councilor Victoria (Class Challenges @ Broken Shore)
 }
 
 local rogueClassHallInsignia = {
@@ -264,6 +261,11 @@ QuickQuest:Register("GOSSIP_SHOW", function()
 	end
 
 	if(available == 0 and active == 0 and GetNumGossipOptions() == 1) then
+		if(string.match((GetGossipOptions()), TRACKER_HEADER_PROVINGGROUNDS)) then
+			-- ignore proving grounds queue
+			return
+		end
+
 		if(QuickQuestDB.faireport) then
 			if(npcID == 57850) then
 				return SelectGossipOption(1)
