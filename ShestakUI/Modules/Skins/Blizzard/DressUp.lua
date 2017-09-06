380ed17e0e67aedd579f9395cb8fb43066b628c5
Wetxius <wetxius@gmail.com>
2017-09-06 13:05:36 +0300

    Reworked DressUp skin.

--- ShestakUI/Modules/Skins/Blizzard/DressUp.lua
+++ ShestakUI/Modules/Skins/Blizzard/DressUp.lua
@@ -8,42 +8,66 @@ local function LoadSkin()
 	DressUpFrame:StripTextures()
 	DressUpFrame:CreateBackdrop("Transparent")
 	DressUpFramePortrait:Hide()
-	if T.wowbuild >= 24904 then
-		DressUpFramePortraitFrame:Hide()
-		DressUpFrameInset:Hide()
-		MaximizeMinimizeFrame:StripTextures()
-		MaximizeMinimizeFrame.MaximizeButton:Kill()
-		MaximizeMinimizeFrame.MinimizeButton:Kill()
-	else
-		DressUpFrame.backdrop:SetPoint("TOPLEFT", 16, -12)
-		DressUpFrame.backdrop:SetPoint("BOTTOMRIGHT", -30, 76)
-	end
+	DressUpFramePortraitFrame:Hide()
+	DressUpFrameInset:Hide()
 
-	DressUpFrameResetButton:SkinButton()
+	MaximizeMinimizeFrame:StripTextures()
+	MaximizeMinimizeFrame:SetSize(18, 18)
+	MaximizeMinimizeFrame:SetPoint("RIGHT", DressUpFrameCloseButton, "LEFT", -2, 0)
+
+	local MaximizeButton = MaximizeMinimizeFrame.MaximizeButton
+	MaximizeButton:StripTextures()
+	MaximizeButton:SetTemplate("Overlay")
+
+	MaximizeButton.minus = MaximizeButton:CreateTexture(nil, "OVERLAY")
+	MaximizeButton.minus:SetSize(7, 1)
+	MaximizeButton.minus:SetPoint("CENTER")
+	MaximizeButton.minus:SetTexture(C.media.blank)
+
+	MaximizeButton.plus = MaximizeButton:CreateTexture(nil, "OVERLAY")
+	MaximizeButton.plus:SetSize(1, 7)
+	MaximizeButton.plus:SetPoint("CENTER")
+	MaximizeButton.plus:SetTexture(C.media.blank)
+
+	MaximizeButton:HookScript("OnEnter", T.SetModifiedBackdrop)
+	MaximizeButton:HookScript("OnLeave", T.SetOriginalBackdrop)
+
+	local MinimizeButton = MaximizeMinimizeFrame.MinimizeButton
+	MinimizeButton:StripTextures()
+	MinimizeButton:SetTemplate("Overlay")
+
+	MinimizeButton.minus = MinimizeButton:CreateTexture(nil, "OVERLAY")
+	MinimizeButton.minus:SetSize(7, 1)
+	MinimizeButton.minus:SetPoint("CENTER")
+	MinimizeButton.minus:SetTexture(C.media.blank)
+
+	MinimizeButton:HookScript("OnEnter", T.SetModifiedBackdrop)
+	MinimizeButton:HookScript("OnLeave", T.SetOriginalBackdrop)
+
 	DressUpFrameCancelButton:SkinButton()
+	DressUpFrameResetButton:SkinButton()
+	DressUpFrameResetButton:SetPoint("RIGHT", DressUpFrameCancelButton, "LEFT", -2, 0)
 	DressUpFrameUndressButton:SkinButton()
+
+	T.SkinDropDownBox(DressUpFrameOutfitDropDown)
+	DressUpFrameOutfitDropDown:SetSize(195, 34)
 	DressUpFrameOutfitDropDown.SaveButton:SkinButton()
 	DressUpFrameOutfitDropDown.SaveButton:ClearAllPoints()
 	DressUpFrameOutfitDropDown.SaveButton:SetPoint("RIGHT", DressUpFrameOutfitDropDown, 86, 4)
-	T.SkinDropDownBox(DressUpFrameOutfitDropDown)
-	DressUpFrameOutfitDropDown:SetSize(195, 34)
 
 	T.SkinCloseButton(DressUpFrameCloseButton, DressUpFrame.backdrop)
-	DressUpFrameResetButton:SetPoint("RIGHT", DressUpFrameCancelButton, "LEFT", -2, 0)
 
-	-- Wardrobe edit frame
 	WardrobeOutfitFrame:StripTextures(true)
-	WardrobeOutfitFrame:SetTemplate("Transparent")
+	WardrobeOutfitFrame:CreateBackdrop("Transparent")
+	WardrobeOutfitFrame.backdrop:SetPoint("TOPLEFT", WardrobeOutfitFrame, "TOPLEFT", 6, -6)
+	WardrobeOutfitFrame.backdrop:SetPoint("BOTTOMRIGHT", WardrobeOutfitFrame, "BOTTOMRIGHT", -6, 6)
 
 	WardrobeOutfitEditFrame:StripTextures(true)
 	WardrobeOutfitEditFrame:SetTemplate("Transparent")
-	WardrobeOutfitEditFrame.EditBox:StripTextures()
-	T.SkinEditBox(WardrobeOutfitEditFrame.EditBox)
-	WardrobeOutfitEditFrame.EditBox.backdrop:SetPoint("TOPLEFT", WardrobeOutfitEditFrame.EditBox, "TOPLEFT", -5, -5)
-	WardrobeOutfitEditFrame.EditBox.backdrop:SetPoint("BOTTOMRIGHT", WardrobeOutfitEditFrame.EditBox, "BOTTOMRIGHT", 0, 5)
 	WardrobeOutfitEditFrame.AcceptButton:SkinButton()
 	WardrobeOutfitEditFrame.CancelButton:SkinButton()
 	WardrobeOutfitEditFrame.DeleteButton:SkinButton()
+	T.SkinEditBox(WardrobeOutfitEditFrame.EditBox)
 end
 
 tinsert(T.SkinFuncs["ShestakUI"], LoadSkin)
