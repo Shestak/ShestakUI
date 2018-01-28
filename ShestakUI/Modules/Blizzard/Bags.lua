742313c7f6a3e8dfd8642b7b3305a52b6ba44a2d
Wetxius <wetxius@gmail.com>
2018-01-28 12:34:33 +0300

    Added "Pawn" icon support.

--- ShestakUI/Modules/Blizzard/Bags.lua
+++ ShestakUI/Modules/Blizzard/Bags.lua
@@ -188,6 +188,17 @@ function Stuffing:SlotUpdate(b)
 		b.frame.text:SetText("")
 	end
 
+	if b.frame.UpgradeIcon then
+		b.frame.UpgradeIcon:SetPoint("TOPLEFT", C.bag.button_size/2.7, -C.bag.button_size/2.7)
+		b.frame.UpgradeIcon:SetSize(C.bag.button_size/1.7, C.bag.button_size/1.7)
+		local itemIsUpgrade = IsContainerItemAnUpgrade(b.frame:GetParent():GetID(), b.frame:GetID())
+		if itemIsUpgrade and itemIsUpgrade == true then
+			b.frame.UpgradeIcon:SetShown(true)
+		else
+			b.frame.UpgradeIcon:SetShown(false)
+		end
+	end
+
 	if clink then
 		b.name, _, _, b.itemlevel, b.level, _, _, _, _, _, _, b.itemClassID, b.itemSubClassID = GetItemInfo(clink)
 
