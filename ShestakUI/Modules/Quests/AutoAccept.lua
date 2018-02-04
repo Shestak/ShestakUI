4c3b010727cd28630cfc747f58e87e6a0ae92356
Wetxius <wetxius@gmail.com>
2018-02-04 12:48:12 +0300

    Updated AutoAccept.

--- ShestakUI/Modules/Quests/AutoAccept.lua
+++ ShestakUI/Modules/Quests/AutoAccept.lua
@@ -123,12 +123,13 @@ local ignoreQuestNPC = {
 	[88570] = true,		-- Fate-Twister Tiklal
 	[87391] = true,		-- Fate-Twister Seress
 	[111243] = true,	-- Archmage Lan'dalock
-	[108868] = true,	-- Hunter's order hall
-	[101462] = true,	-- Engineering
+	[108868] = true,	-- Talua (Hunter's order hall)
+	[101462] = true,	-- Reaves (Engineering)
 	[103792] = true,	-- Griftah (one of his quests is a scam)
-	[119388] = true,	-- Chieftain Hatuun (he will eat your resources)
-	[124312] = true,	-- High Exarch Turalyon (he will eat your resources)
-	[126954] = true,	-- High Exarch Turalyon (he will eat your resources)
+	[119388] = true, 	-- Chieftain Hatuun (repeatable resource quest)
+	[124312] = true,	-- High Exarch Turalyon (repeatable resource quest)
+	[126954] = true,	-- High Exarch Turalyon (repeatable resource quest)
+	[127037] = true,	-- Nabiru (repeatable resource quest)
 }
 
 local function GetQuestLogQuests(onlyComplete)
