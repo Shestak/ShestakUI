75f48419bdd589195ff324a8d3b1d40d09e1bc54
Wetxius <wetxius@gmail.com>
2018-06-21 11:40:21 +0300

    Merged Function, Hide and Grid in Core.lua.
    Separated Vehicle Exit.

--- ShestakUI/Modules/ActionBars/Core.lua
+++ ShestakUI/Modules/ActionBars/Core.lua
@@ -60,6 +60,9 @@ do
 	uiManagedFrames = nil
 end
 
+----------------------------------------------------------------------------------------
+--	Set mouseover for bars
+----------------------------------------------------------------------------------------
 function RightBarMouseOver(alpha)
 	RightActionBarAnchor:SetAlpha(alpha)
 	PetActionBarAnchor:SetAlpha(alpha)
@@ -148,7 +151,9 @@ if C.actionbar.rightbars_mouseover == true then
 	RightActionBarAnchor:SetScript("OnLeave", function() if not HoverBind.enabled then RightBarMouseOver(0) end end)
 end
 
--- Fix cooldown spiral alpha (WoD bug)
+----------------------------------------------------------------------------------------
+--	Fix cooldown spiral alpha (WoD bug)
+----------------------------------------------------------------------------------------
 function T.HideSpiral(f, alpha)
 	f:SetSwipeColor(0, 0, 0, alpha * 0.8)
 	f:SetDrawBling(alpha == 1)
@@ -180,4 +185,159 @@ if (C.actionbar.rightbars_mouseover == true and C.actionbar.petbar_horizontal ==
 		end
 		EventPetSpiral:UnregisterEvent("PET_BAR_UPDATE_COOLDOWN")
 	end)
+end
+
+----------------------------------------------------------------------------------------
+--	Show grid function
+----------------------------------------------------------------------------------------
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event)
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	SetActionBarToggles(1, 1, 1, 1, 0)
+	if C.actionbar.show_grid == true then
+		SetCVar("alwaysShowActionBars", 1)
+		for i = 1, 12 do
+			local button = _G[format("ActionButton%d", i)]
+			button.noGrid = nil
+			button:SetAttribute("showgrid", 1)
+			ActionButton_ShowGrid(button)
+
+			button = _G[format("MultiBarRightButton%d", i)]
+			button.noGrid = nil
+			button:SetAttribute("showgrid", 1)
+			ActionButton_ShowGrid(button)
+
+			button = _G[format("MultiBarBottomRightButton%d", i)]
+			button.noGrid = nil
+			button:SetAttribute("showgrid", 1)
+			ActionButton_ShowGrid(button)
+
+			button = _G[format("MultiBarLeftButton%d", i)]
+			button.noGrid = nil
+			button:SetAttribute("showgrid", 1)
+			ActionButton_ShowGrid(button)
+
+			button = _G[format("MultiBarBottomLeftButton%d", i)]
+			button.noGrid = nil
+			button:SetAttribute("showgrid", 1)
+			ActionButton_ShowGrid(button)
+		end
+	else
+		SetCVar("alwaysShowActionBars", 0)
+	end
+end)
+
+----------------------------------------------------------------------------------------
+--	Pet/StanceBar style functions
+----------------------------------------------------------------------------------------
+T.ShiftBarUpdate = function()
+	local numForms = GetNumShapeshiftForms()
+	local texture, isActive, isCastable
+	local button, icon, cooldown
+	local start, duration, enable
+	for i = 1, NUM_STANCE_SLOTS do
+		button = _G["StanceButton"..i]
+		icon = _G["StanceButton"..i.."Icon"]
+		if i <= numForms then
+			texture, isActive, isCastable = GetShapeshiftFormInfo(i)
+			icon:SetTexture(texture)
+
+			cooldown = _G["StanceButton"..i.."Cooldown"]
+			if texture then
+				cooldown:SetAlpha(1)
+			else
+				cooldown:SetAlpha(0)
+			end
+
+			start, duration, enable = GetShapeshiftFormCooldown(i)
+			CooldownFrame_Set(cooldown, start, duration, enable)
+
+			if isActive then
+				StanceBarFrame.lastSelected = button:GetID()
+				button:SetChecked(true)
+			else
+				button:SetChecked(false)
+			end
+
+			if isCastable then
+				icon:SetVertexColor(1.0, 1.0, 1.0)
+			else
+				icon:SetVertexColor(0.4, 0.4, 0.4)
+			end
+		end
+	end
+end
+
+T.PetBarUpdate = function(self, event)
+	local petActionButton, petActionIcon, petAutoCastableTexture, petAutoCastShine
+	for i = 1, NUM_PET_ACTION_SLOTS, 1 do
+		local buttonName = "PetActionButton"..i
+		petActionButton = _G[buttonName]
+		petActionIcon = _G[buttonName.."Icon"]
+		petAutoCastableTexture = _G[buttonName.."AutoCastable"]
+		petAutoCastShine = _G[buttonName.."Shine"]
+		local name, texture, isToken, isActive, autoCastAllowed, autoCastEnabled = GetPetActionInfo(i)
+
+		if not isToken then
+			petActionIcon:SetTexture(texture)
+			petActionButton.tooltipName = name
+		else
+			petActionIcon:SetTexture(_G[texture])
+			petActionButton.tooltipName = _G[name]
+		end
+
+		petActionButton.isToken = isToken
+
+		if isActive and name ~= "PET_ACTION_FOLLOW" then
+			petActionButton:SetChecked(true)
+			if IsPetAttackAction(i) then
+				PetActionButton_StartFlash(petActionButton)
+			end
+		else
+			petActionButton:SetChecked(false)
+			if IsPetAttackAction(i) then
+				PetActionButton_StopFlash(petActionButton)
+			end
+		end
+
+		if autoCastAllowed then
+			petAutoCastableTexture:Show()
+		else
+			petAutoCastableTexture:Hide()
+		end
+
+		if autoCastEnabled then
+			AutoCastShine_AutoCastStart(petAutoCastShine)
+		else
+			AutoCastShine_AutoCastStop(petAutoCastShine)
+		end
+
+		if name then
+			if not C.actionbar.show_grid then
+				petActionButton:SetAlpha(1)
+			end
+		else
+			if not C.actionbar.show_grid then
+				petActionButton:SetAlpha(0)
+			end
+		end
+
+		if texture then
+			if GetPetActionSlotUsable(i) then
+				SetDesaturation(petActionIcon, nil)
+			else
+				SetDesaturation(petActionIcon, 1)
+			end
+			petActionIcon:Show()
+		else
+			petActionIcon:Hide()
+		end
+
+		if not PetHasActionBar() and texture and name ~= "PET_ACTION_FOLLOW" then
+			PetActionButton_StopFlash(petActionButton)
+			SetDesaturation(petActionIcon, 1)
+			petActionButton:SetChecked(false)
+		end
+	end
 end
