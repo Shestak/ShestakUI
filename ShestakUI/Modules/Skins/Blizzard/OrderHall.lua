b6bc6829840502cd609aab8279c2c28e9ee77b37
Wetxius <wetxius@gmail.com>
2018-07-19 09:29:48 +0300

    Fixed OrderHall skin.

--- ShestakUI/Modules/Skins/Blizzard/OrderHall.lua
+++ ShestakUI/Modules/Skins/Blizzard/OrderHall.lua
@@ -5,33 +5,6 @@ if C.skins.blizzard_frames ~= true then return end
 --	OrderHallUI skin
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
-	OrderHallCommandBar:StripTextures()
-	OrderHallCommandBar:SetTemplate("Transparent")
-	OrderHallCommandBar:ClearAllPoints()
-	OrderHallCommandBar:SetPoint("TOP", UIParent, 0, -1)
-	OrderHallCommandBar.ClassIcon:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
-	OrderHallCommandBar.ClassIcon:SetSize(46, 20)
-	OrderHallCommandBar.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
-	OrderHallCommandBar.AreaName:ClearAllPoints()
-	OrderHallCommandBar.AreaName:SetPoint("LEFT", OrderHallCommandBar.CurrencyIcon, "RIGHT", 25, 0)
-	OrderHallCommandBar.AreaName:SetVertexColor(T.color.r, T.color.g, T.color.b)
-	OrderHallCommandBar.WorldMapButton:Kill()
-
-	hooksecurefunc(OrderHallCommandBar, "RefreshCategories", function(self)
-		local index = 0
-		C_Timer.After(0.5, function()
-			for i, child in ipairs({self:GetChildren()}) do
-				if child.Icon and child.Count and child.TroopPortraitCover then
-					index = index + 1
-					child.TroopPortraitCover:Hide()
-					child.Icon:SetSize(38, 21)
-				end
-			end
-			self:SetWidth(270 + index * 112)
-		end)
-	end)
-
-	-- MissionFrame
 	OrderHallMissionFrame:StripTextures()
 	OrderHallMissionFrame:SetTemplate("Transparent")
 	OrderHallMissionFrame.GarrCorners:StripTextures()
@@ -175,4 +148,33 @@ local function LoadSkin()
 	OrderHallTalentFrame.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
 end
 
-T.SkinFuncs["Blizzard_OrderHallUI"] = LoadSkin
+local function LoadSkinCommandBar()
+	OrderHallCommandBar:StripTextures()
+	OrderHallCommandBar:SetTemplate("Transparent")
+	OrderHallCommandBar:ClearAllPoints()
+	OrderHallCommandBar:SetPoint("TOP", UIParent, 0, -1)
+	OrderHallCommandBar.ClassIcon:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
+	OrderHallCommandBar.ClassIcon:SetSize(46, 20)
+	OrderHallCommandBar.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
+	OrderHallCommandBar.AreaName:ClearAllPoints()
+	OrderHallCommandBar.AreaName:SetPoint("LEFT", OrderHallCommandBar.CurrencyIcon, "RIGHT", 25, 0)
+	OrderHallCommandBar.AreaName:SetVertexColor(T.color.r, T.color.g, T.color.b)
+	OrderHallCommandBar.WorldMapButton:Kill()
+
+	hooksecurefunc(OrderHallCommandBar, "RefreshCategories", function(self)
+		local index = 0
+		C_Timer.After(0.5, function()
+			for i, child in ipairs({self:GetChildren()}) do
+				if child.Icon and child.Count and child.TroopPortraitCover then
+					index = index + 1
+					child.TroopPortraitCover:Hide()
+					child.Icon:SetSize(38, 21)
+				end
+			end
+			self:SetWidth(270 + index * 112)
+		end)
+	end)
+end
+
+T.SkinFuncs["Blizzard_GarrisonUI"] = LoadSkin
+T.SkinFuncs["Blizzard_OrderHallUI"] = LoadSkinCommandBar
