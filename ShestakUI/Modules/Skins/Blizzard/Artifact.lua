98f5fc36e35b6914a018e6bb15ee6a73b1ed0840
Wetxius <wetxius@gmail.com>
2018-07-19 09:31:00 +0300

    Removed Netherlight Crucible skin.

--- ShestakUI/Modules/Skins/Blizzard/Artifact.lua
+++ ShestakUI/Modules/Skins/Blizzard/Artifact.lua
@@ -8,11 +8,6 @@ local function LoadSkin()
 	ArtifactFrame:StripTextures()
 	ArtifactFrame:SetTemplate("Transparent")
 	ArtifactFrame:CreateBackdrop()
-	T.SkinCloseButton(ArtifactFrame.CloseButton)
-	T.SkinCloseButton(ArtifactRelicForgeFrameCloseButton)
-	T.SkinCloseButton(ArtifactFrame.PerksTab.RelicTalentAlert.CloseButton)
-	T.SkinCloseButton(ArtifactFrame.KnowledgeLevelHelpBox.CloseButton)
-
 	ArtifactFrame.BorderFrame:StripTextures()
 
 	for i = 1, 2 do
@@ -20,23 +15,18 @@ local function LoadSkin()
 	end
 	ArtifactFrameTab1:ClearAllPoints()
 	ArtifactFrameTab1:SetPoint("TOPLEFT", ArtifactFrame, "BOTTOMLEFT", 0, -2)
-	
-	ArtifactRelicForgeFrame:StripTextures()
-	ArtifactRelicForgeFrame:CreateBackdrop("Transparent")
-	ArtifactRelicForgeFrame.PreviewRelicFrame:StripTextures()
-	ArtifactRelicForgeFrame.PreviewRelicCover:StripTextures()
-	
-	ArtifactFrame.PerksTab.RelicTalentAlert:StripTextures()
-	ArtifactFrame.PerksTab.RelicTalentAlert:CreateBackdrop("Transparent")
-	ArtifactFrame.PerksTab.RelicTalentAlert:SetPoint("TOPRIGHT", ArtifactFrame, "TOPRIGHT", -20, 0)
-	
+
+	ArtifactFrame.ForgeBadgeFrame.ItemIcon:Hide()
+	ArtifactFrame.ForgeBadgeFrame.ForgeLevelBackground:ClearAllPoints()
+	ArtifactFrame.ForgeBadgeFrame.ForgeLevelBackground:SetPoint("TOPLEFT", ArtifactFrame)
+
 	ArtifactFrame.KnowledgeLevelHelpBox:StripTextures()
 	ArtifactFrame.KnowledgeLevelHelpBox.Arrow:Hide()
 	ArtifactFrame.KnowledgeLevelHelpBox:CreateBackdrop("Transparent")
-	
-	ArtifactRelicForgeFrame.TutorialFrame.GlowBox:StripTextures()
-	ArtifactRelicForgeFrame.TutorialFrame.GlowBox:CreateBackdrop("Transparent")
-	ArtifactRelicForgeFrame.TutorialFrame.GlowBox.Button:SkinButton()
+
+	T.SkinCloseButton(ArtifactFrame.CloseButton)
+	T.SkinCloseButton(ArtifactFrame.KnowledgeLevelHelpBox.CloseButton)
+	T.SkinCloseButton(ArtifactFrame.AppearanceTabHelpBox.CloseButton)
 end
 
 T.SkinFuncs["Blizzard_ArtifactUI"] = LoadSkin
