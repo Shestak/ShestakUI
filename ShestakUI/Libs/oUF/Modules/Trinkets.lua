e8f4b48264415b4d3c415d6ffb5faf2a748a54a7
Wetxius <wetxius@gmail.com>
2018-07-23 10:24:19 +0300

    Updated oUF_Trinkets.

--- ShestakUI/Libs/oUF/Modules/Trinkets.lua
+++ ShestakUI/Libs/oUF/Modules/Trinkets.lua
@@ -2,60 +2,59 @@ local T, C, L = unpack(select(2, ...))
 if C.unitframe.enable ~= true or C.unitframe.show_arena ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	Based on oUF_Trinkets(by Allez)
+--	Based on oUF_Trinkets(by Allez, editor Tukz)
 ----------------------------------------------------------------------------------------
 local _, ns = ...
-local oUF = ns.oUF
-
-local trinketSpells = {
-	[208683] = 120,
-	[195710] = 180,
-	[42292] = 120
-}
-
-local GetTrinketIcon = function(unit)
-	if UnitFactionGroup(unit) == "Horde" then
-		return "Interface\\Icons\\INV_Jewelry_TrinketPVP_02"
-	else
-		return "Interface\\Icons\\INV_Jewelry_TrinketPVP_01"
-	end
-end
+local oUF = ns.oUF or oUF
+assert(oUF, 'oUF not loaded')
 
 local Update = function(self, event, ...)
 	local _, instanceType = IsInInstance()
-	if instanceType ~= "arena" then
+
+	if instanceType ~= 'arena' then
+		self.Trinket.Icon:SetTexture("Interface\\Icons\\Ability_pvp_gladiatormedallion")
 		self.Trinket:Hide()
+
 		return
 	else
 		self.Trinket:Show()
 	end
 
-	if self.Trinket.PreUpdate then self.Trinket:PreUpdate(event) end
+	if(self.Trinket.PreUpdate) then self.Trinket:PreUpdate(event, ...) end
 
-	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-		local _, eventType, _, sourceGUID, _, _, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
-		if eventType == "SPELL_CAST_SUCCESS" and sourceGUID == UnitGUID(self.unit) and trinketSpells[spellID] then
-			CooldownFrame_Set(self.Trinket.cooldownFrame, GetTime(), trinketSpells[spellID], 1)
-		end
-	elseif event == "ARENA_OPPONENT_UPDATE" then
-		local unit, type = ...
-		if type == "seen" then
-			if UnitExists(unit) and UnitIsPlayer(unit) then
-				self.Trinket.Icon:SetTexture(GetTrinketIcon(unit))
+	if event == "ARENA_COOLDOWNS_UPDATE" then
+		local unit = ...
+		local tunit = self.unit
+
+		if self.unit == unit then
+			C_PvP.RequestCrowdControlSpell(unit)
+
+			local spellID, startTime, duration = C_PvP.GetArenaCrowdControlInfo(unit)
+
+			if spellID and startTime ~= 0 and duration ~= 0 then
+				CooldownFrame_Set(self.Trinket.cooldownFrame, startTime / 1000, duration / 1000, 1)
 			end
+		end
+	elseif event == "ARENA_CROWD_CONTROL_SPELL_UPDATE" then
+		local unit, spellID = ...
+
+		if self.unit == unit then
+			local _, _, spellTexture = GetSpellInfo(spellID)
+
+			self.Trinket.Icon:SetTexture(spellTexture)
 		end
 	elseif event == "PLAYER_ENTERING_WORLD" then
 		CooldownFrame_Set(self.Trinket.cooldownFrame, 1, 1, 1)
 	end
 
-	if self.Trinket.PostUpdate then self.Trinket:PostUpdate(event) end
+	if(self.Trinket.PostUpdate) then self.Trinket:PostUpdate(event, ...) end
 end
 
 local Enable = function(self)
 	if self.Trinket then
-		self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", Update)
-		self:RegisterEvent("ARENA_OPPONENT_UPDATE", Update)
-		self:RegisterEvent("PLAYER_ENTERING_WORLD", Update)
+		self:RegisterEvent("ARENA_COOLDOWNS_UPDATE", Update, true)
+		self:RegisterEvent("ARENA_CROWD_CONTROL_SPELL_UPDATE", Update, true)
+		self:RegisterEvent("PLAYER_ENTERING_WORLD", Update, true)
 
 		if not self.Trinket.cooldownFrame then
 			self.Trinket.cooldownFrame = CreateFrame("Cooldown", nil, self.Trinket, "CooldownFrameTemplate")
@@ -68,7 +67,7 @@ local Enable = function(self)
 			self.Trinket.Icon:SetPoint("TOPLEFT", self.Trinket, 2, -2)
 			self.Trinket.Icon:SetPoint("BOTTOMRIGHT", self.Trinket, -2, 2)
 			self.Trinket.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-			self.Trinket.Icon:SetTexture(GetTrinketIcon("player"))
+			self.Trinket.Icon:SetTexture("Interface\\Icons\\Ability_pvp_gladiatormedallion")
 		end
 
 		return true
@@ -77,11 +76,11 @@ end
 
 local Disable = function(self)
 	if self.Trinket then
-		self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED", Update)
-		self:UnregisterEvent("ARENA_OPPONENT_UPDATE", Update)
+		self:UnregisterEvent("ARENA_COOLDOWNS_UPDATE", Update)
+		self:UnregisterEvent("ARENA_CROWD_CONTROL_SPELL_UPDATE", Update)
 		self:UnregisterEvent("PLAYER_ENTERING_WORLD", Update)
 		self.Trinket:Hide()
 	end
 end
 
-oUF:AddElement("Trinket", Update, Enable, Disable)
+oUF:AddElement('Trinket', Update, Enable, Disable)
