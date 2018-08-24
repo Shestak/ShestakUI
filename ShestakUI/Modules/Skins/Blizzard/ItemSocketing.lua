789893690602dac849157b3013530a0820cc75c0
Wetxius <wetxius@gmail.com>
2018-08-24 13:52:50 +0300

    Try to hide ItemSocketingDescription texture.

--- ShestakUI/Modules/Skins/Blizzard/ItemSocketing.lua
+++ ShestakUI/Modules/Skins/Blizzard/ItemSocketing.lua
@@ -13,6 +13,8 @@ local function LoadSkin()
 	ItemSocketingScrollFrame:StripTextures()
 	ItemSocketingScrollFrame:CreateBackdrop("Overlay")
 	T.SkinScrollBar(ItemSocketingScrollFrameScrollBar)
+	ItemSocketingDescription:DisableDrawLayer("BORDER")
+	ItemSocketingDescription:DisableDrawLayer("BACKGROUND")
 
 	for i = 1, MAX_NUM_SOCKETS do
 		local button = _G["ItemSocketingSocket"..i]
