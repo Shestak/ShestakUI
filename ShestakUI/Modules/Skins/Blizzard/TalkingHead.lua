e53020084832e47593a78641deccd273b94d1cc9
Wetxius <wetxius@gmail.com>
2018-09-01 09:56:47 +0300

    Hide textures for TalkingHead.

--- ShestakUI/Modules/Skins/Blizzard/TalkingHead.lua
+++ ShestakUI/Modules/Skins/Blizzard/TalkingHead.lua
@@ -10,16 +10,25 @@ local function LoadSkin()
 	TalkingHeadFrame.backdrop:SetPoint("TOPLEFT", 13, -13)
 	TalkingHeadFrame.backdrop:SetPoint("BOTTOMRIGHT", -35, 11)
 
-	TalkingHeadFrame.MainFrame:StripTextures()
-	TalkingHeadFrame.PortraitFrame:StripTextures()
-	TalkingHeadFrame.BackgroundFrame:StripTextures()
+	TalkingHeadFrame.BackgroundFrame.TextBackground:SetAtlas(nil)
+	TalkingHeadFrame.PortraitFrame.Portrait:SetAtlas(nil)
+	TalkingHeadFrame.MainFrame.Model.PortraitBg:SetAtlas(nil)
+	TalkingHeadFrame.BackgroundFrame.TextBackground.SetAtlas = T.dummy
+	TalkingHeadFrame.PortraitFrame.Portrait.SetAtlas = T.dummy
+	TalkingHeadFrame.MainFrame.Model.PortraitBg.SetAtlas = T.dummy
 
 	TalkingHeadFrame.MainFrame.Model:CreateBackdrop("Default")
-	TalkingHeadFrame.MainFrame.Model.backdrop:SetPoint("TOPLEFT", -3, 3)
+	TalkingHeadFrame.MainFrame.Model.backdrop:SetPoint("TOPLEFT", -2, 2)
 	TalkingHeadFrame.MainFrame.Model.backdrop:SetPoint("BOTTOMRIGHT", 3, -3)
 
 	T.SkinCloseButton(TalkingHeadFrame.MainFrame.CloseButton)
 	TalkingHeadFrame.MainFrame.CloseButton:SetPoint("TOPRIGHT", -39, -17)
+
+	hooksecurefunc(TalkingHeadFrame.TextFrame.Text, "SetTextColor", function(self, r, g, b)
+		if r == 0 then
+			self:SetTextColor(1, 1, 1)
+		end
+	end)
 end
 
 T.SkinFuncs["Blizzard_TalkingHeadUI"] = LoadSkin
