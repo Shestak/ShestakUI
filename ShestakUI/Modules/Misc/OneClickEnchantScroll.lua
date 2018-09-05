f07c65fb61e500ddc1c1d375a4e23a8bcd7e0a0a
Wetxius <wetxius@gmail.com>
2018-09-05 12:47:23 +0300

    Fixed OneClickEnchantScroll.

--- ShestakUI/Modules/Misc/OneClickEnchantScroll.lua
+++ ShestakUI/Modules/Misc/OneClickEnchantScroll.lua
@@ -20,34 +20,33 @@ frame:SetScript("OnEvent", function(self, event, addon)
 			UseItemByName(38682)
 		end)
 
-		hooksecurefunc(TradeSkillFrame.RecipeList, "OnRecipeButtonClicked", function(_, recipeInfo)
+		hooksecurefunc(TradeSkillFrame.DetailsFrame, "RefreshButtons", function(self)
 			if C_TradeSkillUI.IsTradeSkillGuild() or C_TradeSkillUI.IsNPCCrafting() or C_TradeSkillUI.IsTradeSkillLinked() then
 				button:Hide()
-			elseif recipeInfo.tradeSkillInfo.alternateVerb then
-				local _, tradeSkillName = C_TradeSkillUI.GetTradeSkillLine()
-				if tradeSkillName == GetSpellInfo(7411) then
-					button:Show()
-					local numCreateable = recipeInfo.tradeSkillInfo.numAvailable
-					local numScrollsAvailable = GetItemCount(38682)
-					button:SetText(L_MISC_SCROLL.." ("..numScrollsAvailable..")")
-					 if numScrollsAvailable == 0 then
-						numCreateable = 0
-					end
-					if numCreateable > 0 then
-						button:Enable()
+			else
+				local recipeInfo = self.selectedRecipeID and C_TradeSkillUI.GetRecipeInfo(self.selectedRecipeID)
+				if recipeInfo and recipeInfo.alternateVerb then
+					local _, tradeSkillName = C_TradeSkillUI.GetTradeSkillLine()
+					if string.find(tradeSkillName, GetSpellInfo(7411)) then
+						button:Show()
+						local numCreateable = recipeInfo.numAvailable
+						local numScrollsAvailable = GetItemCount(38682)
+						button:SetText(L_MISC_SCROLL.." ("..numScrollsAvailable..")")
+						 if numScrollsAvailable == 0 then
+							numCreateable = 0
+						end
+						if numCreateable > 0 then
+							button:Enable()
+						else
+							button:Disable()
+						end
 					else
-						button:Disable()
+						button:Hide()
 					end
 				else
 					button:Hide()
 				end
-			else
-				button:Hide()
 			end
-		end)
-
-		hooksecurefunc(TradeSkillFrame, "RefreshTitle", function()
-			button:Hide()
 		end)
 	end
 end)
