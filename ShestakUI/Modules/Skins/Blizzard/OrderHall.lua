0b7046f241ee01108bfb71556d017dfde944f410
Wetxius <wetxius@gmail.com>
2018-12-15 19:27:38 +0300

    Added skin for TalentFrame of OrderHall.

--- ShestakUI/Modules/Skins/Blizzard/OrderHall.lua
+++ ShestakUI/Modules/Skins/Blizzard/OrderHall.lua
@@ -31,14 +31,86 @@ local function LoadSkin()
 		end)
 	end)
 
-	-- TalentFrame
+	-- TalentFrame skin from ElvUI
+	local function colorBorder(child, backdrop, atlas)
+		if child.AlphaIconOverlay:IsShown() then --isBeingResearched or (talentAvailability and not selected)
+			local alpha = child.AlphaIconOverlay:GetAlpha()
+			if alpha <= 0.5 then --talentAvailability
+				backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5) --[border = grey, shadow x2]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.50)
+				child.darkOverlay:Show()
+			elseif alpha <= 0.7 then --isBeingResearched
+				backdrop:SetBackdropBorderColor(0, 1, 1) --[border = teal, shadow x1]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.25)
+				child.darkOverlay:Show()
+			end
+		elseif atlas == "orderhalltalents-spellborder-green" then
+			backdrop:SetBackdropBorderColor(0, 1, 0) --[border = green, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder-yellow" then
+			backdrop:SetBackdropBorderColor(1, 1, 0) --[border = yellow, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder" then
+			backdrop:SetBackdropBorderColor(unpack(C.media.backdrop_color))
+			child.darkOverlay:SetColorTexture(0, 0, 0, 0.75) --[border will be default, shadow x3]
+			child.darkOverlay:Show()
+		end
+	end
+
 	OrderHallTalentFrame:StripTextures()
 	OrderHallTalentFrame:SetTemplate("Transparent")
 	OrderHallTalentFrame.NineSlice:Hide()
+	OrderHallTalentFrame.OverlayElements:Hide()
 	T.SkinCloseButton(OrderHallTalentFrameCloseButton)
 	OrderHallTalentFrame:HookScript("OnShow", function(self)
+		if self.CloseButton.Border then
+			self.CloseButton.Border:Hide()
+		end
+		if self.skinned then return end
 		OrderHallTalentFramePortrait:Hide()
-		OrderHallTalentFrame.Currency.Icon:SetAtlas("legionmission-icon-currency", false)
+		self.Currency.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+		self.Background:SetDrawLayer("BACKGROUND", 2)
+
+		for i = 1, self:GetNumChildren() do
+			local child = select(i, self:GetChildren())
+			if child and child.Icon and child.DoneGlow and not child.backdrop then
+				child:StyleButton()
+				child:CreateBackdrop()
+				child.Border:SetAlpha(0)
+				child.Highlight:SetAlpha(0)
+				child.AlphaIconOverlay:SetTexture(nil)
+				child.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+				child.Icon:SetInside(child.backdrop)
+				child.hover:SetInside(child.backdrop)
+				child.pushed:SetInside(child.backdrop)
+				child.backdrop:SetFrameLevel(child.backdrop:GetFrameLevel()+1)
+
+				child.darkOverlay = child:CreateTexture()
+				child.darkOverlay:SetAllPoints(child.Icon)
+				child.darkOverlay:SetDrawLayer('OVERLAY')
+				child.darkOverlay:Hide()
+
+				colorBorder(child, child.backdrop, child.Border:GetAtlas())
+
+				child.TalentDoneAnim:HookScript("OnFinished", function()
+					child.Border:SetAlpha(0) -- clear the yellow glow border again, after it finishes the animation
+				end)
+			end
+		end
+
+		self.choiceTexturePool:ReleaseAll()
+		hooksecurefunc(self, "RefreshAllData", function(frame)
+			frame.choiceTexturePool:ReleaseAll()
+
+			for i = 1, frame:GetNumChildren() do
+				local child = select(i, frame:GetChildren())
+				if child and child.Icon and child.backdrop then
+					colorBorder(child, child.backdrop, child.Border:GetAtlas())
+				end
+			end
+		end)
+
+		self.skinned = true
 	end)
 end
 
