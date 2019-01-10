4c645680388fda717c474f8d651efbee50ea14fa
Wetxius <wetxius@gmail.com>
2019-01-10 20:41:14 +0300

    Check drink spell from table.

--- ShestakUI/Modules/Announcements/Drinking.lua
+++ ShestakUI/Modules/Announcements/Drinking.lua
@@ -4,13 +4,19 @@ if C.announcements.drinking ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Announce enemy drinking in arena(by Duffed)
 ----------------------------------------------------------------------------------------
+local drinkSpell = {
+	[GetSpellInfo(118358)] = true,	-- Drink
+	[GetSpellInfo(167152)] = true,	-- Refreshment
+	[GetSpellInfo(167268)] = true,	-- Ba'ruun's Bountiful Bloom
+}
+
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 frame:SetScript("OnEvent", function(self, event, ...)
 	if not (event == "UNIT_SPELLCAST_SUCCEEDED" and GetZonePVPInfo() == "arena") then return end
 
 	local unit, _, spellID = ...
-	if UnitIsEnemy("player", unit) and (GetSpellInfo(spellID) == GetSpellInfo(118358) or GetSpellInfo(spellID) == GetSpellInfo(167152) or GetSpellInfo(spellID) == GetSpellInfo(167268)) then
+	if UnitIsEnemy("player", unit) and drinkSpell[GetSpellInfo(spellID)] then
 		SendChatMessage(UnitClass(unit).." "..UnitName(unit)..L_MISC_DRINKING, T.CheckChat(true))
 	end
 end)
