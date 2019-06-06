25eebc0b4c454bd4d6729a0894058da7bd1f15d9
Esrever <49285632+EsreverWoW@users.noreply.github.com>
2019-06-06 14:26:58 -0500

    Disable skinning of popup buttons for now

--- ShestakUI/Compatibility/Classic/Modules/Skins/Blizzard/Others.lua
+++ ShestakUI/Compatibility/Classic/Modules/Skins/Blizzard/Others.lua
@@ -51,9 +51,11 @@ SkinBlizzUI:SetScript("OnEvent", function(self, event, addon)
 
 		-- Reskin popups
 		for i = 1, 4 do
+			--[[
 			for j = 1, 4 do
-				_G["StaticPopup"..i.."Button"..j]:SkinButton()
+				_G["StaticPopup"..i.."Button"..j]:SkinButton() -- possibly causing issues entering BGs
 			end
+			--]]
 			_G["StaticPopup"..i]:StripTextures()
 			_G["StaticPopup"..i]:CreateBackdrop("Transparent")
 			_G["StaticPopup"..i].backdrop:SetPoint("TOPLEFT", 2, -2)
