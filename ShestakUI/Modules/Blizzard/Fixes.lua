4ae3a7b519e566454b458a5c674cff343b751ed8
Wetxius <wetxius@gmail.com>
2019-06-27 19:48:48 +0300

    Updated FrameStackGlobalizer.

--- ShestakUI/Modules/Blizzard/Fixes.lua
+++ ShestakUI/Modules/Blizzard/Fixes.lua
@@ -55,48 +55,60 @@ end)
 
 
 ----------------------------------------------------------------------------------------
---	FrameStackGlobalizer
+--	FrameStackGlobalizer(by Gethe)
 ----------------------------------------------------------------------------------------
 local FrameStackFix = CreateFrame("Frame")
 FrameStackFix:RegisterEvent("ADDON_LOADED")
 FrameStackFix:SetScript("OnEvent", function(self, event, addon)
 	if addon == "Blizzard_DebugTools" then
-		local frames = {}
 		local EnumerateFrames = _G.EnumerateFrames
 		local tostring = _G.tostring
+
+		local ignore = {}
+		local frames = {}
 		local function FindFrame(hash)
+			if ignore[hash] then return end
+
 			if frames[hash] then
 				return frames[hash]
 			else
 				local frame = EnumerateFrames()
 				while frame do
-					if frame:IsVisible() and frame:IsMouseOver() then
-						if tostring(frame):find(hash) then
-							frames[hash] = frame
-							return frame
-						end
+					local frameHash = tostring(frame)
+					if frameHash:find(hash) then
+						frames[hash] = frame
+						return frame
 					end
 					frame = EnumerateFrames(frame)
 				end
 			end
+
+			ignore[hash] = true
 		end
 
-		local next = _G.next
-		_G.hooksecurefunc(_G.FrameStackTooltip, "SetFrameStack", function(self, showHidden, showRegions, highlightIndexChanged)
-			local regions = {self:GetRegions()}
-			for _, region in next, regions do
-				if region:GetObjectType() == "FontString" then
-					local text = region:GetText()
-					if text and text:find("<%d+>") then
-						local hash = text:match("UIParent%.(%x*)%.?")
-						if hash then
-							local frame = FindFrame(hash:upper())
-							if frame then
-								local name = frame:GetName() or frame:GetDebugName()
-								region:SetText(text:gsub("(UIParent%.%x*)", name))
-							end
-						end
+		local matchPattern, subPattern = "%s%%.(%%x*)%%.?", "(%s%%.%%x*)"
+		local function TransformText(text)
+			local parent = text:match("%s+([%w_]+)%.")
+			if parent then
+				local hash = text:match(matchPattern:format(parent))
+				if hash and #hash > 5 then
+					local frame = FindFrame(hash:upper())
+					if frame and frame:GetName() then
+						text = text:gsub(subPattern:format(parent), frame:GetName())
+						return TransformText(text)
 					end
+				end
+			end
+
+			return text
+		end
+
+		_G.hooksecurefunc(_G.FrameStackTooltip, "SetFrameStack", function(self)
+			for i = 1, self:NumLines() do
+				local line = _G["FrameStackTooltipTextLeft"..i]
+				local text = line:GetText()
+				if text and text:find("<%d+>") then
+					line:SetText(TransformText(text))
 				end
 			end
 		end)
