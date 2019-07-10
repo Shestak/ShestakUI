07842226bc9c4b9b793e508308bad29f77232595
Wetxius <wetxius@gmail.com>
2019-07-10 10:23:15 +0300

    Cleanup.

--- ShestakUI/Libs/oUF/Units.lua
+++ ShestakUI/Libs/oUF/Units.lua
@@ -48,7 +48,7 @@ local function updateArenaPreparationElements(self, event, elementName, specID)
 			-- this section just replicates the color options available to the Health and Power elements
 			local r, g, b, t, _
 			-- if(element.colorPower and elementName == 'Power') then
-				-- FIXME: no idea if we can get power type here without the unit
+				-- no idea if we can get power type here without the unit
 			if(element.colorClass) then
 				local _, _, _, _, _, class = GetSpecializationInfoByID(specID)
 				t = self.colors.class[class]
