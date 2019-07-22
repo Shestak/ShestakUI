505a259a449eb97d65cadf944a30684101f4989c
Wetxius <wetxius@gmail.com>
2019-07-22 08:41:37 +0300

    Updated MoveBlizzFrames.

--- ShestakUI/Modules/Blizzard/MoveBlizzFrames.lua
+++ ShestakUI/Modules/Blizzard/MoveBlizzFrames.lua
@@ -7,11 +7,11 @@ if C.misc.move_blizzard ~= true then return end
 local frames = {
 	"CharacterFrame", "SpellBookFrame", "TaxiFrame", "QuestFrame", "PVEFrame", "AddonList",
 	"QuestLogPopupDetailFrame", "MerchantFrame", "TradeFrame", "MailFrame", "LootFrame",
-	"FriendsFrame", "CinematicFrame", "TabardFrame", "PetStableFrame", "MissingLootFrame",
+	"FriendsFrame", "CinematicFrame", "TabardFrame", "PetStableFrame", "BankFrame",
 	"PetitionFrame", "HelpFrame", "GossipFrame", "DressUpFrame", "GuildRegistrarFrame",
-	"WorldStateScoreFrame", "ChatConfigFrame", "RaidBrowserFrame", "InterfaceOptionsFrame",
-	"GameMenuFrame", "VideoOptionsFrame", "GuildInviteFrame", "ItemTextFrame", "BankFrame",
-	"OpenMailFrame", "StackSplitFrame", "MacOptionsFrame", "TutorialFrame", "StaticPopup1",
+	"ChatConfigFrame", "RaidBrowserFrame", "InterfaceOptionsFrame", "WorldMapFrame"
+	"GameMenuFrame", "VideoOptionsFrame", "GuildInviteFrame", "ItemTextFrame",
+	"OpenMailFrame", "StackSplitFrame", "TutorialFrame", "StaticPopup1",
 	"StaticPopup2", "ScrollOfResurrectionSelectionFrame"
 }
 
@@ -29,32 +29,35 @@ end
 local AddOnFrames = {
 	["Blizzard_AchievementUI"] = {"AchievementFrame"},
 	["Blizzard_ArchaeologyUI"] = {"ArchaeologyFrame"},
+	["Blizzard_ArtifactUI"] = {"ArtifactRelicForgeFrame"},
 	["Blizzard_AuctionUI"] = {"AuctionFrame"},
 	["Blizzard_BarberShopUI"] = {"BarberShopFrame"},
 	["Blizzard_BindingUI"] = {"KeyBindingFrame"},
 	["Blizzard_BlackMarketUI"] = {"BlackMarketFrame"},
 	["Blizzard_Calendar"] = {"CalendarCreateEventFrame", "CalendarFrame", "CalendarViewEventFrame", "CalendarViewHolidayFrame"},
 	["Blizzard_ChallengesUI"] = {"ChallengesLeaderboardFrame"},
-	["Blizzard_Collections"] = {"CollectionsJournal"},
+	["Blizzard_Collections"] = {"CollectionsJournal", "WardrobeFrame"},
+	["Blizzard_Communities"] = {"CommunitiesFrame"},
 	["Blizzard_EncounterJournal"] = {"EncounterJournal"},
-	["Blizzard_GarrisonUI"] = {"GarrisonLandingPage", "GarrisonMissionFrame", "GarrisonCapacitiveDisplayFrame", "GarrisonBuildingFrame", "GarrisonRecruiterFrame", "GarrisonRecruitSelectFrame", "GarrisonShipyardFrame"},
 	["Blizzard_GMChatUI"] = {"GMChatStatusFrame"},
 	["Blizzard_GMSurveyUI"] = {"GMSurveyFrame"},
+	["Blizzard_GarrisonUI"] = {"GarrisonLandingPage", "GarrisonMissionFrame", "GarrisonCapacitiveDisplayFrame", "GarrisonBuildingFrame", "GarrisonRecruiterFrame", "GarrisonRecruitSelectFrame", "GarrisonShipyardFrame"},
 	["Blizzard_GuildBankUI"] = {"GuildBankFrame"},
 	["Blizzard_GuildControlUI"] = {"GuildControlUI"},
-	["Blizzard_GuildUI"] = {"GuildFrame", "GuildLogFrame"},
 	["Blizzard_InspectUI"] = {"InspectFrame"},
 	["Blizzard_ItemAlterationUI"] = {"TransmogrifyFrame"},
 	["Blizzard_ItemSocketingUI"] = {"ItemSocketingFrame"},
 	["Blizzard_ItemUpgradeUI"] = {"ItemUpgradeFrame"},
 	["Blizzard_LookingForGuildUI"] = {"LookingForGuildFrame"},
 	["Blizzard_MacroUI"] = {"MacroFrame"},
+	["Blizzard_OrderHallUI"] = {"OrderHallMissionFrame"},
 	["Blizzard_QuestChoice"] = {"QuestChoiceFrame"},
+	["Blizzard_ReforgingUI"] = {"ReforgingFrame"},
 	["Blizzard_TalentUI"] = {"PlayerTalentFrame"},
+	["Blizzard_TalkingHeadUI"] = {"TalkingHeadFrame"},
 	["Blizzard_TradeSkillUI"] = {"TradeSkillFrame"},
 	["Blizzard_TrainerUI"] = {"ClassTrainerFrame"},
-	["Blizzard_VoidStorageUI"] = {"VoidStorageFrame"},
-	["Blizzard_TalkingHeadUI"] = {"TalkingHeadFrame"}
+	["Blizzard_VoidStorageUI"] = {"VoidStorageFrame"}
 }
 
 local frame = CreateFrame("Frame")
