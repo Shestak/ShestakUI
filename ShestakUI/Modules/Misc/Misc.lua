38515c2e1f6dfc18b6f25fcbcbe1743c29b916b1
Wetxius <wetxius@gmail.com>
2019-08-03 10:07:44 +0300

    Remove Force quit cause this function is protected.

--- ShestakUI/Modules/Misc/Misc.lua
+++ ShestakUI/Modules/Misc/Misc.lua
@@ -171,19 +171,6 @@ PanelTemplates_DeselectTab(gtframe)
 gtframe:SetScript("OnClick", function() ToggleGuildFrame() end)
 
 ----------------------------------------------------------------------------------------
---	Force quit
-----------------------------------------------------------------------------------------
-local CloseWoW = CreateFrame("Frame")
-CloseWoW:RegisterEvent("CHAT_MSG_SYSTEM")
-CloseWoW:SetScript("OnEvent", function(self, event, msg)
-	if event == "CHAT_MSG_SYSTEM" then
-		if msg and msg == IDLE_MESSAGE then
-			ForceQuit()
-		end
-	end
-end)
-
-----------------------------------------------------------------------------------------
 --	Old achievements filter
 ----------------------------------------------------------------------------------------
 function AchievementFrame_GetCategoryNumAchievements_OldIncomplete(categoryID)
