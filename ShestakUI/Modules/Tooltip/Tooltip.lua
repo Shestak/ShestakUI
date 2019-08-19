97267b9455577b6f3ad1f9b1fc9e4e80aa406fc1
Wetxius <wetxius@gmail.com>
2019-08-19 09:05:28 +0300

    Don't show white border for tooltip reward.

--- ShestakUI/Modules/Tooltip/Tooltip.lua
+++ ShestakUI/Modules/Tooltip/Tooltip.lua
@@ -524,7 +524,9 @@ ItemRefTooltip:HookScript("OnTooltipSetItem", FixFont)
 ----------------------------------------------------------------------------------------
 GameTooltip.ItemTooltip.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 hooksecurefunc(GameTooltip.ItemTooltip.IconBorder, "SetVertexColor", function(self, r, g, b)
-	self:GetParent().backdrop:SetBackdropBorderColor(r, g, b)
+	if r ~= 0.65882 and g ~= 0.65882 and b ~= 0.65882 then
+		self:GetParent().backdrop:SetBackdropBorderColor(r, g, b)
+	end
 	self:SetTexture("")
 end)
 
@@ -547,7 +549,9 @@ if icon then
 	reward.backdrop:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
 
 	hooksecurefunc(reward.IconBorder, "SetVertexColor", function(self, r, g, b)
-		self:GetParent().backdrop:SetBackdropBorderColor(r, g, b)
+		if r ~= 0.65882 and g ~= 0.65882 and b ~= 0.65882 then
+			self:GetParent().backdrop:SetBackdropBorderColor(r, g, b)
+		end
 		self:SetTexture("")
 	end)
 
