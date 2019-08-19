32307c6d1e4482a75e07ae2e32f307411b3e718a
Wetxius <wetxius@gmail.com>
2019-08-19 09:51:26 +0300

    Updated BlackMarket skin.

--- ShestakUI/Modules/Skins/Blizzard/BlackMarket.lua
+++ ShestakUI/Modules/Skins/Blizzard/BlackMarket.lua
@@ -8,6 +8,10 @@ local function LoadSkin()
 	BlackMarketFrame:StripTextures()
 	BlackMarketFrame:SetTemplate("Transparent")
 
+	BlackMarketFrame.Inset:CreateBackdrop("Overlay")
+	BlackMarketFrame.Inset.backdrop:SetPoint("TOPLEFT", 0, 0)
+	BlackMarketFrame.Inset.backdrop:SetPoint("BOTTOMRIGHT", 1, 4)
+
 	T.SkinScrollBar(BlackMarketScrollFrameScrollBar)
 	BlackMarketFrame.MoneyFrameBorder:StripTextures()
 	T.SkinEditBox(BlackMarketBidPriceGold, nil, 18)
@@ -18,6 +22,7 @@ local function LoadSkin()
 	BlackMarketBidPrice:SetPoint("BOTTOMRIGHT", BlackMarketFrame.BidButton, "BOTTOMLEFT", 0, -2)
 
 	BlackMarketFrame.HotDeal:StripTextures()
+	BlackMarketFrame.HotDeal:CreateBackdrop("Overlay")
 	BlackMarketFrame.HotDeal.Item.IconTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 	BlackMarketFrame.HotDeal.Item.IconBorder:SetAlpha(0)
 	BlackMarketFrame.HotDeal.Item:CreateBackdrop("Default")
