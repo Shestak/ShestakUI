4612c9c797193954c50fcf3853afeb04c6ac461e
Wetxius <wetxius@gmail.com>
2019-09-04 08:38:37 +0300

    Finished AzeriteEssence skin.

--- ShestakUI/Modules/Skins/Blizzard/AzeriteEssence.lua
+++ ShestakUI/Modules/Skins/Blizzard/AzeriteEssence.lua
@@ -10,6 +10,29 @@ local function LoadSkin()
 	AzeriteEssenceUI:StripTextures()
 	AzeriteEssenceUI:SetTemplate("Transparent")
 
+	AzeriteEssenceUIPortrait:SetAlpha(0)
+
+	AzeriteEssenceUI.PowerLevelBadgeFrame:ClearAllPoints()
+	AzeriteEssenceUI.PowerLevelBadgeFrame:SetPoint("TOPLEFT")
+	AzeriteEssenceUI.PowerLevelBadgeFrame.Ring:Hide()
+	AzeriteEssenceUI.PowerLevelBadgeFrame.BackgroundBlack:Hide()
+
+	for _, button in pairs(AzeriteEssenceUI.EssenceList.buttons) do
+		button:DisableDrawLayer("ARTWORK")
+		button:StyleButton()
+
+		button.Icon:SkinIcon()
+		button.Icon:SetSize(27, 27)
+		button.Icon:SetPoint("LEFT", button, "LEFT", 6, 0)
+
+		button.bg = CreateFrame("Frame", nil, button)
+		button.bg:CreateBackdrop("Overlay")
+		button.bg:SetFrameLevel(button:GetFrameLevel() - 1)
+		button.bg:SetPoint("TOPLEFT", 2, -3)
+		button.bg:SetPoint("BOTTOMRIGHT", -2, 3)
+	end
+
+	T.SkinScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
 	T.SkinCloseButton(AzeriteEssenceUICloseButton)
 end
 
