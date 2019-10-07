80e1789fab4cae44a26deb2b1bbfd4b50e15f6c8
Wetxius <wetxius@gmail.com>
2019-10-07 20:09:49 +0300

    Added test command for BonusRollFrame.

--- ShestakUI/Core/Commands.lua
+++ ShestakUI/Core/Commands.lua
@@ -309,6 +309,7 @@ SlashCmdList.TEST_ACHIEVEMENT = function()
 	RafRewardDeliveredAlertSystem:AddAlert("", "Interface\\Icons\\Ability_pvp_gladiatormedallion", TRINKET0SLOT, 214)
 	DigsiteCompleteAlertSystem:AddAlert("Human")
 	NewRecipeLearnedAlertSystem:AddAlert(204)
+	-- BonusRollFrame_StartBonusRoll(242969, 'test', 20, 515, 15, 14)
 end
 SLASH_TEST_ACHIEVEMENT1 = "/tach"
 SLASH_TEST_ACHIEVEMENT2 = "/ефср"
