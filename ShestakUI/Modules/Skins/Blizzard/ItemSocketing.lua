86a75a1b88d0b32c43e7845fd4b0a899d53b4512
Wetxius <wetxius@gmail.com>
2019-10-22 18:25:58 +0300

    Returned back color for ItemSocketing.

--- ShestakUI/Modules/Skins/Blizzard/ItemSocketing.lua
+++ ShestakUI/Modules/Skins/Blizzard/ItemSocketing.lua
@@ -34,6 +34,29 @@ local function LoadSkin()
 		button_icon:SetPoint("BOTTOMRIGHT", -2, 2)
 	end
 
+	local GEM_TYPE_INFO = {
+		Yellow = {r = 0.97, g = 0.82, b = 0.29},
+		Red = {r = 1, g = 0.47, b = 0.47},
+		Blue = {r = 0.47, g = 0.67, b = 1},
+		PunchcardRed = {r = 1, g = 0.47, b = 0.47},
+		PunchcardYellow = {r = 0.97, g = 0.82, b = 0.29},
+		PunchcardBlue = {r = 0.47, g = 0.67, b = 1}
+	}
+
+	hooksecurefunc("ItemSocketingFrame_Update", function()
+		for i, socket in ipairs(_G.ItemSocketingFrame.Sockets) do
+			local gemColor = GetSocketTypes(i)
+			local color = GEM_TYPE_INFO[gemColor]
+			if color then
+				socket:SetBackdropBorderColor(color.r, color.g, color.b)
+				socket.overlay:SetVertexColor(color.r, color.g, color.b, 0.35)
+			else
+				socket:SetBackdropBorderColor(unpack(C.media.border_color))
+				socket.overlay:SetVertexColor(0.1, 0.1, 0.1, 1)
+			end
+		end
+	end)
+
 	ItemSocketingFramePortrait:Kill()
 	ItemSocketingSocketButton:ClearAllPoints()
 	ItemSocketingSocketButton:SetPoint("BOTTOMRIGHT", ItemSocketingFrame.backdrop, "BOTTOMRIGHT", -5, 5)
