8ca5e41d2ee8d5646c1864d749b910d43aa87e9b
Wetxius <wetxius@gmail.com>
2019-10-26 14:03:50 +0300

    Added tooltip for TrainAll button.

--- ShestakUI/Modules/Misc/TrainAll.lua
+++ ShestakUI/Modules/Misc/TrainAll.lua
@@ -7,6 +7,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(self, event, addon)
 	if addon == "Blizzard_TrainerUI" then
+		local cost, num
 		local button = CreateFrame("Button", "ClassTrainerTrainAllButton", ClassTrainerFrame, "UIPanelButtonTemplate")
 		button:SetText(ACHIEVEMENTFRAME_FILTER_ALL)
 		if C.skins.blizzard_frames == true then
@@ -16,6 +17,13 @@ frame:SetScript("OnEvent", function(self, event, addon)
 			button:SetPoint("TOPRIGHT", ClassTrainerTrainButton, "TOPLEFT", 0, 0)
 		end
 		button:SetWidth(min(50, button:GetTextWidth() + 15))
+		button:SetScript("OnEnter", function()
+			GameTooltip:SetOwner(button, "ANCHOR_RIGHT")
+			GameTooltip:SetText(AVAILABLE..": "..num.."\n"..COSTS_LABEL.." "..GetMoneyString(cost))
+		end)
+		button:SetScript("OnLeave", function()
+			GameTooltip:Hide()
+		end)
 		button:SetScript("OnClick", function()
 			for i = 1, GetNumTrainerServices() do
 				if select(2, GetTrainerServiceInfo(i)) == "available" then
@@ -24,13 +32,14 @@ frame:SetScript("OnEvent", function(self, event, addon)
 			end
 		end)
 		hooksecurefunc("ClassTrainerFrame_Update", function()
+			num, cost = 0, 0
 			for i = 1, GetNumTrainerServices() do
-				if ClassTrainerTrainButton:IsEnabled() and select(2, GetTrainerServiceInfo(i)) == "available" then
-					button:Enable()
-					return
+				if select(2, GetTrainerServiceInfo(i)) == "available" then
+					num = num + 1
+					cost = cost + GetTrainerServiceCost(i)
 				end
 			end
-			button:Disable()
+			button:SetEnabled(num > 0)
 		end)
 	end
 end)
