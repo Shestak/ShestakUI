a985d62c5c63b17e1fb629a1f22b0fd75fc1693b
Wetxius <wetxius@gmail.com>
2019-11-23 11:17:41 +0300

    Rework button for SafariHat. Now in pet journal frame.

--- ShestakUI/Modules/Announcements/SafariHat.lua
+++ ShestakUI/Modules/Announcements/SafariHat.lua
@@ -4,34 +4,46 @@ if C.announcements.safari_hat ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Check Safari Hat
 ----------------------------------------------------------------------------------------
-local button = CreateFrame("Button", "PetSafariButton", UIParent, "SecureActionButtonTemplate, ActionButtonTemplate")
-button:Hide()
-button:SetAttribute("type", "toy")
-button:SetAttribute("toy", 92738)
-button:SetTemplate("Default")
-button:StyleButton()
-button:SetNormalTexture(nil)
-button.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-button.icon:ClearAllPoints()
-button.icon:SetPoint("TOPLEFT", 2, -2)
-button.icon:SetPoint("BOTTOMRIGHT", -2, 2)
-button.icon:SetTexture("Interface\\Icons\\inv_helm_cloth_petsafari_a_01")
-
-button:SetScript("OnEnter", function(self)
-	GameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT")
-	GameTooltip:SetToyByItemID(92738)
-end)
-
-button:SetScript("OnLeave", function()
-	GameTooltip:Hide()
-end)
-
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("PET_BATTLE_OPENING_DONE")
-frame:RegisterEvent("PET_BATTLE_OVER")
-frame:SetScript("OnEvent", function(_, event)
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, addon)
+	if addon == "Blizzard_Collections" then
+		if not PlayerHasToy(92738) then return end
+		local button = CreateFrame("Button", "PetJournalSafariButton", PetJournal, "SecureActionButtonTemplate, ActionButtonTemplate")
+		button:SetAttribute("type", "toy")
+		button:SetAttribute("toy", 92738)
+		if C.skins.blizzard_frames == true then
+			button:SetPoint("TOPLEFT", PetJournal, "TOPRIGHT", 1, 0)
+			button:SetTemplate("Default")
+			button:StyleButton()
+			button:SetNormalTexture(nil)
+			button.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			button.icon:ClearAllPoints()
+			button.icon:SetPoint("TOPLEFT", 2, -2)
+			button.icon:SetPoint("BOTTOMRIGHT", -2, 2)
+			button.icon:SetDrawLayer("OVERLAY")
+		else
+			button:SetPoint("TOPLEFT", PetJournal, "TOPRIGHT", 3, -24)
+		end
+
+		button.icon:SetTexture(774766)
+
+		button:SetScript("OnEnter", function(self)
+			GameTooltip:SetOwner(self, "ANCHOR_BOTTOMRIGHT")
+			GameTooltip:SetToyByItemID(92738)
+		end)
+
+		button:SetScript("OnLeave", function()
+			GameTooltip:Hide()
+		end)
+	end
+end)
+
+local announceFrame = CreateFrame("Frame")
+announceFrame:RegisterEvent("PET_BATTLE_OPENING_DONE")
+announceFrame:RegisterEvent("PET_BATTLE_OVER")
+announceFrame:SetScript("OnEvent", function(_, event)
 	if event == "PET_BATTLE_OPENING_DONE" then
-		button:Hide()
 		local name = GetSpellInfo(158486)
 		if PlayerHasToy(92738) and not T.CheckPlayerBuff(name) then
 			local maxlevel = true
@@ -42,13 +54,9 @@ frame:SetScript("OnEvent", function(_, event)
 				end
 			end
 			if maxlevel then return end
-			button:SetPoint("BOTTOM", PetBattleBarHolder, "TOP", 0, 45)
-			button:Show()
 			PlaySound(SOUNDKIT.RAID_WARNING, "master")
 			RaidNotice_AddMessage(RaidWarningFrame, RESISTANCE_NONE.." "..GetSpellLink(158486).."!", ChatTypeInfo["RAID_WARNING"])
 			print("|cffff3300"..RESISTANCE_NONE.." "..GetSpellLink(158486).."|cffff3300!|r")
 		end
-	else
-		button:Hide()
 	end
 end)
