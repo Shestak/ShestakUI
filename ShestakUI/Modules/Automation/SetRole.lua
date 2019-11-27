7e1933d3a05611c86326d95fa84eaf03143e1d14
Wetxius <wetxius@gmail.com>
2019-11-27 14:07:40 +0300

    Rework SetRole to less spam.

--- ShestakUI/Modules/Automation/SetRole.lua
+++ ShestakUI/Modules/Automation/SetRole.lua
@@ -4,18 +4,21 @@ if C.automation.auto_role ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Automatically sets your role(Auto role setter by iSpawnAtHome)
 ----------------------------------------------------------------------------------------
+local prev = 0
 local function SetRole()
-	local spec = GetSpecialization()
-	if T.level >= 10 and not InCombatLockdown() then
-		if spec == nil then
-			UnitSetRole("player", "No Role")
-		elseif spec ~= nil then
-			if GetNumGroupMembers() > 1 then
-				local role = GetSpecializationRole(spec)
-				if UnitGroupRolesAssigned("player") ~= role then
+	if T.level >= 10 and not InCombatLockdown() and IsInGroup() and not IsPartyLFG() then
+		local spec = GetSpecialization()
+		if spec then
+			local role = GetSpecializationRole(spec)
+			if UnitGroupRolesAssigned("player") ~= role then
+				local t = GetTime()
+				if t - prev > 2 then
+					prev = t
 					UnitSetRole("player", role)
 				end
 			end
+		else
+			UnitSetRole("player", "No Role")
 		end
 	end
 end
