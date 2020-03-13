538187245f42307c46b062647d263114c960a6d8
Wetxius <wetxius@gmail.com>
2020-03-13 20:08:09 +0300

    Added "Gift of the Titans" proc.

--- ShestakUI/Config/Filters/FilgerSpells.lua
+++ ShestakUI/Config/Filters/FilgerSpells.lua
@@ -2813,7 +2813,10 @@ C["filger_spells"] = {
 
 			-- Ashjra'kamas, Shroud of Resolve
 			-- Draconic Empowerment
-			{spellID = 317859 , unitID = "player", caster = "player", filter = "BUFF"},
+			{spellID = 317859, unitID = "player", caster = "player", filter = "BUFF"},
+
+			-- Gift of the Titans
+			{spellID = 313698, unitID = "player", caster = "player", filter = "BUFF"},
 
 			-- Potions: Miscellaneous
 			-- Invisibility [Invisibility Potion]
