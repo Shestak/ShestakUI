899d38001676fe2ac64d821188df5e941401e63a
Wetxius <wetxius@gmail.com>
2020-06-02 14:54:59 +0300

    Use global cooldown spell id for GCD module.

--- ShestakUI/Libs/oUF/Modules/GCD.lua
+++ ShestakUI/Libs/oUF/Modules/GCD.lua
@@ -10,21 +10,6 @@ local oUF = ns.oUF
 local starttime, duration, usingspell, spellid
 local GetTime = GetTime
 
-local spells = {
-	["DEATHKNIGHT"] = 50977,
-	["DEMONHUNTER"] = 204157,
-	["DRUID"] = 8921,
-	["HUNTER"] = 982,
-	["MAGE"] = 118,
-	["MONK"] = 100780,
-	["PALADIN"] = 35395,
-	["PRIEST"] = 585,
-	["ROGUE"] = 1752,
-	["SHAMAN"] = 403,
-	["WARLOCK"] = 686,
-	["WARRIOR"] = 57755,
-}
-
 local Enable = function(self)
 	if not self.GCD then return end
 	local bar = self.GCD
@@ -49,17 +34,6 @@ local Enable = function(self)
 		end
 	end
 
-	local function Init()
-		local isKnown = IsSpellKnown(spells[T.class])
-		if isKnown then
-			spellid = spells[T.class]
-		end
-		if spellid == nil then
-			return
-		end
-		return spellid
-	end
-
 	local function OnHide()
 		bar:SetScript("OnUpdate", nil)
 		usingspell = nil
@@ -70,12 +44,7 @@ local Enable = function(self)
 	end
 
 	local function UpdateGCD()
-		if spellid == nil then
-			if Init() == nil then
-				return
-			end
-		end
-		local start, dur = GetSpellCooldown(spellid)
+		local start, dur = GetSpellCooldown(61304)
 		if dur and dur > 0 and dur <= 2 then
 			usingspell = 1
 			starttime = start
