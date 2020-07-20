01d3525ef1f5b8681d32fc600bf27814608d99bc
Wetxius <wetxius@gmail.com>
2020-07-20 11:23:56 +0300

    Added popup with canceling auto summon.

--- ShestakUI/Modules/Automation/Summon.lua
+++ ShestakUI/Modules/Automation/Summon.lua
@@ -4,15 +4,29 @@ if C.automation.summon ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Auto confirm summon
 ----------------------------------------------------------------------------------------
+local disable = false
 local AutoSummon = CreateFrame("Frame")
 AutoSummon:RegisterEvent("CONFIRM_SUMMON")
 AutoSummon:SetScript("OnEvent", function(_, event)
 	if event == "CONFIRM_SUMMON" then
 		if not UnitAffectingCombat("player") and PlayerCanTeleport() then
+			StaticPopup_Show("CANCEL_SUMMON")
 			C_Timer.After(10, function()
-				C_SummonInfo.ConfirmSummon()
-				StaticPopup_Hide("CONFIRM_SUMMON")
+				if disable then return end
+				if not UnitAffectingCombat("player") and PlayerCanTeleport() then
+					C_SummonInfo.ConfirmSummon()
+					StaticPopup_Hide("CONFIRM_SUMMON")
+				end
 			end)
 		end
 	end
 end)
+
+StaticPopupDialogs["CANCEL_SUMMON"] = {
+	text = LFG_LIST_AUTO_ACCEPT.." ".. string.lower(SUMMONS);
+	button1 = CANCEL,
+	OnAccept = function(self)
+		disable = true
+	end,
+	timeout = 12,
+}
