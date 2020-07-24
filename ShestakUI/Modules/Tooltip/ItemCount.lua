2290c0104bf48e52c6b9e4996cb1369b9c3dcdaf
Wetxius <wetxius@gmail.com>
2020-07-24 11:24:13 +0300

    Simplified ItemCount code and changed description.

--- ShestakUI/Modules/Tooltip/ItemCount.lua
+++ ShestakUI/Modules/Tooltip/ItemCount.lua
@@ -2,29 +2,13 @@
 if C.tooltip.enable ~= true or C.tooltip.item_count ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	Item count in tooltip(by Tukz)
+--	Item count in bags and bank(by Tukz)
 ----------------------------------------------------------------------------------------
-GameTooltip:HookScript("OnTooltipCleared", function(self) self.UIItemTooltip = nil end)
 GameTooltip:HookScript("OnTooltipSetItem", function(self)
-	if UIItemTooltip and not self.UIItemTooltip and UIItemTooltip.count then
-		local _, link = self:GetItem()
-		local num = GetItemCount(link, true)
-		local item_count = ""
+	local _, link = self:GetItem()
+	local num = GetItemCount(link, true)
 
-		if UIItemTooltip.count and num > 1 then
-			item_count = "|cffffffff"..L_TOOLTIP_ITEM_COUNT.." "..num
-		end
-
-		self:AddLine(item_count)
-		self.UIItemTooltip = 1
+	if num > 1 then
+		self:AddLine("|cffffffff"..L_TOOLTIP_ITEM_COUNT.." "..num.."|r")
 	end
-end)
-
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(_, _, name)
-	if name ~= "ShestakUI" then return end
-	f:UnregisterEvent("ADDON_LOADED")
-	f:SetScript("OnEvent", nil)
-	UIItemTooltip = UIItemTooltip or {count = true}
 end)
