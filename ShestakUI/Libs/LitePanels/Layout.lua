d7fd9084ed7da4f9c204fea6c2322947c331c046
Wetxius <wetxius@gmail.com>
2020-08-03 19:11:54 +0300

    Tweak size for AFK panel.

--- ShestakUI/Libs/LitePanels/Layout.lua
+++ ShestakUI/Libs/LitePanels/Layout.lua
@@ -8,7 +8,7 @@ lpanels:CreateLayout("Load For All", {
 
 -- AFK panel
 {	name = "AFK", anchor_to = "TOP", y_off = -210,
-	width = 180, height = 75 + (C.font.stats_font_size - 8),
+	width = 180 + (C.font.stats_font_size - 8), height = 75 + ((C.font.stats_font_size - 8) * 5),
 	text = {
 			{	string = L_PANELS_AFK, anchor_to = "TOP", y_off = -10,
 				shadow = 0, outline = 3, font = C.font.stats_font, size = C.font.stats_font_size,
@@ -23,7 +23,7 @@ lpanels:CreateLayout("Load For All", {
 				shadow = 0, outline = 3, font = C.font.stats_font, size = C.font.stats_font_size * 2,
 				anchor_to = "CENTER", color = "1 0.1 0.1"
 			},
-			{	string = L_PANELS_AFK_RCLICK, anchor_to = "BOTTOM", y_off = 13,
+			{	string = L_PANELS_AFK_RCLICK, anchor_to = "BOTTOM", y_off = 13 + (C.font.stats_font_size - 8),
 				shadow = 0, outline = 3, font = C.font.stats_font, size = C.font.stats_font_size,
 			},
 			{	string = L_PANELS_AFK_LCLICK, anchor_to = "BOTTOM", y_off = 3,
