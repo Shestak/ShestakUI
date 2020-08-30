9ba861b3440e8cb7b320c439ad0897b5b3569e81
Wetxius <wetxius@gmail.com>
2020-08-30 19:23:33 +0300

    Fixed CancelBadBuffs.

--- ShestakUI/Modules/Automation/CancelBadBuffs.lua
+++ ShestakUI/Modules/Automation/CancelBadBuffs.lua
@@ -7,15 +7,16 @@ if C.automation.cancel_bad_buffs ~= true then return end
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_AURA")
 frame:SetScript("OnEvent", function(_, event, unit)
-	if unit ~= "player" then return end
-
-	if event == "UNIT_AURA" and not InCombatLockdown() then
-		for buff, enabled in next, T.BadBuffs do
-			local icon = T.CheckPlayerBuff(buff)
-			if icon and enabled then
-				CancelUnitBuff(unit, icon)
-				print("|cffffff00"..ACTION_SPELL_AURA_REMOVED.."|r "..(GetSpellLink(buff) or ("|cffffff00["..buff.."]|r")).."|cffffff00.|r")
+	if event == "UNIT_AURA" and unit == "player" and not InCombatLockdown() then
+		local i = 1
+		while true do
+			local name = UnitBuff(unit, i)
+			if not name then return end
+			if T.BadBuffs[name] then
+				CancelSpellByName(name)
+				print("|cffffff00"..ACTION_SPELL_AURA_REMOVED.." ["..name.."].|r")
 			end
+			i = i + 1
 		end
 	end
 end)
