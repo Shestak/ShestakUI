ec1ef939f25981148844669024cb7e2613581734
Wetxius <wetxius@gmail.com>
2020-10-15 12:58:47 +0300

    Start skinning BarberShop.

--- ShestakUI/Modules/Skins/Blizzard/BarberShop.lua
+++ ShestakUI/Modules/Skins/Blizzard/BarberShop.lua
@@ -5,37 +5,32 @@ if C.skins.blizzard_frames ~= true then return end
 --	BarbershopUI skin
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
-	-- BarberShopFrame:StripTextures()
-	-- BarberShopFrame:CreateBackdrop("Transparent")
-	-- BarberShopFrame.backdrop:SetPoint("TOPLEFT", 2, -2)
-	-- BarberShopFrame.backdrop:SetPoint("BOTTOMRIGHT", -2, 35)
+	local buttons = {
+		BarberShopFrame.AcceptButton,
+		BarberShopFrame.CancelButton,
+		BarberShopFrame.ResetButton
+	}
 
-	-- -- BarberShopFrameMoneyFrame:StripTextures()
-	-- -- BarberShopFrameMoneyFrame:CreateBackdrop("Overlay")
-	-- -- BarberShopFrameMoneyFrame.backdrop:SetPoint("TOPLEFT", -3, 0)
-	-- -- BarberShopFrameMoneyFrame.backdrop:SetPoint("BOTTOMRIGHT", 0, 3)
+	for i = 1, #buttons do
+		local button = buttons[i]
+		button:SkinButton(true)
+		button:SetScale(UIParent:GetScale())
+	end
 
-	-- BarberShopFrame.AcceptButton:SkinButton(true)
-	-- BarberShopFrame.CancelButton:SkinButton(true)
-	-- BarberShopFrame.ResetButton:SkinButton(true)
+	local smallButtons = {
+		CharCustomizeFrame.SmallButtons.ResetCameraButton,
+		CharCustomizeFrame.SmallButtons.ZoomOutButton,
+		CharCustomizeFrame.SmallButtons.ZoomInButton,
+		CharCustomizeFrame.SmallButtons.RotateLeftButton,
+		CharCustomizeFrame.SmallButtons.RotateRightButton,
+	}
 
-	-- for i = 1, #BarberShopFrame.Selector do
-		-- local prevBtn, nextBtn = BarberShopFrame.Selector[i]:GetChildren()
-		-- T.SkinNextPrevButton(prevBtn, true)
-		-- T.SkinNextPrevButton(nextBtn)
-	-- end
-
-	-- BarberShopBannerFrameBGTexture:Kill()
-	-- BarberShopBannerFrame:Kill()
-
-	-- BarberShopBannerFrameCaption:ClearAllPoints()
-	-- BarberShopBannerFrameCaption:SetPoint("TOP", BarberShopFrame, 0, 0)
-	-- BarberShopBannerFrameCaption:SetParent(BarberShopFrame)
-
-	-- BarberShopAltFormFrameBorder:StripTextures()
-	-- BarberShopAltFormFrame:SetPoint("BOTTOM", BarberShopFrame, "TOP", 0, 3)
-	-- BarberShopAltFormFrame:StripTextures()
-	-- BarberShopAltFormFrame:CreateBackdrop("Transparent")
+	for i = 1, #smallButtons do
+		local button = smallButtons[i]
+		button:SetNormalTexture("")
+		button:SetPushedTexture("")
+		button:SetHighlightTexture("")
+	end
 end
 
 T.SkinFuncs["Blizzard_BarbershopUI"] = LoadSkin
