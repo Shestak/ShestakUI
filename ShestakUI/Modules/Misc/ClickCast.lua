affaed8d1fe233bcd3f69b0cedb3346d2e7b8d6b
Wetxius <wetxius@gmail.com>
2020-10-17 20:34:13 +0300

    Fixed backdrop error for ClickCast.

--- ShestakUI/Modules/Misc/ClickCast.lua
+++ ShestakUI/Modules/Misc/ClickCast.lua
@@ -85,7 +85,7 @@ SpellBinder.makeSpellsList = function(_, delete)
 	for i, spell in ipairs(DB.spells) do
 		local v = spell.spell
 		if v then
-			local bf = _G["SpellBinder"..i.."_cbs"] or CreateFrame("Button", "SpellBinder"..i.."_cbs", scroll)
+			local bf = _G["SpellBinder"..i.."_cbs"] or CreateFrame("Button", "SpellBinder"..i.."_cbs", scroll, "BackdropTemplate")
 			spell.checked = spell.checked or false
 
 			if i == 1 then
--- ShestakUI_Config/Options.lua
+++ ShestakUI_Config/Options.lua
@@ -215,7 +215,7 @@ SpellList.makeSpellsList = function(_, db, double)
 			if not isFilger or isFilger and spell[2] == T.class then
 				local sp = (double or ShestakUIOptionsPanelfilger:IsShown()) and spell[1] or spell
 				local name, _, icon = GetSpellInfo(sp)
-				local bf = _G["SpellList"..i.."_cbs"] or CreateFrame("Button", "SpellList"..i.."_cbs", scroll)
+				local bf = _G["SpellList"..i.."_cbs"] or CreateFrame("Button", "SpellList"..i.."_cbs", scroll, "BackdropTemplate")
 
 				if i == 1 then
 					bf:SetPoint("TOPLEFT", scroll, "TOPLEFT", 10, -5)
