13ed99468243f8d5614b7bd47d2025f40b01e614
Wetxius <wetxius@gmail.com>
2020-10-20 14:00:12 +0300

    Disable Quest level function.

--- ShestakUI/Modules/Quests/QuestLog.lua
+++ ShestakUI/Modules/Quests/QuestLog.lua
@@ -3,22 +3,23 @@ local T, C, L, _ = unpack(select(2, ...))
 ----------------------------------------------------------------------------------------
 --	Quest level
 ----------------------------------------------------------------------------------------
-local function Showlevel()
-	for button in pairs(QuestScrollFrame.titleFramePool.activeObjects) do
-		local title = button.info.title
-		local isHeader = button.info.isHeader
-		local level = button.info.level
-		if title and not isHeader then
-			local title = level ~= T.level and "["..level.."] "..title or title
-			local height = button.Text:GetHeight()
-			button.Text:SetText(title)
-			button.Text:SetWidth(190)
-			button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
-			button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
-		end
-	end
-end
-hooksecurefunc("QuestLogQuests_Update", Showlevel)
+-- It breaks space between quests
+--FIXME local function Showlevel()
+	-- for button in pairs(QuestScrollFrame.titleFramePool.activeObjects) do
+		-- local title = button.info.title
+		-- local isHeader = button.info.isHeader
+		-- local level = button.info.level
+		-- if title and not isHeader then
+			-- local title = level ~= T.level and "["..level.."] "..title or title
+			-- local height = button.Text:GetHeight()
+			-- button.Text:SetText(title)
+			-- button.Text:SetWidth(190)
+			-- button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
+			-- button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+		-- end
+	-- end
+-- end
+-- hooksecurefunc("QuestLogQuests_Update", Showlevel)
 
 ----------------------------------------------------------------------------------------
 --	Ctrl+Click to abandon a quest or Alt+Click to share a quest(by Suicidal Katt)
