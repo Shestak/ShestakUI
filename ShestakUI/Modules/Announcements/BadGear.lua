2ec64595114bf1b31f09cebc5fc87ab3673abdcf
Wetxius <wetxius@gmail.com>
2020-10-22 18:33:45 +0300

    Cleanup.

--- ShestakUI/Modules/Announcements/BadGear.lua
+++ ShestakUI/Modules/Announcements/BadGear.lua
@@ -8,15 +8,15 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 frame:SetScript("OnEvent", function(_, event)
 	if event ~= "ZONE_CHANGED_NEW_AREA" or not IsInInstance() then return end
-	local item = {}
 	for i = 1, 17 do
-		if T.AnnounceBadGear[i] ~= nil then
-			item[i] = GetInventoryItemID("player", i) or 0
+		if T.AnnounceBadGear[i] then
+			local id = GetInventoryItemID("player", i) or 0
 			for _, baditem in pairs(T.AnnounceBadGear[i]) do
-				if item[i] == baditem then
+				if id == baditem then
+					local _, itemLink = GetItemInfo(id)
 					PlaySound(SOUNDKIT.RAID_WARNING, "Master")
-					RaidNotice_AddMessage(RaidWarningFrame, format("%s %s", CURRENTLY_EQUIPPED, select(2, GetItemInfo(item[i])).."!!!"), ChatTypeInfo["RAID_WARNING"])
-					print(format("|cffff3300%s %s", CURRENTLY_EQUIPPED, select(2, GetItemInfo(item[i])).."|cffff3300!!!|r"))
+					RaidNotice_AddMessage(RaidWarningFrame, format("%s %s", CURRENTLY_EQUIPPED, itemLink.."!!!"), ChatTypeInfo["RAID_WARNING"])
+					print(format("|cffff3300%s %s", CURRENTLY_EQUIPPED, itemLink.."|cffff3300!!!|r"))
 				end
 			end
 		end
