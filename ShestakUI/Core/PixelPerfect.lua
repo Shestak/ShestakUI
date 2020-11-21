962c928d656fc69ddcbb9a75adfa90e0dfae6051
Wetxius <wetxius@gmail.com>
2020-11-21 11:33:18 +0300

    Cleanup.

--- ShestakUI/Core/PixelPerfect.lua
+++ ShestakUI/Core/PixelPerfect.lua
@@ -3,50 +3,44 @@
 ----------------------------------------------------------------------------------------
 --	Pixel perfect script of custom ui Scale
 ----------------------------------------------------------------------------------------
-T.UIScale = function()
-	if T.screenWidth <= 1440 then
-		T.low_resolution = true
-	else
-		T.low_resolution = false
-	end
-
-	if C.general.auto_scale == true then
-		C.general.uiscale = min(2, max(0.20, 768 / T.screenHeight))
-		if T.screenHeight >= 2400 then
-			C.general.uiscale = C.general.uiscale * 3
-		elseif T.screenHeight >= 1600 then
-			C.general.uiscale = C.general.uiscale * 2
-		end
-		C.general.uiscale = tonumber(string.sub(C.general.uiscale, 0, 5)) -- 8.1 Fix scale bug
-	end
-end
-T.UIScale()
-
-local mult = 768 / T.screenHeight / C.general.uiscale
-local Scale = function(x)
-	return mult * math.floor(x / mult + 0.5)
+if T.screenWidth <= 1440 then
+	T.low_resolution = true
+else
+	T.low_resolution = false
 end
 
-T.Scale = function(x) return Scale(x) end
-T.mult = mult
+if C.general.auto_scale == true then
+	C.general.uiscale = min(2, max(0.20, 768 / T.screenHeight))
+	if T.screenHeight >= 2400 then
+		C.general.uiscale = C.general.uiscale * 3
+	elseif T.screenHeight >= 1600 then
+		C.general.uiscale = C.general.uiscale * 2
+	end
+	C.general.uiscale = tonumber(string.sub(C.general.uiscale, 0, 5)) -- 8.1 Fix scale bug
+end
+
+T.mult = 768 / T.screenHeight / C.general.uiscale
 T.noscalemult = T.mult * C.general.uiscale
 
+T.Scale = function(x)
+	return T.mult * math.floor(x / T.mult + 0.5)
+end
+
 ----------------------------------------------------------------------------------------
---	Pixel perfect fonts function
+--	Pixel perfect fonts for high resolution
 ----------------------------------------------------------------------------------------
 if T.screenHeight <= 1200 then return end
-C.media.pixel_font_size = C.media.pixel_font_size * mult
-C.font.chat_tabs_font_size = C.font.chat_tabs_font_size * mult
-C.font.action_bars_font_size = C.font.action_bars_font_size * mult
-C.font.threat_meter_font_size = C.font.threat_meter_font_size * mult
-C.font.raid_cooldowns_font_size = C.font.raid_cooldowns_font_size * mult
-C.font.unit_frames_font_size = C.font.unit_frames_font_size * mult
-C.font.auras_font_size = C.font.auras_font_size * mult
-C.font.filger_font_size = C.font.filger_font_size * mult
-C.font.bags_font_size = C.font.bags_font_size * mult
-C.font.loot_font_size = C.font.loot_font_size * mult
-C.font.combat_text_font_size = C.font.combat_text_font_size * mult
-
-C.font.stats_font_size = C.font.stats_font_size * mult
-C.font.stylization_font_size = C.font.stylization_font_size * mult
-C.font.cooldown_timers_font_size = C.font.cooldown_timers_font_size * mult
+C.media.pixel_font_size = C.media.pixel_font_size * T.mult
+C.font.chat_tabs_font_size = C.font.chat_tabs_font_size * T.mult
+C.font.action_bars_font_size = C.font.action_bars_font_size * T.mult
+C.font.threat_meter_font_size = C.font.threat_meter_font_size * T.mult
+C.font.raid_cooldowns_font_size = C.font.raid_cooldowns_font_size * T.mult
+C.font.unit_frames_font_size = C.font.unit_frames_font_size * T.mult
+C.font.auras_font_size = C.font.auras_font_size * T.mult
+C.font.filger_font_size = C.font.filger_font_size * T.mult
+C.font.bags_font_size = C.font.bags_font_size * T.mult
+C.font.loot_font_size = C.font.loot_font_size * T.mult
+C.font.combat_text_font_size = C.font.combat_text_font_size * T.mult
+C.font.stats_font_size = C.font.stats_font_size * T.mult
+C.font.stylization_font_size = C.font.stylization_font_size * T.mult
+C.font.cooldown_timers_font_size = C.font.cooldown_timers_font_size * T.mult
