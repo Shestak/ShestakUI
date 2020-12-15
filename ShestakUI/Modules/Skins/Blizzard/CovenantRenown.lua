131a8cfed7017dac9da553292714b19d0ea121bc
Wetxius <wetxius@gmail.com>
2020-12-15 12:18:44 +0300

    [Skins] Added skin for CovenantRenown rewards.

--- ShestakUI/Modules/Skins/Blizzard/CovenantRenown.lua
+++ ShestakUI/Modules/Skins/Blizzard/CovenantRenown.lua
@@ -13,6 +13,29 @@ local function LoadSkin()
 		self.CloseButton.Border:Hide()
 		self:StripTextures()
 	end)
+
+	hooksecurefunc(frame, "SetRewards", function(self)
+		for reward in self.rewardsPool:EnumerateActive() do
+			if not reward.backdrop then
+				reward:CreateBackdrop("Overlay")
+				reward.backdrop:SetPoint("TOPLEFT", reward, 2, -15)
+				reward.backdrop:SetPoint("BOTTOMRIGHT", reward, -2, 15)
+
+				reward.Toast:SetAlpha(0)
+				reward.Highlight:SetAlpha(0)
+				reward.CircleMask:Hide()
+				reward.IconBorder:SetAlpha(0)
+
+				reward.b = CreateFrame("Frame", nil, reward)
+				reward.b:SetTemplate("Default")
+				reward.b:SetPoint("TOPLEFT", reward.Icon, "TOPLEFT", -2, 2)
+				reward.b:SetPoint("BOTTOMRIGHT", reward.Icon, "BOTTOMRIGHT", 2, -2)
+				reward.Icon:SetParent(reward.b)
+				reward.Icon:SetTexCoord(0.15, 0.85, 0.15, 0.85)
+				reward.Check:SetParent(reward.b)
+			end
+		end
+	end)
 end
 
 T.SkinFuncs["Blizzard_CovenantRenown"] = LoadSkin
