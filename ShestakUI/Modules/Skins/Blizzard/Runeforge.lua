072503dfe9559e45e3f1331c998a4ba80ed3fb9f
Wetxius <wetxius@gmail.com>
2020-12-24 20:31:12 +0300

    [Skins] Added Runeforge skin.

---
+++ ShestakUI/Modules/Skins/Blizzard/Runeforge.lua
@@ -0,0 +1,38 @@
+local T, C, L, _ = unpack(select(2, ...))
+if C.skins.blizzard_frames ~= true then return end
+
+----------------------------------------------------------------------------------------
+--	Runeforge skin
+----------------------------------------------------------------------------------------
+local function LoadSkin()
+	local frame = RuneforgeFrame
+	T.SkinCloseButton(frame.CloseButton)
+	frame.CloseButton:SetPoint("TOPRIGHT", -50, 50)
+
+	frame.CreateFrame.CraftItemButton:SkinButton()
+
+	local powerFrame = frame.CraftingFrame.PowerFrame
+	powerFrame:StripTextures()
+	powerFrame:CreateBackdrop("Overlay")
+
+	local pageControl = powerFrame.PageControl
+	T.SkinNextPrevButton(pageControl.BackwardButton)
+	T.SkinNextPrevButton(pageControl.ForwardButton)
+
+	hooksecurefunc(powerFrame.PowerList, "RefreshListDisplay", function(list)
+		if not list.elements then return end
+
+		for i = 1, list:GetNumElementFrames() do
+			local button = list.elements[i]
+			if button and not button.IsSkinned then
+				button.Border:SetAlpha(0)
+				button.CircleMask:Hide()
+				button.Icon:SkinIcon()
+
+				button.IsSkinned = true
+			end
+		end
+	end)
+end
+
+T.SkinFuncs["Blizzard_RuneforgeUI"] = LoadSkin
