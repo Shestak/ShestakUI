bc189047d7658f8a6fa2146fba9fecaf1d3d289d
Wetxius <wetxius@gmail.com>
2021-02-04 19:35:25 +0300

    [TellTarget] Allow whisper self player.

--- ShestakUI/Modules/Chat/TellTarget.lua
+++ ShestakUI/Modules/Chat/TellTarget.lua
@@ -10,7 +10,7 @@ for i = 1, NUM_CHAT_WINDOWS do
 		local text = self:GetText()
 		if text:len() < 7 then
 			if text:sub(1, 4) == "/tt " or text:sub(1, 6) == "/ее " then
-				if UnitCanCooperate("player", "target") then
+				if UnitCanAssist("player", "target") then
 					ChatFrame_SendTell((GetUnitName("target", true)), ChatFrame1)
 				end
 			end
