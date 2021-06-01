e41a67e34140327e6d831d84c69bf9037955589f
Wetxius <wetxius@gmail.com>
2021-06-01 13:26:13 +0300

    Cleanup.

--- ShestakUI/Modules/Trade/Merchant.lua
+++ ShestakUI/Modules/Trade/Merchant.lua
@@ -5,34 +5,23 @@ local T, C, L, _ = unpack(select(2, ...))
 ----------------------------------------------------------------------------------------
 hooksecurefunc("MerchantItemButton_OnModifiedClick", function(self)
 	if IsAltKeyDown() then
-		local itemLink = GetMerchantItemLink(self:GetID())
+		local id = self:GetID()
+		local itemLink = GetMerchantItemLink(id)
 		if not itemLink then return end
 
 		local maxStack = select(8, GetItemInfo(itemLink))
 		if maxStack and maxStack > 1 then
-			local numAvailable = select(5, GetMerchantItemInfo(self:GetID()))
+			local numAvailable = select(5, GetMerchantItemInfo(id))
 			if numAvailable > -1 then
-				BuyMerchantItem(self:GetID(), numAvailable)
+				BuyMerchantItem(id, numAvailable)
 			else
-				BuyMerchantItem(self:GetID(), GetMerchantItemMaxStack(self:GetID()))
+				BuyMerchantItem(id, GetMerchantItemMaxStack(id))
 			end
 		end
 	end
 end)
 
-local function IsMerchantButtonOver()
-	return GetMouseFocus():GetName() and GetMouseFocus():GetName():find("MerchantItem%d")
-end
-
-GameTooltip:HookScript("OnTooltipSetItem", function()
-	if MerchantFrame:IsShown() and IsMerchantButtonOver() then
-		for i = 2, GameTooltip:NumLines() do
-			if _G["GameTooltipTextLeft"..i]:GetText():find(ITEM_VENDOR_STACK_BUY) then
-				GameTooltip:AddLine("|cff00ff00<"..L_MISC_BUY_STACK..">|r")
-			end
-		end
-	end
-end)
+ITEM_VENDOR_STACK_BUY = _G.ITEM_VENDOR_STACK_BUY.."\n|cff00ff00<"..L_MISC_BUY_STACK..">|r"
 
 ----------------------------------------------------------------------------------------
 --	Show item level for weapons and armor in merchant
