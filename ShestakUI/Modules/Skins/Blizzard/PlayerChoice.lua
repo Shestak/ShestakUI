f65d955cca959d0284edae9917474cf46cb9d01e
Wetxius <wetxius@gmail.com>
2021-06-29 13:25:22 +0300

    [Skins] Rework PlayerChoice skin for 9.1.

--- ShestakUI/Modules/Skins/Blizzard/PlayerChoice.lua
+++ ShestakUI/Modules/Skins/Blizzard/PlayerChoice.lua
@@ -7,12 +7,12 @@ if C.skins.blizzard_frames ~= true then return end
 local function LoadSkin()
 	local frame = _G.PlayerChoiceFrame
 
-	hooksecurefunc(frame, "Update", function()
+	hooksecurefunc(frame, "TryShow", function(self)
 		if not frame.IsSkinned then
 			frame.BlackBackground:SetAlpha(0)
 			frame.Background:SetAlpha(0)
 			frame.NineSlice:SetAlpha(0)
-			frame.BorderFrame.Header:SetAlpha(0)
+			frame.Header.Texture:SetAlpha(0)
 
 			frame:CreateBackdrop("Transparent")
 
@@ -20,7 +20,6 @@ local function LoadSkin()
 			frame.Title.Text:SetTextColor(1, .8, 0)
 
 			T.SkinCloseButton(frame.CloseButton)
-			frame.CloseButton.Border:SetAlpha(0)
 
 			hooksecurefunc(frame.CloseButton, "SetPoint", function(self, point, anchor, attachTo, x, y)
 				if x ~= -4 then
@@ -31,70 +30,83 @@ local function LoadSkin()
 			frame.IsSkinned = true
 		end
 
+		if frame.CloseButton.Border then
+			frame.CloseButton.Border:SetAlpha(0)
+		end
+
 		local IsInJailers = IsInJailersTower()
 		frame.backdrop:SetShown(not IsInJailers)
 
-		for i = 1, frame:GetNumOptions() do
-			local option = frame.Options[i]
-			local hasArtworkBorderArt = option.ArtworkBorder:IsShown() or option.ArtworkBorderDisabled:IsShown()
+		for option in self.optionPools:EnumerateActiveByTemplate(self.optionFrameTemplate) do
+			local hasArtworkBorderArt = option.ArtworkBorder and option.ArtworkBorder:IsShown()
 			option:CreateBackdrop("Overlay")
 			option.backdrop:SetPoint("TOPLEFT", -2, 20)
+			option.backdrop:SetPoint("BOTTOMRIGHT", 2, -8)
 			option.backdrop:SetShown(not IsInJailers and hasArtworkBorderArt)
 
-			for i = 1, #option.OptionButtonsContainer.Buttons do
-				local button = option.OptionButtonsContainer.Buttons[i]
-				if not button.isSkinned then
-					if IsInJailers then
-						button:StripTextures(true)
-					end
-					if i == 1 or (hasArtworkBorderArt and i == 2) then
-						button:SkinButton()
-					end
-					button.isSkinned = true
-				end
-			end
-
-			option.Header.Text:SetTextColor(1, .8, 0)
+			if option.Header.Ribbon then option.Header.Ribbon:SetAlpha(0) end
+			if option.Header.Contents then option.Header.Contents.Text:SetTextColor(1, .8, 0) end
 			option.OptionText:SetTextColor(1, 1, 1)
-
-			local reward = option.RewardsFrame.Rewards.lastReward
-			if reward then
-				if not reward.backdrop then
-					reward.Icon:SkinIcon()
-				end
-				if reward.Name then reward.Name:SetTextColor(1, 1, 1) end
-				if reward.IconBorder then reward.IconBorder:SetTexture("") end
-				local r, g, b
-				if reward.IconBorder and reward.IconBorder:IsShown() then
-					r, g, b = reward.IconBorder:GetVertexColor()
-					if (r > 0.64 and r < 0.67) or (r > 0.99 and g > 0.99 and b > 0.99) then
-						r, g, b = unpack(C.media.border_color)
-					end
-				else
-					r, g, b = unpack(C.media.border_color)
-				end
-				reward.backdrop:SetBackdropBorderColor(r, g, b)
-			end
 
 			option.Background:SetShown(not hasArtworkBorderArt)
 			if IsInJailers then
 				option.Background:Show()
 			end
-			option.Header.Ribbon:SetAlpha(0)
 
-			option.ArtworkBorder:SetAlpha(0)
-			option.ArtworkBorderDisabled:SetAlpha(0)
-			if not option.ArtBackdrop then
-				option.ArtBackdrop = CreateFrame("Frame", nil, option)
-				option.ArtBackdrop:SetFrameLevel(option:GetFrameLevel())
-				option.ArtBackdrop:SetPoint("TOPLEFT", option.Artwork, -2, 2)
-				option.ArtBackdrop:SetPoint("BOTTOMRIGHT", option.Artwork, 2, -2)
-				option.ArtBackdrop:SetTemplate("Default")
+			if option.ArtworkBorder then
+				option.ArtworkBorder:SetAlpha(0)
+				if not option.ArtBackdrop then
+					option.ArtBackdrop = CreateFrame("Frame", nil, option)
+					option.ArtBackdrop:SetFrameLevel(option:GetFrameLevel())
+					option.ArtBackdrop:SetPoint("TOPLEFT", option.Artwork, -2, 2)
+					option.ArtBackdrop:SetPoint("BOTTOMRIGHT", option.Artwork, 2, -2)
+					option.ArtBackdrop:SetTemplate("Default")
+				end
+				option.ArtBackdrop:SetShown(not IsInJailers and hasArtworkBorderArt)
+				if PlayerChoiceFrame:IsLegacy() then -- Garrison
+					option.ArtBackdrop:Hide()
+				end
 			end
-			option.ArtBackdrop:SetShown(not IsInJailers and hasArtworkBorderArt)
 
-			if option.WidgetContainer.widgetFrames then
-				for _, widgetFrame in next, option.WidgetContainer.widgetFrames do
+			local buttonsContainer = option.OptionButtonsContainer
+			if buttonsContainer and buttonsContainer.buttonPool then
+				for button in buttonsContainer.buttonPool:EnumerateActive() do
+					if not button.isSkinned then
+						if IsInJailers then
+							button:StripTextures(true)
+						end
+						-- if i == 1 or (hasArtworkBorderArt and i == 2) then
+							button:SkinButton()
+						-- end
+						button.isSkinned = true
+					end
+				end
+			end
+
+			local rewardframe = option.Rewards
+			if rewardframe then
+				for reward in rewardframe.rewardsPool:EnumerateActive() do
+					if not reward.backdrop and reward.Icon then
+						reward.Icon:SkinIcon()
+					end
+					if reward.Name then reward.Name:SetTextColor(1, 1, 1) end
+					if reward.IconBorder then reward.IconBorder:SetTexture("") end
+					local r, g, b
+					if reward.IconBorder and reward.IconBorder:IsShown() then
+						r, g, b = reward.IconBorder:GetVertexColor()
+						if (r > 0.64 and r < 0.67) or (r > 0.99 and g > 0.99 and b > 0.99) then
+							r, g, b = unpack(C.media.border_color)
+						end
+					else
+						r, g, b = unpack(C.media.border_color)
+					end
+					if reward.backdrop then reward.backdrop:SetBackdropBorderColor(r, g, b) end
+				end
+			end
+
+			local widgetFrames = option.WidgetContainer.widgetFrames
+			if widgetFrames then
+				for _, widgetFrame in next, widgetFrames do
 					if widgetFrame.widgetType == _G.Enum.UIWidgetVisualizationType.TextWithState then
 						widgetFrame.Text:SetTextColor(1, 1, 1)
 					elseif widgetFrame.widgetType == _G.Enum.UIWidgetVisualizationType.SpellDisplay then
@@ -117,4 +129,4 @@ local function LoadSkin()
 	end)
 end
 
-T.SkinFuncs["Blizzard_PlayerChoiceUI"] = LoadSkin
+T.SkinFuncs["Blizzard_PlayerChoice"] = LoadSkin
