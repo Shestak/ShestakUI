cee87a414bc2f609c61cb39178f7f2051dfe3e77
Wetxius <wetxius@gmail.com>
2021-08-04 14:50:04 +0300

    Fixed RaidDPS error.

--- ShestakUI/Modules/UnitFrames/RaidDPS.lua
+++ ShestakUI/Modules/UnitFrames/RaidDPS.lua
@@ -17,8 +17,8 @@ local partytarget_height = party_height
 local raid_width = C.raidframe.dps_raid_width
 local raid_height = C.raidframe.dps_raid_height
 local raid_power_height = C.raidframe.dps_raid_power_height
-local tank_width = C.raidframe.heal_width
-local tank_height = C.raidframe.heal_height
+local tank_width = C.raidframe.heal_raid_width
+local tank_height = C.raidframe.heal_raid_height
 
 -- Create layout
 local function Shared(self, unit)
