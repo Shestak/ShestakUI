7cd0dc9c0f821004bf3aa164f18c579ae450719d
Wetxius <Wetxius@users.noreply.github.com>
2022-09-27 10:41:29 +0300

    Update Chinese.lua

--- ShestakUI/Locales/Chinese.lua
+++ ShestakUI/Locales/Chinese.lua
@@ -168,8 +168,8 @@ L_INFO_SKIN_DISABLED1 = "界面美化启用"
 L_INFO_SKIN_DISABLED2 = " 已禁用"
 
 -- Moving elements
-L_MOVE_RIGHT_CLICK = "右键点击重置位置"
-L_MOVE_MIDDLE_CLICK = "中键点击临时隐藏"
+L_MOVE_RIGHT_CLICK = "右键重置位置"
+L_MOVE_MIDDLE_CLICK = "中键临时隐藏"
 
 -- Popups
 L_POPUP_INSTALLUI = "该角色首次使用ShestakUI，你必须重新加载UI来配置。"
