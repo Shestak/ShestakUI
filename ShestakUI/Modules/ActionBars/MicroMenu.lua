66f0f80f7f7f3fa1cdffc65f10c8d4bd9c38eeba
Wetxius <Wetxius@users.noreply.github.com>
2022-10-28 19:03:05 +0300

    Fixed MicroMenu.

--- ShestakUI/Modules/ActionBars/MicroMenu.lua
+++ ShestakUI/Modules/ActionBars/MicroMenu.lua
@@ -11,6 +11,21 @@ frame:SetSize(284, 30)
 UpdateMicroButtonsParent(frame)
 if C.actionbar.micromenu_mouseover == true then frame:SetAlpha(0) end
 
+local MICRO_BUTTONS = {
+	"CharacterMicroButton",
+	"SpellbookMicroButton",
+	"TalentMicroButton",
+	"AchievementMicroButton",
+	"QuestLogMicroButton",
+	"GuildMicroButton",
+	"LFDMicroButton",
+	"EJMicroButton",
+	"CollectionsMicroButton",
+	"MainMenuMicroButton",
+	"HelpMicroButton",
+	"StoreMicroButton",
+}
+
 for _, button in pairs(MICRO_BUTTONS) do
 	local bu = _G[button]
 	local normal = bu:GetNormalTexture()
@@ -19,30 +34,33 @@ for _, button in pairs(MICRO_BUTTONS) do
 
 	bu:SetParent(frame)
 	bu.SetParent = T.dummy
-	_G[button.."Flash"]:SetTexture("")
+	if button.Flash then
+		button.Flash:SetInside()
+		button.Flash:SetTexture()
+	end
 	bu:SetHighlightTexture(C.media.empty)
 	bu.SetHighlightTexture = T.dummy
 
 	local f = CreateFrame("Frame", nil, bu)
 	f:SetFrameLevel(1)
 	f:SetFrameStrata("BACKGROUND")
-	f:SetPoint("BOTTOMLEFT", bu, "BOTTOMLEFT", 2, 0)
-	f:SetPoint("TOPRIGHT", bu, "TOPRIGHT", -2, -6)
+	f:SetPoint("BOTTOMLEFT", bu, "BOTTOMLEFT", 1, 0)
+	f:SetPoint("TOPRIGHT", bu, "TOPRIGHT", -1, -2)
 	f:SetTemplate("Default")
 	bu.frame = f
 
-	normal:SetTexCoord(0.22, 0.81, 0.26, 0.82)
+	normal:SetTexCoord(0.1, 0.85, 0.12, 0.78)
 	normal:ClearAllPoints()
 	normal:SetPoint("TOPLEFT", f, "TOPLEFT", 2, -2)
 	normal:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -2, 2)
 
-	pushed:SetTexCoord(0.22, 0.81, 0.26, 0.82)
+	pushed:SetTexCoord(0.1, 0.85, 0.12, 0.78)
 	pushed:ClearAllPoints()
 	pushed:SetPoint("TOPLEFT", f, "TOPLEFT", 2, -2)
 	pushed:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -2, 2)
 
 	if disabled then
-		disabled:SetTexCoord(0.22, 0.81, 0.26, 0.82)
+		disabled:SetTexCoord(0.1, 0.85, 0.12, 0.78)
 		disabled:ClearAllPoints()
 		disabled:SetPoint("TOPLEFT", f, "TOPLEFT", 2, -2)
 		disabled:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -2, 2)
@@ -64,15 +82,15 @@ end
 
 -- Fix textures for buttons
 hooksecurefunc("UpdateMicroButtons", function()
-	MicroButtonPortrait:ClearAllPoints()
-	MicroButtonPortrait:SetPoint("TOPLEFT", CharacterMicroButton.frame, "TOPLEFT", 2, -2)
-	MicroButtonPortrait:SetPoint("BOTTOMRIGHT", CharacterMicroButton.frame, "BOTTOMRIGHT", -2, 2)
+	-- MicroButtonPortrait:ClearAllPoints()
+	-- MicroButtonPortrait:SetPoint("TOPLEFT", CharacterMicroButton.frame, "TOPLEFT", 2, -2)
+	-- MicroButtonPortrait:SetPoint("BOTTOMRIGHT", CharacterMicroButton.frame, "BOTTOMRIGHT", -2, 2)
 
 	CharacterMicroButton:ClearAllPoints()
 	CharacterMicroButton:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", -2, 0)
 
-	GuildMicroButtonTabard:ClearAllPoints()
-	GuildMicroButtonTabard:SetPoint("TOP", GuildMicroButton.frame, "TOP", 0, 25)
+	-- GuildMicroButtonTabard:ClearAllPoints()
+	-- GuildMicroButtonTabard:SetPoint("TOP", GuildMicroButton.frame, "TOP", 0, 25)
 
-	MainMenuBarPerformanceBar:SetPoint("BOTTOM", MainMenuMicroButton, "BOTTOM", 0, 0)
+	MainMenuMicroButton.MainMenuBarPerformanceBar:SetPoint("BOTTOM", MainMenuMicroButton, "BOTTOM", 0, 0)
 end)
