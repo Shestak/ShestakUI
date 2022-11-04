50d8cc388b21cb393b489159f0a1794fb149482c
Wetxius <Wetxius@users.noreply.github.com>
2022-11-04 12:40:54 +0300

    Fixed ToggleMenu position.

--- ShestakUI/Modules/Maps/ToggleMenu.lua
+++ ShestakUI/Modules/Maps/ToggleMenu.lua
@@ -517,19 +517,21 @@ refreshAddOnMenu()
 ----------------------------------------------------------------------------------------
 --	Reposition if minimap on top
 ----------------------------------------------------------------------------------------
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:SetScript("OnEvent", function()
-	local _, y = Minimap:GetCenter()
-	local vhalf = (y > UIParent:GetHeight() / 2) and "TOP" or "BOTTOM"
-	if vhalf == "TOP" then
-		MenuBG:ClearAllPoints()
-		MenuBG:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 2, -3)
+if C.minimap.on_top then
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	frame:SetScript("OnEvent", function()
+		local _, y = Minimap:GetCenter()
+		local vhalf = (y > UIParent:GetHeight() / 2) and "TOP" or "BOTTOM"
+		if vhalf == "TOP" then
+			MenuBG:ClearAllPoints()
+			MenuBG:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 2, -3)
 
-		AddonBG:ClearAllPoints()
-		AddonBG:SetPoint("TOPRIGHT", MenuBG, "TOPRIGHT", 0, 0)
+			AddonBG:ClearAllPoints()
+			AddonBG:SetPoint("TOPRIGHT", MenuBG, "TOPRIGHT", 0, 0)
 
-		OpenMenuBG:ClearAllPoints()
-		OpenMenuBG:SetPoint("TOP", MenuBG, "TOP", 0, 0)
-	end
-end)
+			OpenMenuBG:ClearAllPoints()
+			OpenMenuBG:SetPoint("TOP", MenuBG, "TOP", 0, 0)
+		end
+	end)
+end
