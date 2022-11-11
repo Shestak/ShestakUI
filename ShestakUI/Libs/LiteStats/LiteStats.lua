2ba5ef9e322e1b87d658cfff70f07eb67f1c5e75
Wetxius <Wetxius@users.noreply.github.com>
2022-11-11 20:43:47 +0300

    Fixed Durability datatext for 10.0.2.

--- ShestakUI/Libs/LiteStats/LiteStats.lua
+++ ShestakUI/Libs/LiteStats/LiteStats.lua
@@ -1058,7 +1058,7 @@ end
 if durability.enabled then
 	Inject("Durability", {
 		OnLoad = function(self)
-			CreateFrame("GameTooltip", "LPDURA")
+			CreateFrame("GameTooltip", "LPDURA", nil, "GameTooltipTemplate")
 			LPDURA:SetOwner(WorldFrame, "ANCHOR_NONE")
 			if durability.man then DurabilityFrame.Show = DurabilityFrame.Hide end
 			RegEvents(self, "UPDATE_INVENTORY_DURABILITY MERCHANT_SHOW PLAYER_LOGIN")
@@ -1118,7 +1118,13 @@ if durability.enabled then
 					local perc = dur ~= 0 and dur/dmax or 0
 					local hex = gradient(perc)
 					GameTooltip:AddDoubleLine(durability.gear_icons and format("|T%s:"..t_icon..":"..t_icon..":0:0:64:64:5:59:5:59:%d|t %s", GetInventoryItemTexture(P, slot), t_icon, string) or string,format("|cffaaaaaa%s/%s | %s%s%%", dur, dmax, hex, floor(perc * 100)), 1, 1, 1)
-					totalcost, nodur = totalcost + select(3, LPDURA:SetInventoryItem(P, slot))
+					if T.newPatch then
+						local data = LPDURA:GetTooltipData()
+						repairCost = data and data.repairCost or 0
+						totalcost, nodur = totalcost + repairCost
+					else
+						totalcost, nodur = totalcost + select(3, LPDURA:SetInventoryItem(P, slot))
+					end
 				end
 			end
 			if nodur then
