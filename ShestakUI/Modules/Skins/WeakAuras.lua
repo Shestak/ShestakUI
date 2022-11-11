03a0e0442576f3a89ced8f888476d9d5ecf32cec
Wetxius <Wetxius@users.noreply.github.com>
2022-11-11 15:02:27 +0300

    Enable all addon skins.

--- ShestakUI/Modules/Skins/WeakAuras.lua
+++ ShestakUI/Modules/Skins/WeakAuras.lua
@@ -4,61 +4,65 @@ if C.skins.weak_auras ~= true then return end
 ----------------------------------------------------------------------------------------
 --	WeakAuras skin
 ----------------------------------------------------------------------------------------
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:SetScript("OnEvent", function()
-	if not IsAddOnLoaded("WeakAuras") then return end
+if not IsAddOnLoaded("WeakAuras") then return end
 
-	local function Skin_WeakAuras(frame, type)
-		if not frame.backdrop then
-			if type == "icon" then
-				frame:SetSize(frame:GetWidth() - 5, frame:GetHeight() - 5)
-				frame:CreateBackdrop("Transparent")
-				frame.backdrop:SetBackdropColor(0, 0, 0, 0)
-				frame.backdrop:HookScript("OnUpdate", function(self)
-					self:SetAlpha(self:GetParent().icon:GetAlpha())
-				end)
-			else
-				frame:CreateBackdrop("Transparent")
-				frame.backdrop:SetBackdropColor(0, 0, 0, 0)
-			end
-		end
-
-		if frame.icon then
-			frame.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-			frame.icon.SetTexCoord = T.dummy
-		end
-
-		if frame.bar then
-			frame.bar.fg:SetTexture(C.media.texture)
-			frame.bar.bg:SetTexture(C.media.texture)
-		end
-
-		if frame.stacks then
-			frame.stacks:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.stacks:SetShadowOffset(C.media.cooldown_timers_font_shadow and 1 or 0, C.media.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.text then
-			frame.text:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.text:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.text2 then
-			frame.text2:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.text2:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.timer then
-			frame.timer:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.timer:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
+local function Skin_WeakAuras(frame, type)
+	if not frame.backdrop then
+		if type == "icon" then
+			if frame.b then return end
+			frame.b = CreateFrame("Frame", nil, frame)
+			frame.b:CreateBackdrop("Transparent")
+			frame.b:SetInside(frame)
+			frame.b.backdrop:SetBackdropColor(0, 0, 0, 0)
+			frame.b.backdrop:HookScript("OnUpdate", function(self)
+				self:SetAlpha(frame.icon:GetAlpha())
+			end)
+			frame.icon:SetAllPoints(frame.b)
+		else
+			frame:CreateBackdrop("Transparent")
+			frame.backdrop:SetBackdropColor(0, 0, 0, 0)
 		end
 	end
 
-	for weakAura, _ in pairs(WeakAuras.regions) do
-		if WeakAuras.regions[weakAura].regionType == "icon" or WeakAuras.regions[weakAura].regionType == "aurabar" then
-			Skin_WeakAuras(WeakAuras.regions[weakAura].region, WeakAuras.regions[weakAura].regionType)
-		end
+	if frame.icon then
+		frame.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+		frame.icon.SetTexCoord = T.dummy
+	end
+
+	if frame.bar then
+		frame.bar.fg:SetTexture(C.media.texture)
+		frame.bar.bg:SetTexture(C.media.texture)
+	end
+
+	if frame.stacks then
+		frame.stacks:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
+		frame.stacks:SetShadowOffset(C.media.cooldown_timers_font_shadow and 1 or 0, C.media.cooldown_timers_font_shadow and -1 or 0)
+	end
+
+	if frame.text then
+		frame.text:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
+		frame.text:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
+	end
+
+	if frame.text2 then
+		frame.text2:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
+		frame.text2:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
+	end
+
+	if frame.timer then
+		frame.timer:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
+		frame.timer:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
+	end
+end
+
+hooksecurefunc(WeakAuras.regionPrototype, "create", function(region)
+	if region.regionType == "icon" or region.regionType == "aurabar" then
+		Skin_WeakAuras(region, region.regionType)
+	end
+
+end)
+hooksecurefunc(WeakAuras.regionPrototype, "modifyFinish", function(_, region)
+	if region.regionType == "icon" or region.regionType == "aurabar" then
+		Skin_WeakAuras(region, region.regionType)
 	end
 end)
