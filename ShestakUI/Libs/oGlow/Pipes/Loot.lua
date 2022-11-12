0f0542f51fe094b29264f7a9c8536e0290464cda
Wetxius <Wetxius@users.noreply.github.com>
2022-11-12 19:35:25 +0300

    Fixed oGlow Loot.

--- ShestakUI/Libs/oGlow/Pipes/Loot.lua
+++ ShestakUI/Libs/oGlow/Pipes/Loot.lua
@@ -4,43 +4,30 @@ if C.loot.lootframe == true then return end
 local hook
 local _E
 
-local update = function()
-	if LootFrame:IsShown() and oGlow:IsPipeEnabled"loot" then
-		for i = 1, LOOTFRAME_NUMBUTTONS or 4 do
-			local slotFrame = _G["LootButton"..i]
-			local slot = slotFrame.slot
+local update = function(self)
+	local slot = self:GetSlotIndex()
+	local slotFrame = self.Item
 
-			local itemLink
-			if slot then
-				itemLink = GetLootSlotLink(slot)
-			end
-
-			oGlow:CallFilters("loot", slotFrame, _E and itemLink)
-		end
+	local itemLink
+	if slot then
+		itemLink = GetLootSlotLink(slot)
 	end
+
+	oGlow:CallFilters("loot", slotFrame, _E and itemLink)
 end
 
 local enable = function(self)
 	_E = true
 
 	if not hook then
-		LootFrameUpButton:HookScript("OnClick", update)
-		LootFrameDownButton:HookScript("OnClick", update)
+		hooksecurefunc(LootFrameElementMixin, "Init", update)
 
 		hook = true
 	end
-
-	self:RegisterEvent("LOOT_OPENED", update)
-	self:RegisterEvent("LOOT_SLOT_CLEARED", update)
-	self:RegisterEvent("LOOT_SLOT_CHANGED", update)
 end
 
 local disable = function(self)
 	_E = nil
-
-	self:UnregisterEvent("LOOT_OPENED", update)
-	self:UnregisterEvent("LOOT_SLOT_CLEARED", update)
-	self:UnregisterEvent("LOOT_SLOT_CHANGED", update)
 end
 
 oGlow:RegisterPipe("loot", enable, disable, update, "Loot frame")
