6fc0fd1f4db1e042822ba83f16267fe3238d890f
Wetxius <Wetxius@users.noreply.github.com>
2022-11-24 16:27:51 +0300

    Fixed repair cost for LiteStats.

--- ShestakUI/Libs/LiteStats/LiteStats.lua
+++ ShestakUI/Libs/LiteStats/LiteStats.lua
@@ -1058,8 +1058,6 @@ end
 if durability.enabled then
 	Inject("Durability", {
 		OnLoad = function(self)
-			CreateFrame("GameTooltip", "LPDURA", nil, "GameTooltipTemplate")
-			LPDURA:SetOwner(WorldFrame, "ANCHOR_NONE")
 			if durability.man then DurabilityFrame.Show = DurabilityFrame.Hide end
 			RegEvents(self, "UPDATE_INVENTORY_DURABILITY MERCHANT_SHOW PLAYER_LOGIN")
 		end,
@@ -1081,11 +1079,17 @@ if durability.enabled then
 								total = GetRepairAllCost(); RepairAllItems()
 							else
 								for id = 1, 18 do
-									local cost = select(3, LPDURA:SetInventoryItem(P, id))
-									if cost ~= 0 and cost <= GetMoney() then
-										if not InRepairMode() then ShowRepairCursor() end
-										PickupInventoryItem(id)
-										total = total + cost
+									local data = C_TooltipInfo.GetInventoryItem(P, id)
+									if data then
+										local argVal = data.args and data.args[7]
+										if argVal and argVal.field == "repairCost" then
+											local cost = argVal.intVal
+											if cost ~= 0 and cost <= GetMoney() then
+												if not InRepairMode() then ShowRepairCursor() end
+												PickupInventoryItem(id)
+												total = total + cost
+											end
+										end
 									end
 								end
 							end
@@ -1118,12 +1122,15 @@ if durability.enabled then
 					local perc = dur ~= 0 and dur/dmax or 0
 					local hex = gradient(perc)
 					GameTooltip:AddDoubleLine(durability.gear_icons and format("|T%s:"..t_icon..":"..t_icon..":0:0:64:64:5:59:5:59:%d|t %s", GetInventoryItemTexture(P, slot), t_icon, string) or string,format("|cffaaaaaa%s/%s | %s%s%%", dur, dmax, hex, floor(perc * 100)), 1, 1, 1)
-					if T.newPatch then
-						local data = LPDURA:GetTooltipData()
-						repairCost = data and data.repairCost or 0
-						totalcost, nodur = totalcost + repairCost
-					else
-						totalcost, nodur = totalcost + select(3, LPDURA:SetInventoryItem(P, slot))
+					local data = C_TooltipInfo.GetInventoryItem(P, slot)
+					if data then
+						local argVal = data.args and data.args[7]
+						if argVal and argVal.field == "repairCost" then
+							totalcost = totalcost + argVal.intVal
+							if totalcost > 0 then
+								nodur = false
+							end
+						end
 					end
 				end
 			end
