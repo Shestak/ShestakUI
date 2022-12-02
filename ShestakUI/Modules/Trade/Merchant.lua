82c7d2b6ec2abae58ed9b46975afd19ca4913d6c
Wetxius <Wetxius@users.noreply.github.com>
2022-12-02 20:17:48 +0300

    Replace LE_ITEM_ with Enum for OneClickDisenchanting and Merchant.

--- ShestakUI/Modules/Trade/Merchant.lua
+++ ShestakUI/Modules/Trade/Merchant.lua
@@ -47,7 +47,7 @@ local function MerchantItemlevel()
 			local itemLink = GetMerchantItemLink(index)
 			if itemLink then
 				local _, _, quality, itemlevel, _, _, _, _, _, _, _, itemClassID = GetItemInfo(itemLink)
-				if (itemlevel and itemlevel > 1) and (quality and quality > 1) and (itemClassID == LE_ITEM_CLASS_WEAPON or itemClassID == LE_ITEM_CLASS_ARMOR) then
+				if (itemlevel and itemlevel > 1) and (quality and quality > 1) and (itemClassID == Enum.ItemClass.Weapon or itemClassID == Enum.ItemClass.Armor) then
 					button.text:SetText(itemlevel)
 				end
 			end
--- ShestakUI/Modules/Trade/OneClickDisenchanting.lua
+++ ShestakUI/Modules/Trade/OneClickDisenchanting.lua
@@ -167,11 +167,7 @@ function button:PLAYER_LOGIN()
 				spell, r, g, b = GetSpellInfo(31252), 1, 0.33, 0.33
 			elseif disenchanter then
 				local _, _, itemRarity, _, _, _, _, _, _, _, _, class, subClass = GetItemInfo(link)
-				if T.newPatch then
-					if not (itemRarity and (itemRarity > 1 and (itemRarity < 5 or itemRarity == 6))) then return end
-				else
-					if not (class == LE_ITEM_CLASS_WEAPON or class == LE_ITEM_CLASS_ARMOR or (class == 3 and subClass == 11)) or not (itemRarity and (itemRarity > 1 and (itemRarity < 5 or itemRarity == 6))) then return end
-				end
+				if not (class == Enum.ItemClass.Weapon or class == Enum.ItemClass.Armor or (class == 3 and subClass == 11)) or not (itemRarity and (itemRarity > 1 and (itemRarity < 5 or itemRarity == 6))) then return end
 				spell, r, g, b = GetSpellInfo(13262), 0.5, 0.5, 1
 			elseif rogue then
 				for index = 1, self:NumLines() do
