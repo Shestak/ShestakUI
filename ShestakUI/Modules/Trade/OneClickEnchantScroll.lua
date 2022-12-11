15371b9cf5680bbe34467a5518c50c89dd092061
Wetxius <Wetxius@users.noreply.github.com>
2022-12-11 17:49:43 +0300

    Fixed OneClickEnchantScroll.

--- ShestakUI/Modules/Trade/OneClickEnchantScroll.lua
+++ ShestakUI/Modules/Trade/OneClickEnchantScroll.lua
@@ -1,52 +1,49 @@
 local T, C, L, _ = unpack(select(2, ...))
-if C.trade.enchantment_scroll ~= true then return end
+if C.trade.enchantment_scroll ~= true or IsAddOnLoaded("OneClickEnchantScroll") then return end
 
 ----------------------------------------------------------------------------------------
 --	Enchantment scroll on TradeSkill frame(OneClickEnchantScroll by Sara.Festung)
 ----------------------------------------------------------------------------------------
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
-frame:SetScript("OnEvent", function(_, _, addon)
-	if addon == "Blizzard_TradeSkillUI" and not IsAddOnLoaded("OneClickEnchantScroll") then
-		local button = CreateFrame("Button", "TradeSkillCreateScrollButton", TradeSkillFrame, "MagicButtonTemplate")
-		if C.skins.blizzard_frames == true then
-			button:SkinButton(true)
-			button:SetPoint("TOPRIGHT", TradeSkillFrame.DetailsFrame.CreateButton, "TOPLEFT", -1, 0)
-		else
-			button:SetPoint("TOPRIGHT", TradeSkillFrame.DetailsFrame.CreateButton, "TOPLEFT")
-		end
-		button:SetScript("OnClick", function()
-			C_TradeSkillUI.CraftRecipe(TradeSkillFrame.DetailsFrame.selectedRecipeID)
-			UseItemByName(38682)
-		end)
+local button = CreateFrame("Button", "TradeSkillCreateScrollButton", ProfessionsFrame, "MagicButtonTemplate")
+if C.skins.blizzard_frames == true then
+	button:SkinButton(true)
+	button:SetPoint("TOPRIGHT", ProfessionsFrame.CraftingPage.CreateButton, "TOPLEFT", -1, 0)
+else
+	button:SetPoint("TOPRIGHT", ProfessionsFrame.CraftingPage.CreateButton, "TOPLEFT")
+end
+button:SetScript("OnClick", function()
+	local currentRecipeInfo = ProfessionsFrame.CraftingPage.SchematicForm:GetRecipeInfo()
+	if currentRecipeInfo then
+		C_TradeSkillUI.CraftRecipe(currentRecipeInfo.recipeID)
+		UseItemByName(38682)
+	end
+end)
 
-		hooksecurefunc(TradeSkillFrame.DetailsFrame, "RefreshButtons", function(self)
-			if C_TradeSkillUI.IsTradeSkillGuild() or C_TradeSkillUI.IsNPCCrafting() or C_TradeSkillUI.IsTradeSkillLinked() then
-				button:Hide()
-			else
-				local recipeInfo = self.selectedRecipeID and C_TradeSkillUI.GetRecipeInfo(self.selectedRecipeID)
-				if recipeInfo and recipeInfo.alternateVerb then
-					local _, _, _, _, _, parentSkillLineID = C_TradeSkillUI.GetTradeSkillLine()
-					if parentSkillLineID == 333 then
-						button:Show()
-						local numCreateable = recipeInfo.numAvailable
-						local numScrollsAvailable = GetItemCount(38682)
-						button:SetText(L_MISC_SCROLL.." ("..numScrollsAvailable..")")
-						 if numScrollsAvailable == 0 then
-							numCreateable = 0
-						end
-						if numCreateable > 0 then
-							button:Enable()
-						else
-							button:Disable()
-						end
-					else
-						button:Hide()
-					end
-				else
-					button:Hide()
+hooksecurefunc(ProfessionsFrame.CraftingPage, "ValidateControls", function(self)
+	if C_TradeSkillUI.IsTradeSkillGuild() or C_TradeSkillUI.IsNPCCrafting() or C_TradeSkillUI.IsTradeSkillLinked() then
+		button:Hide()
+	else
+		local currentRecipeInfo = ProfessionsFrame.CraftingPage.SchematicForm:GetRecipeInfo()
+		if currentRecipeInfo and currentRecipeInfo.alternateVerb then
+			local professionInfo = ProfessionsFrame:GetProfessionInfo()
+			if professionInfo and professionInfo.parentProfessionID == 333 then
+				button:Show()
+				local isEnchantingRecipe = currentRecipeInfo.isEnchantingRecipe
+				local numScrollsAvailable = GetItemCount(38682)
+				button:SetText(L_MISC_SCROLL.." ("..numScrollsAvailable..")")
+				 if numScrollsAvailable == 0 then
+					isEnchantingRecipe = false
 				end
+				if isEnchantingRecipe then
+					button:Enable()
+				else
+					button:Disable()
+				end
+			else
+				button:Hide()
 			end
-		end)
+		else
+			button:Hide()
+		end
 	end
 end)
