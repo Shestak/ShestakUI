c24f8cf9e67c7a89e7e5c6c65126b1b104866452
Wetxius <Wetxius@users.noreply.github.com>
2022-12-21 16:26:25 +0300

    [Layout] Added check for dynamic position if enabled auto raid layout.

--- ShestakUI/Modules/UnitFrames/Layout.lua
+++ ShestakUI/Modules/UnitFrames/Layout.lua
@@ -1433,7 +1433,7 @@ end
 if C.raidframe.auto_position == "DYNAMIC" then
 	local prevNum = 5
 	local function Reposition(self, event)
-		if C.raidframe.layout == "HEAL" and not C.raidframe.raid_groups_vertical and C.raidframe.raid_groups > 5 then
+		if (C.raidframe.layout == "HEAL" or C.raidframe.layout == "AUTO") and not C.raidframe.raid_groups_vertical and C.raidframe.raid_groups > 5 then
 			if InCombatLockdown() then
 				self:RegisterEvent("PLAYER_REGEN_ENABLED")
 				return
@@ -1449,9 +1449,11 @@ if C.raidframe.auto_position == "DYNAMIC" then
 			if maxGroup >= C.raidframe.raid_groups then
 				maxGroup = C.raidframe.raid_groups
 			end
+			if C.raidframe.layout == "AUTO" and not T.IsHealerSpec() then maxGroup = 5 end
 			if prevNum ~= maxGroup then
 				-- local offset = (maxGroup - 5) * (C.raidframe.heal_raid_height + 7) + ((maxGroup - ((maxGroup - 5))) * (C.raidframe.heal_raid_height - 26))
 				local offset = (maxGroup - 5) * (C.raidframe.heal_raid_height + 7)
+				if C.raidframe.layout == "AUTO" and not T.IsHealerSpec() then offset = 0 end
 				if C.unitframe.castbar_icon == true then
 					oUF_Player_Castbar:SetPoint(C.position.unitframes.player_castbar[1], C.position.unitframes.player_castbar[2], C.position.unitframes.player_castbar[3], C.position.unitframes.player_castbar[4] + 11, C.position.unitframes.player_castbar[5] + offset)
 				else
@@ -1466,19 +1468,23 @@ if C.raidframe.auto_position == "DYNAMIC" then
 				self:UnregisterEvent("PLAYER_REGEN_ENABLED")
 			end
 		else
-			self:UnregisterEvent("GROUP_ROSTER_UPDATE")
+			self:UnregisterAllEvents()
 		end
 	end
 
 	local frame = CreateFrame("Frame")
 	frame:RegisterEvent("PLAYER_LOGIN")
 	frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+	if C.raidframe.layout == "AUTO" then
+		frame:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
+	end
 	frame:SetScript("OnEvent", Reposition)
 elseif C.raidframe.auto_position == "STATIC" then
-	local function Reposition()
-		if C.raidframe.layout == "HEAL" and not C.raidframe.raid_groups_vertical and C.raidframe.raid_groups > 5 then
+	local function Reposition(self)
+		if (C.raidframe.layout == "HEAL" or C.raidframe.layout == "AUTO") and not C.raidframe.raid_groups_vertical and C.raidframe.raid_groups > 5 then
 			-- local offset = (C.raidframe.raid_groups - 5) * (C.raidframe.heal_raid_height + 7) + ((C.raidframe.raid_groups - ((C.raidframe.raid_groups - 5))) * (C.raidframe.heal_raid_height - 26))
 			local offset = (C.raidframe.raid_groups - 5) * (C.raidframe.heal_raid_height + 7)
+			if C.raidframe.layout == "AUTO" and not T.IsHealerSpec() then offset = 0 end
 			if C.unitframe.castbar_icon == true then
 				oUF_Player_Castbar:SetPoint(C.position.unitframes.player_castbar[1], C.position.unitframes.player_castbar[2], C.position.unitframes.player_castbar[3], C.position.unitframes.player_castbar[4] + 11, C.position.unitframes.player_castbar[5] + offset)
 			else
@@ -1487,10 +1493,15 @@ elseif C.raidframe.auto_position == "STATIC" then
 
 			player:SetPoint(C.position.unitframes.player[1], C.position.unitframes.player[2], C.position.unitframes.player[3], C.position.unitframes.player[4], C.position.unitframes.player[5] + offset)
 			target:SetPoint(C.position.unitframes.target[1], C.position.unitframes.target[2], C.position.unitframes.target[3], C.position.unitframes.target[4], C.position.unitframes.target[5] + offset)
+		else
+			self:UnregisterAllEvents()
 		end
 	end
 
 	local frame = CreateFrame("Frame")
 	frame:RegisterEvent("PLAYER_LOGIN")
+	if C.raidframe.layout == "AUTO" then
+		frame:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
+	end
 	frame:SetScript("OnEvent", Reposition)
 end
