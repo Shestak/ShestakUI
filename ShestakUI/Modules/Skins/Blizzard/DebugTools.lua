05ef7377c905593581e0c6256fe954e0af51481e
Wetxius <Wetxius@users.noreply.github.com>
2023-01-13 17:13:24 +0300

    [Skins] Added TableAttributeDisplay skin.

--- ShestakUI/Modules/Skins/Blizzard/DebugTools.lua
+++ ShestakUI/Modules/Skins/Blizzard/DebugTools.lua
@@ -8,6 +8,40 @@ local function LoadSkin()
 	FrameStackTooltip:HookScript("OnShow", function(self)
 		self.NineSlice:SetTemplate("Transparent")
 	end)
+
+	local function SkinTableAttributeDisplay(frame)
+		if frame.isSkinned then return end
+		T.SkinFrame(frame)
+		frame.ScrollFrameArt.NineSlice:SetTemplate("Overlay")
+
+		T.SkinNextPrevButton(frame.OpenParentButton, nil, "Up")
+		T.SkinNextPrevButton(frame.NavigateBackwardButton, true, "Any")
+		T.SkinNextPrevButton(frame.NavigateForwardButton, nil, "Any")
+		T.SkinNextPrevButton(frame.DuplicateButton, nil, "Down")
+
+		T.SkinCheckBox(frame.VisibilityButton, 26)
+		T.SkinCheckBox(frame.HighlightButton, 26)
+		T.SkinCheckBox(frame.DynamicUpdateButton, 26)
+
+		frame.OpenParentButton:ClearAllPoints()
+		frame.OpenParentButton:SetPoint("TOPLEFT", frame, "TOPLEFT", 4, -4)
+		frame.OpenParentButton:SetSize(17, 17)
+		frame.NavigateBackwardButton:ClearAllPoints()
+		frame.NavigateBackwardButton:SetPoint("LEFT", frame.OpenParentButton, "RIGHT", 3, 0)
+		frame.NavigateForwardButton:ClearAllPoints()
+		frame.NavigateForwardButton:SetPoint("LEFT", frame.NavigateBackwardButton, "RIGHT", 3, 0)
+		frame.DuplicateButton:ClearAllPoints()
+		frame.DuplicateButton:SetPoint("LEFT", frame.NavigateForwardButton, "RIGHT", 3, 0)
+		frame.DuplicateButton:SetSize(17, 17)
+
+		T.SkinEditBox(frame.FilterBox)
+		T.SkinScrollBar(frame.LinesScrollFrame.ScrollBar)
+
+		frame.isSkinned = true
+	end
+
+	SkinTableAttributeDisplay(TableAttributeDisplay)
+	hooksecurefunc(TableInspectorMixin, "InspectTable", SkinTableAttributeDisplay)
 end
 
 T.SkinFuncs["Blizzard_DebugTools"] = LoadSkin
